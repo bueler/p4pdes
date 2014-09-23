962275cf32adfc0238532e4187ba85e3e3fb5809
Ed Bueler <elbueler@alaska.edu>
2014-09-23 08:01:41 -0600

    possibly better way to do it ...

--- setmypetsc.sh
+++ setmypetsc.sh
@@ -1,5 +1,25 @@
+# set PETSC for
+# use
+#   $ source setmypetsc.sh
 
-export PETSC_DIR=~/petsc-3.5.2/
-export PETSC_ARCH=linux-gnu-opt
-export PATH=${PETSC_DIR}${PETSC_ARCH}/bin:$PATH
+machine=$(uname -n)
+if [[ "$machine" == 'bueler-leopard' ]]; then
+   echo "on my workstation ${machine}:"
+   export PETSC_DIR=~/petsc-3.5.2/
+   export PETSC_ARCH=linux-gnu-opt
+   # mpiexec is global
+elif [[ "$machine" == 'bueler-gazelle' ]]; then
+   echo "on my laptop ${machine}:"
+   export PETSC_DIR=~/petsc-3.5.2/
+   export PETSC_ARCH=linux-gnu-opt
+   export PATH=${PETSC_DIR}${PETSC_ARCH}/bin:$PATH  # find mpiexec
+else
+   echo "'uname -n' returns: ${machine}"
+   echo "... huh?  no change to PETSC_DIR/ARCH because unknown machine ..."
+   return
+fi
 
+echo "  PETSC_DIR  = ${PETSC_DIR}"
+echo "  PETSC_ARCH = ${PETSC_ARCH}"
+mympiexec=$(which mpiexec)
+echo "  'which mpiexec' returns: ${mympiexec}"
