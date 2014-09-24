a447a0c8eaf7b084f5c888226ff60abb443ae06a
Ed Bueler <elbueler@alaska.edu>
2014-09-24 09:17:08 -0600

    this version successfully writes and reads one Vec (= x coords of nodes); reads and writes ASCII file on rank 0; reads and shows in parallel

--- c/c2triangle.c
+++ c/c2triangle.c
@@ -10,21 +10,16 @@ Write out in PETSc binary format.\n\n";
 
 #include <petscmat.h>
 
-#define DEBUG 1
+#define doassembly(X) { ierr = VecAssemblyBegin(X); CHKERRQ(ierr); \
+                        ierr = VecAssemblyEnd(X); CHKERRQ(ierr); }
 
-int main(int argc,char **args)
-{
-  // MAJOR VARIABLES
-  PetscInt N,   // number of degrees of freedom (= number of all nodes)
-           M;   // number of elements;
-  Vec vx,vy,    // coordinates of nodes
-      vP,       // array with M rows and 3 columns; P[k][q] is node index
-      vBT;      // array with N rows and 2 columns;
-                //   BT[i] = 0 if interior, 2 if Dirichlet, 3 if Neumann
+#define DEBUG 0
 
-  // INITIALIZE PETSC
+int main(int argc,char **args) {
+
   PetscInitialize(&argc,&args,(char*)0,help);
-  const MPI_Comm  COMM = PETSC_COMM_WORLD;
+  const MPI_Comm  COMM = PETSC_COMM_WORLD,
+                  SELF = PETSC_COMM_SELF;
   PetscMPIInt     rank;
   MPI_Comm_rank(COMM,&rank);
   PetscErrorCode  ierr;
@@ -42,34 +37,41 @@ int main(int argc,char **args)
   }
 
   // BUILD FILENAMES
-  char           nodefilename[MPL], elefilename[MPL];
+  char outfilename[MPL], nodefilename[MPL], elefilename[MPL];
   strcpy(nodefilename,fnameroot);
   strcat(nodefilename,".node");
   strcpy(elefilename,fnameroot);
   strcat(elefilename,".ele");
+  strcpy(outfilename,fnameroot);
+  strcat(outfilename,".petsc");
 
   // RANK 0 OPENS ASCII FILES
-  FILE           *nodefile, *elefile;
+  FILE *nodefile, *elefile;
   ierr = PetscFOpen(COMM,nodefilename,"r",&nodefile); CHKERRQ(ierr);
   ierr = PetscFOpen(COMM,elefilename,"r",&elefile); CHKERRQ(ierr);
 
   if (rank == 0) {
     // READ NODE HEADER
-    PetscInt ndim, nattr, nbdrymarkers;
+    PetscInt N,   // number of degrees of freedom (= number of all nodes)
+             ndim, nattr, nbdrymarkers;
     if (4 != fscanf(nodefile,"%d %d %d %d\n",&N,&ndim,&nattr,&nbdrymarkers)) {
-      SETERRQ1(COMM,1,"expected 4 values in reading from %s",nodefilename);
+      SETERRQ1(SELF,1,"expected 4 values in reading from %s",nodefilename);
     }
     if (ndim != 2) {
-      SETERRQ1(COMM,1,"ndim read from %s not equal to 2",nodefilename);
+      SETERRQ1(SELF,1,"ndim read from %s not equal to 2",nodefilename);
     }
-    ierr = PetscPrintf(COMM,"read %s:\n",nodefilename); CHKERRQ(ierr);
-    ierr = PetscPrintf(COMM,
-             "  N=%d nodes in 2D polygon with %d attributes and %d boundary markers per node\n",
+    ierr = PetscPrintf(SELF,"read %s on rank 0:\n",nodefilename); CHKERRQ(ierr);
+    ierr = PetscPrintf(SELF,
+             "  N=%d nodes in 2D polygon\n"
+             "  %d attributes and %d boundary markers per node\n",
              N,nattr,nbdrymarkers); CHKERRQ(ierr);
 
     // ALLOCATE 1D VECS
-    ierr = VecCreateSeq(COMM,N,&vx); CHKERRQ(ierr);
-    ierr = VecSetFromOptions(vx); CHKERRQ(ierr);
+    Vec vx,vy,    // coordinates of nodes
+        vP,       // array with M rows and 3 columns; P[k][q] is node index
+        vBT;      // array with N rows and 2 columns;
+                  //   BT[i] = 0 if interior, 2 if Dirichlet, 3 if Neumann
+    ierr = VecCreateSeq(SELF,N,&vx); CHKERRQ(ierr);
     ierr = VecDuplicate(vx,&vy); CHKERRQ(ierr);
     ierr = VecDuplicate(vx,&vBT); CHKERRQ(ierr);
     ierr = PetscObjectSetName((PetscObject)vx,"node x coordinate"); CHKERRQ(ierr);
@@ -88,13 +90,13 @@ int main(int argc,char **args)
     for (i = 0; i < N; i++) {
       if (4 != fscanf(nodefile,"%d %lf %lf %lf\n",
                  &iplusone,&(ax[i]),&(ay[i]),&(aBT[i]))) {
-        SETERRQ1(COMM,1,"expected 4 values in reading from %s",nodefilename);
+        SETERRQ1(SELF,1,"expected 4 values in reading from %s",nodefilename);
       }
     }
 #if DEBUG
-    ierr = PetscPrintf(COMM,"boundary type read:\n"); CHKERRQ(ierr);
+    ierr = PetscPrintf(SELF,"node location and boundary type read:\n"); CHKERRQ(ierr);
     for (i = 0; i < N; i++) {
-      ierr = PetscPrintf(COMM,"%4d%6d\n",i,(int)aBT[i]); CHKERRQ(ierr);
+      ierr = PetscPrintf(SELF,"%4d%14.8f%14.8f%6d\n",i,ax[i],ay[i],(int)aBT[i]); CHKERRQ(ierr);
     }
 #endif
     ierr = VecRestoreArray(vx,&ax); CHKERRQ(ierr);
@@ -102,16 +104,18 @@ int main(int argc,char **args)
     ierr = VecRestoreArray(vBT,&aBT); CHKERRQ(ierr);
 
     // READ ELEMENT HEADER
-    PetscInt nthree, nattrele;
+    PetscInt M,   // number of elements
+             nthree, nattrele;
     if (3 != fscanf(elefile,"%d %d %d\n",&M,&nthree,&nattrele)) {
-      SETERRQ1(COMM,1,"expected 3 values in reading from %s",elefilename);
+      SETERRQ1(SELF,1,"expected 3 values in reading from %s",elefilename);
     }
     if (nthree != 3) {
-      SETERRQ1(COMM,1,"nthree read from %s not equal to 3 (= nodes per element)",elefilename);
+      SETERRQ1(SELF,1,"nthree read from %s not equal to 3 (= nodes per element)",elefilename);
     }
-    ierr = PetscPrintf(COMM,"read %s:\n",elefilename); CHKERRQ(ierr);
-    ierr = PetscPrintf(COMM,
-             "  M=%d elements in 2D polygon with %d attributes per element\n",
+    ierr = PetscPrintf(SELF,"read %s on rank 0:\n",elefilename); CHKERRQ(ierr);
+    ierr = PetscPrintf(SELF,
+             "  M=%d elements in 2D polygon\n"
+             "  %d attributes per element\n",
              M,nattrele); CHKERRQ(ierr);
 
     // READ ELEMENTS; EACH PROCESS HOLDS FULL INFO (THREE NODE INDICES PER ELEMENT)
@@ -121,44 +125,71 @@ int main(int argc,char **args)
     for (k = 0; k < M; k++) {
       PetscMalloc(3*sizeof(int),&(P[k]));
       if (4 != fscanf(elefile,"%d %d %d %d\n",&kplusone,&(P[k][0]),&(P[k][1]),&(P[k][2]))) {
-        SETERRQ1(COMM,1,"expected 4 values in reading from %s",elefilename);
+        SETERRQ1(SELF,1,"expected 4 values in reading from %s",elefilename);
       }
     }
 #if DEBUG
-    ierr = PetscPrintf(COMM,"elements read:\n"); CHKERRQ(ierr);
+    ierr = PetscPrintf(SELF,"elements read:\n"); CHKERRQ(ierr);
     for (k = 0; k < M; k++) {
-      ierr = PetscPrintf(COMM,"%4d%8d%6d%6d\n",k,P[k][0],P[k][1],P[k][2]); CHKERRQ(ierr);
+      ierr = PetscPrintf(SELF,"%4d%8d%6d%6d\n",k,P[k][0],P[k][1],P[k][2]); CHKERRQ(ierr);
     }
 #endif
 
-// FIXME: fill Vec vP 
+    // ALLOCATE VIEWER FOR BINARY WRITE, AND VECS
+    PetscViewer viewer;
+    ierr = PetscViewerBinaryOpen(PETSC_COMM_SELF,outfilename,FILE_MODE_WRITE,
+               &viewer); CHKERRQ(ierr); 
 
-    PetscFree(P);
-  }
-  ierr = PetscFClose(COMM,nodefile); CHKERRQ(ierr);
-  ierr = PetscFClose(COMM,elefile); CHKERRQ(ierr);
-
-  // ALLOCATE VIEWER FOR BINARY WRITE, AND VECS
-  PetscViewer viewer;
-  char        outfilename[MPL];
-  strcpy(outfilename,fnameroot);
-  strcat(outfilename,".petsc");
-  ierr = PetscViewerBinaryOpen(PETSC_COMM_WORLD,outfilename,FILE_MODE_WRITE,&viewer); CHKERRQ(ierr);
-
-  if (rank == 0) {
- 
-// FIXME: write Vecs in binary format
     ierr = VecView(vx,viewer); CHKERRQ(ierr);
-    ierr = VecView(vy,viewer); CHKERRQ(ierr);
-    ierr = VecView(vBT,viewer); CHKERRQ(ierr);
+    //ierr = VecView(vy,viewer); CHKERRQ(ierr);
+    //ierr = VecView(vBT,viewer); CHKERRQ(ierr);
+#if DEBUG
+    ierr = VecView(vx,PETSC_VIEWER_STDOUT_SELF); CHKERRQ(ierr);
+#endif
 
     VecDestroy(&vx);
     VecDestroy(&vy);
     VecDestroy(&vP);
     VecDestroy(&vBT);
+
+    PetscViewerDestroy(&viewer);
+
+// FIXME: fill Vec vP 
+
+    PetscFree(P);
   }
 
-  PetscViewerDestroy(&viewer);
+  ierr = PetscFClose(COMM,nodefile); CHKERRQ(ierr);
+  ierr = PetscFClose(COMM,elefile); CHKERRQ(ierr);
+
+  // READ BACK IN PARALLEL: ALLOCATE VIEWER AND VECS
+  Vec rx;
+  //Vec rx,ry,rP,rBT;
+  PetscViewer rviewer;
+  ierr = PetscViewerBinaryOpen(PETSC_COMM_WORLD,outfilename,FILE_MODE_READ,
+             &rviewer); CHKERRQ(ierr);
+  ierr = VecCreate(COMM,&rx); CHKERRQ(ierr);
+  //ierr = VecDuplicate(rx,&ry); CHKERRQ(ierr);
+  //ierr = VecDuplicate(rx,&rBT); CHKERRQ(ierr);
+
+  // FILL 1D VECS FROM FILE
+  ierr = PetscPrintf(COMM,"reading from %s in parallel:\n",outfilename); CHKERRQ(ierr);
+  ierr = VecLoad(rx,rviewer); CHKERRQ(ierr);
+  //ierr = VecLoad(ry,rviewer); CHKERRQ(ierr);
+  //ierr = VecLoad(rBT,rviewer); CHKERRQ(ierr);
+
+  PetscInt rN;
+  ierr = VecGetSize(rx,&rN); CHKERRQ(ierr);
+  ierr = PetscPrintf(COMM,"  N=%d nodes\n",rN); CHKERRQ(ierr);
+
+  ierr = VecView(rx,PETSC_VIEWER_STDOUT_WORLD); CHKERRQ(ierr);
+
+  VecDestroy(&rx);
+  //VecDestroy(&ry);
+  //VecDestroy(&rP);
+  //VecDestroy(&rBT);
+  PetscViewerDestroy(&rviewer);
+
   PetscFinalize();
   return 0;
 }
