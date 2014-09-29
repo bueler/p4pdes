71145f2773381ffec2e2e22bf92ccd3e9a3bfdd5
Ed Bueler <elbueler@alaska.edu>
2014-09-29 08:36:54 -0600

    add ability to generate only the polygon in TikZ

--- tex/tri2tikz.py
+++ tex/tri2tikz.py
@@ -12,31 +12,49 @@ commandline = " ".join(sys.argv[:])
 
 parser = argparse.ArgumentParser(description='Converts .node, .ele, .poly files from triangle into TikZ format.')
 parser.add_argument('--labelnodes', action='store_true',
-                    help='label the nodes with zero-based index', default=False)
+                    help='label the nodes with zero-based index',
+                    default=False)
+parser.add_argument('--polyonly', action='store_true',
+                    help='build a TikZ figure with the polygon only (reads .poly and .node)',
+                    default=False)
 parser.add_argument('--scale', action='store', metavar='X',
-                    help='amount by which to scale TikZ figure', default=1.0)
+                    help='amount by which to scale TikZ figure',
+                    default=1.0)
 parser.add_argument('--labeloffset', action='store', metavar='X',
-                    help='offset to use in labeling points', default=0.0)
+                    help='offset to use in labeling points',
+                    default=0.0)
 parser.add_argument('inroot', metavar='NAMEROOT',
-                    help='root of input file name for .node,.ele,.poly', default='foo')
+                    help='root of input file name for .node,.ele,.poly',
+                    default='foo')
 parser.add_argument('outfile', metavar='FILENAME',
-                    help='output file name', default='foo.tikz')
+                    help='output file name',
+                    default='foo.tikz')
 
 args = parser.parse_args()
-
 nodename = args.inroot + '.node'
 elename  = args.inroot + '.ele'
 polyname = args.inroot + '.poly'
 outname  = args.outfile
-dolabel = args.labelnodes
-scale   = (float)(args.scale)
-offset  = (float)(args.labeloffset)
+dolabel  = args.labelnodes
+polyonly = args.polyonly
+scale    = (float)(args.scale)
+offset   = (float)(args.labeloffset)
 
-nodefile = open(nodename, 'r')
-elefile  = open(elename,  'r')
+# always read .poly and .node files
 polyfile = open(polyname, 'r')
+nodefile = open(nodename, 'r')
+if not polyonly:
+  elefile = open(elename,  'r')
 
-tikz     = open(outname, 'w')
+tikz = open(outname, 'w')
+
+print 'reading from %s ' % polyname
+polyheader1 = polyfile.readline()
+polyheader2 = polyfile.readline()
+print '  header says (line 1):  ' + polyheader1,
+print '  header says (line 2):  ' + polyheader2,
+P = (int)(polyheader2.split()[0])
+print '  will read P=%d boundary segments ...' % P
 
 print 'reading from %s ' % nodename
 nodeheader = nodefile.readline()
@@ -63,19 +81,12 @@ for j in range(N):
   y[j] = float(nxyb[2])
   bt[j] = int(nxyb[3])
 
-print 'reading from %s ' % elename
-eleheader = elefile.readline()
-print '  header says:  ' + eleheader,
-K = (int)(eleheader.split()[0])
-print '  will read K=%d elements ...' % K
-
-print 'reading from %s ' % polyname
-polyheader1 = polyfile.readline()
-polyheader2 = polyfile.readline()
-print '  header says (line 1):  ' + polyheader1,
-print '  header says (line 2):  ' + polyheader2,
-P = (int)(polyheader2.split()[0])
-print '  will read P=%d boundary segments ...' % P
+if not polyonly:
+  print 'reading from %s ' % elename
+  eleheader = elefile.readline()
+  print '  header says:  ' + eleheader,
+  K = (int)(eleheader.split()[0])
+  print '  will read K=%d elements ...' % K
 
 print 'writing %s ...' % outname,
 
@@ -84,25 +95,33 @@ tikz.write('%%   %s\n' % commandline)
 tikz.write('%%%s\n' % '')
 tikz.write('\\begin{tikzpicture}[scale=%f]\n' % scale)
 
-for m in range(K):
-  emabc = elefile.readline()
-  if emabc == '':
-    break
-  if emabc == '\n':
-    continue
-  emabc = emabc.split()
-  if emabc[0] == '#':
-    continue
-  if m+1 != int(emabc[0]):
-    print 'ERROR: INDEXING WRONG IN READING ELEMENTS'
-  pp = [int(emabc[1])-1, int(emabc[2])-1, int(emabc[3])-1] # node indices for this element
-  kk = [0, 1, 2, 0]  # cycle through nodes
-  for k in range(3):
-    jfrom = pp[kk[k]]
-    jto   = pp[kk[k+1]]
-    tikz.write('  \\draw[gray,very thin] (%f,%f) -- (%f,%f);\n' \
-               % (x[jfrom],y[jfrom],x[jto],y[jto]))
+if not polyonly:
+  # read element file (.ele) and plot elements with light gray
+  for m in range(K):
+    emabc = elefile.readline()
+    if emabc == '':
+      break
+    if emabc == '\n':
+      continue
+    emabc = emabc.split()
+    if emabc[0] == '#':
+      continue
+    if m+1 != int(emabc[0]):
+      print 'ERROR: INDEXING WRONG IN READING ELEMENTS'
+    pp = [int(emabc[1])-1, int(emabc[2])-1, int(emabc[3])-1] # node indices for this element
+    kk = [0, 1, 2, 0]  # cycle through nodes
+    for k in range(3):
+      jfrom = pp[kk[k]]
+      jto   = pp[kk[k+1]]
+      tikz.write('  \\draw[gray,very thin] (%f,%f) -- (%f,%f);\n' \
+                 % (x[jfrom],y[jfrom],x[jto],y[jto]))
+  # plot nodes themselves
+  for j in range(N):
+    if dolabel:
+      tikz.write( '  \\draw (%f,%f) node {$%d$};\n' % (x[j]+0.7*offset,y[j]-offset,j))
+    tikz.write('  \\filldraw (%f,%f) circle (1.25pt);\n' % (x[j],y[j]))
 
+# read polygon file (.poly) and plot according to boundary type
 for p in range(P):
   pjkb = polyfile.readline()
   if pjkb == '':
@@ -124,16 +143,12 @@ for p in range(P):
   tikz.write('  \\draw[line width=%s] (%f,%f) -- (%f,%f);\n' \
              % (mywidth,x[jfrom],y[jfrom],x[jto],y[jto]))
 
-for j in range(N):
-  if dolabel:
-    tikz.write( '  \\draw (%f,%f) node {$%d$};\n' % (x[j]+0.7*offset,y[j]-offset,j))
-  tikz.write('  \\filldraw (%f,%f) circle (1.25pt);\n' % (x[j],y[j]))
-
 tikz.write('\\end{tikzpicture}\n')
+tikz.close()
 print 'done'
 
-nodefile.close()
-elefile.close()
 polyfile.close()
-tikz.close()
+nodefile.close()
+if not polyonly:
+  elefile.close()
 
