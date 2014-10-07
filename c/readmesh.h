dd6b154d23f1434de457ef78ccdf79d22ccd0563
Ed Bueler <elbueler@alaska.edu>
2014-10-07 11:12:17 -0600

    show element struct the right way, without duplication

--- c/readmesh.h
+++ c/readmesh.h
@@ -5,14 +5,16 @@
 // meshes which have either Dirichlet or Neumann boundary.  Such files are
 // written by  c2convert,  which converts from output of triangle.
 
+//STARTSTRUCT
 typedef struct {
   PetscScalar j[3],  // global indices of vertices (nodes) j[0], j[1], j[2]
               bN[3], // boundary type of node:  bN[0], bN[1], bN[2] in {0,1,2}
               bE[3], // boundary type of edge:  bE[0], bE[1], bE[2] in {0,1,2},
                      //   where bE[0] = <0,1>, bE[1] = <1,2>, bE[2] = <2,0>
-              x[3],  // node x-coordinate x[0], x[1], x[2]
-              y[3];  // node y-coordinate y[0], y[1], y[2]
+              x[3],  // node x-coordinates x[0], x[1], x[2]
+              y[3];  // node y-coordinates y[0], y[1], y[2]
 } elementtype;
+//ENDSTRUCT
 
 // get input file name from option "-f", and create corresponding viewer
 PetscErrorCode getmeshfile(MPI_Comm comm, const char suffix[], char filename[], PetscViewer *viewer);
