61f7222997143e490aa41b3bbc70b6d5f6a38404
Ed Bueler <elbueler@alaska.edu>
2014-10-10 11:40:56 -0600

    record *all* boundary segments into .poly file

--- c/structured.py
+++ c/structured.py
@@ -21,20 +21,22 @@ args = parser.parse_args()
 N    = (int)(args.N)
 h    = 1.0 / ((float)(N) - 1.0)
 
+def n(i,j):                 # computes node index from local
+    return j*N + i + 1
+
 # write .node file
 node = open(args.file + '.node', 'w')
 print 'writing file %s with %d nodes ...' % (args.file + '.node',N*N)
 node.write('%d  2  0  1\n' % (N*N))  # N^2 nodes, 2D, no attributes, one bdry marker
 for j in range(N):
     for i in range(N):
-        n = j*N + i + 1
         x = (float)(i) * h
         y = (float)(j) * h
         if (i == 0) or (i == N-1) or (j == 0) or (j == N-1):
             bdry = 2  # only Dirichlet boundary
         else:
             bdry = 0
-        node.write(' %4d  %g  %g  %4d\n' % (n, x, y, bdry))
+        node.write(' %4d  %12g  %12g  %4d\n' % (n(i,j), x, y, bdry))
 node.write('# created by: %s\n' % commandline)
 node.close()
 
@@ -45,15 +47,16 @@ print 'writing file %s with %d eles ...' % (args.file + '.ele',K)
 ele.write('%d  3  0\n' % K)  # K triangles, 3 nodes per triangle, no attributes
 for j in range(N-1):
     for i in range(N-1):
-        # write two triangles
+        # write first triangle in cell
         k = 2*(j*(N-1) + i) + 1
-        A = j*N + i + 1
-        B = A+1
-        C = (j+1)*N + i + 1
+        A = n(i,j)
+        B = A + 1
+        C = n(i,j+1)
+        # write second triangle in cell
         ele.write(' %4d  %4d  %4d  %4d\n' % (k, A, B, C))
         k += 1
         A1 = C
-        B1 = C+1
+        B1 = C + 1
         C1 = B
         ele.write(' %4d  %4d  %4d  %4d\n' % (k, A1, B1, C1))
 ele.write('# created by: %s\n' % commandline)
@@ -61,17 +64,31 @@ ele.close()
 
 # write .poly file
 poly = open(args.file + '.poly', 'w')
-print 'writing file %s ...' % (args.file + '.poly')
+M = 4*(N-1)          # N-1 boundary segments on each side
+print 'writing file %s with %d boundary segments ...' % (args.file + '.poly',M)
 bottomleft  = 1
 bottomright = N
 topright    = N*N
 topleft     = N*(N-1)+1
-poly.write('0  2  0  1\n')        # no nodes in this file
-poly.write('4  1\n')              # four boundary segments, one marker
-poly.write(' 1  %4d  %4d    2\n' % (bottomleft ,bottomright)) # sides cycle; Dirichlet = 2
-poly.write(' 2  %4d  %4d    2\n' % (bottomright,topright))
-poly.write(' 3  %4d  %4d    2\n' % (topright,   topleft))
-poly.write(' 4  %4d  %4d    2\n' % (topleft,    bottomleft))
+poly.write('0  2  0  1\n')          # no nodes in this file
+poly.write('%d  1\n' % (M))         # M boundary segments, one marker
+count = 1
+j = 0                               # bottom segments
+for i in range(N-1):
+    poly.write(' %4d  %4d  %4d    2\n' % (count,n(i,j),n(i,j)+1))
+    count += 1
+i = N-1                             # right segments
+for j in range(N-1):
+    poly.write(' %4d  %4d  %4d    2\n' % (count,n(i,j),n(i,j)+N))
+    count += 1
+j = N-1                             # top segments
+for i in range(N-1,0,-1): # count down
+    poly.write(' %4d  %4d  %4d    2\n' % (count,n(i,j),n(i,j)-1))
+    count += 1
+i = 0                               # left segments
+for j in range(N-1,0,-1): # count down
+    poly.write(' %4d  %4d  %4d    2\n' % (count,n(i,j),n(i,j)-N))
+    count += 1
 poly.write('0\n')                 # no holes
 poly.write('# created by: %s\n' % commandline)
 poly.close()
