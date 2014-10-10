f7366a4afd8a30e5aa9bc5f56bfceea34324b3ba
Ed Bueler <elbueler@alaska.edu>
2014-10-10 14:48:30 -0600

    this version does not compile: initial plan for first setting Dirichlet rows (with INSERT_VALUES) then everything else (including Dirichlet columns; with ADD_VALUES)

--- c/poissontools.c
+++ c/poissontools.c
@@ -13,7 +13,38 @@ PetscScalar chi(PetscInt q, PetscScalar xi, PetscScalar eta) {
 }
 
 
-PetscErrorCode assemble(MPI_Comm comm,
+PetscErrorCode assembledirichlet(MPI_Comm comm,
+                        Vec E,
+                        PetscScalar (*g)(PetscScalar, PetscScalar),
+                        Mat A, Vec b) {
+  PetscErrorCode ierr;  //STRIP
+  PetscInt       bs, Kstart, Kend, i;
+  PetscScalar    *ae, bval;
+  elementtype    *et;
+  ierr = VecGetBlockSize(E,&bs); CHKERRQ(ierr);
+  ierr = VecGetOwnershipRange(E,&Kstart,&Kend); CHKERRQ(ierr);
+  ierr = VecGetArray(E,&ae); CHKERRQ(ierr);
+  for (k = Kstart; k < Kend; k += bs) {    // loop through owned elements
+    et = (elementtype*)(&(ae[k-Kstart]));  // points to current element
+    // loop over vertices of current element
+    for (q = 0; q < 3; q++) {
+      if ((int)et->bN[q] == 2) {           // node is Dirichlet
+        i = (int)et->j[q];                 // global row index
+        one = 1.0;
+        ierr = MatSetValues(A,1,&i,1,&i,&one,INSERT_VALUES); CHKERRQ(ierr);
+        bval = (*g)(et->x[q],et->y[q]);
+        ierr = VecSetValues(b,1,&i,&bval,INSERT_VALUES); CHKERRQ(ierr);
+      }
+    }
+  }
+  ierr = VecRestoreArray(E,&ae); CHKERRQ(ierr);
+  matassembly(A)
+  vecassembly(b)
+  return 0;
+}
+
+
+PetscErrorCode assemblegeneral(MPI_Comm comm,
                         Vec E,
                         PetscScalar (*f)(PetscScalar, PetscScalar),
                         PetscScalar (*g)(PetscScalar, PetscScalar),
@@ -40,22 +71,15 @@ PetscErrorCode assemble(MPI_Comm comm,
     y01 = et->y[0] - et->y[1];
     x10 = et->x[1] - et->x[0];
     detJ = x10 * y20 - y01 * x02;          // note area = fabs(detJ)/2.0
+    // store element quadrature points (x,y coordinates)
+    for (r = 0; r < 3; r++) {
+      xquad[r] = et->x[0] + x10 * quadxi[r] - x02 * quadeta[r]; // = x0 + (x1-x0) xi + (x2-x0) eta
+      yquad[r] = et->x[0] - y01 * quadxi[r] + y20 * quadeta[r]; // = y0 + (y1-y0) xi + (y2-y0) eta
+    }
     // loop over vertices of current element
     for (q = 0; q < 3; q++) {
-      // compute initial element stiffness contributions (ignore Dirchlet)
       i = (int)et->j[q];                   // global row index
-      for (r = 0; r < 3; r++) {            // loop over other vertices
-        jj[r] = (int)et->j[r];             // global column index
-        vv[r] =  (dxi[q] * y20 + deta[q] * y01) * (dxi[r] * y20 + deta[r] * y01);
-        vv[r] += (dxi[q] * x02 + deta[q] * x10) * (dxi[r] * x02 + deta[r] * x10);
-        vv[r] /= 2.0 * fabs(detJ);
-      }
-      ierr = MatSetValues(A,1,&i,3,jj,vv,ADD_VALUES); CHKERRQ(ierr);
-      // store element quadrature points (x,y coordinates)
-      for (r = 0; r < 3; r++) {
-        xquad[r] = et->x[0] + x10 * quadxi[r] - x02 * quadeta[r]; // = x0 + (x1-x0) xi + (x2-x0) eta
-        yquad[r] = et->x[0] - y01 * quadxi[r] + y20 * quadeta[r]; // = y0 + (y1-y0) xi + (y2-y0) eta
-      }
+      FIXME: check if Dirichlet; if so skip it!
       // compute element RHS contribution from source f
       bval = 0.0;
       if (f) {
@@ -78,10 +102,37 @@ PetscErrorCode assemble(MPI_Comm comm,
         }
       }
       ierr = VecSetValues(b,1,&i,&bval,ADD_VALUES); CHKERRQ(ierr);
+      // compute initial element stiffness contributions (ignore Dirchlet)
+      for (r = 0; r < 3; r++) {            // loop over other vertices
+        jj[r] = (int)et->j[r];             // global column index
+        vv[r] =  (dxi[q] * y20 + deta[q] * y01) * (dxi[r] * y20 + deta[r] * y01);
+        vv[r] += (dxi[q] * x02 + deta[q] * x10) * (dxi[r] * x02 + deta[r] * x10);
+        vv[r] /= 2.0 * fabs(detJ);
+      }
+      // edit according to Dirchlet info
+      if ((g) && ((int)et->bN[q] == 2)) {
+        FIXME
+      } else {
+        for (r = 0; r < 3; r++) {            // loop over other vertices
+        
+        if (g) &&
+        ierr = MatSetValues(A,1,&i,3,jj,vv,ADD_VALUES); CHKERRQ(ierr);
+      }
     }
   }
   ierr = VecRestoreArray(E,&ae); CHKERRQ(ierr);
   matassembly(A)
   vecassembly(b)
   return 0;
+}
+
+
+
+PetscErrorCode assemble(MPI_Comm comm,
+                        Vec E,
+                        PetscScalar (*f)(PetscScalar, PetscScalar),
+                        PetscScalar (*g)(PetscScalar, PetscScalar),
+                        PetscScalar (*gamma)(PetscScalar, PetscScalar),
+                        Mat A, Vec b) {
+  FIXME: call assembledirichlet() then assemblegeneral()
 }
