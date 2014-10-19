6ddbd89bb7180af6e98ab793323ab8b1fd8a4499
Ed Bueler <elbueler@alaska.edu>
2014-10-18 21:58:52 -0600

    rewrote c2heat.m; probably correct, but does funny things with -ts_type beuler; see bug report 10/18/2014 to petsc-maint@mcs.anl.gov

--- c/c2heat.c
+++ c/c2heat.c
@@ -1,90 +1,40 @@
 
 static char help[] = "Time-dependent heat equation in 2d.\n";
 
+/*
+  Solves heat equation
+    u_t = u_xx + u_yy
+  on the unit square S = (0,1) x (0,1) with homogeneous Dirichlet
+  with initial condition
+    u(0,x,y) = sin(pi x) sin(pi y) + sin(3 pi x) sin(2 pi y)
+
+  Exact solution is
+    u(t,x,y) = e^{- pi^2 t} sin(pi x) sin(pi y)
+                 + e^{- (3^2 + 2^2) pi^2 t} sin(3 pi x) sin(2 pi y)
+
+For examples, try
+
+  ./c2heat
+  ./c2heat -ts_type euler                           # same as above
+  ./c2heat -da_grid_x 20 -da_grid_y 20              # higher res
+  ./c2heat -da_grid_x 100 -da_grid_y 100            # higher; Euler failure
+  ./c2heat -da_grid_x 100 -da_grid_y 100 -steps      # higher; Euler failure
+  ./c2heat -ts_type beuler                          #FIXME: see 10/18/2014 bug report
+  ./c2heat -ts_monitor_draw_solution -draw_pause 1
+
+*/
+
 #include <petscdmda.h>
 #include <petscts.h>
 #include "structuredlaplacian.h"
-
-extern PetscErrorCode FormRHSFunction(TS, PetscReal, Vec, Vec, void*);
-extern PetscErrorCode FormSolutionAtTime(DM, PetscReal, Vec);
-extern PetscErrorCode MyTSMonitor(TS, PetscInt, PetscReal, Vec, void*);
-
-int main(int argc,char **argv)
-{
-  TS             ts;                  // time integrator
-  DM             da;                  // grid topology
-  Vec            u;                   // solution vector
-  PetscInt       maxsteps = 10;
-  PetscReal      t0 = 0.0, tf = 0.01;
-  PetscErrorCode ierr;
-
-  PetscInitialize(&argc,&argv,(char*)0,help);
-
-  ierr = DMDACreate2d(PETSC_COMM_WORLD, DM_BOUNDARY_NONE, DM_BOUNDARY_NONE,
-                DMDA_STENCIL_STAR,-5,-5,PETSC_DECIDE,PETSC_DECIDE,1,1,NULL,NULL,
-                &da); CHKERRQ(ierr);
-  ierr = DMCreateGlobalVector(da,&u);CHKERRQ(ierr);
-
-  Mat  A;
-  ierr = DMSetMatType(da,MATAIJ);CHKERRQ(ierr);
-  ierr = DMCreateMatrix(da,&A);CHKERRQ(ierr);
-  ierr = MatSetOptionsPrefix(A,"a_"); CHKERRQ(ierr);
-  ierr = formlaplacian(da,A); CHKERRQ(ierr);
-
-  ierr = TSCreate(PETSC_COMM_WORLD,&ts);CHKERRQ(ierr);
-  ierr = TSSetProblemType(ts,TS_LINEAR);CHKERRQ(ierr);
-
-  ierr = TSSetRHSFunction(ts,NULL,FormRHSFunction,NULL);CHKERRQ(ierr);
-  ierr = TSSetRHSJacobian(ts,A,A,TSComputeRHSJacobianConstant,NULL);CHKERRQ(ierr);
-
-  ierr = TSMonitorSet(ts,MyTSMonitor,0,0);CHKERRQ(ierr);
-  ierr = TSSetDM(ts,da);CHKERRQ(ierr);
-  ierr = TSSetType(ts,TSBEULER);CHKERRQ(ierr);
-
-  ierr = FormSolutionAtTime(da,t0,u);CHKERRQ(ierr);
-  ierr = TSSetInitialTimeStep(ts,t0,(tf-t0)/(double)maxsteps);CHKERRQ(ierr);
-  ierr = TSSetSolution(ts,u);CHKERRQ(ierr);
-  ierr = TSSetDuration(ts,maxsteps,tf);CHKERRQ(ierr);
-
-  ierr = TSSetFromOptions(ts);CHKERRQ(ierr);
-
-  ierr = TSSolve(ts,u);CHKERRQ(ierr);
-
-  ierr = VecDestroy(&u);CHKERRQ(ierr);
-  ierr = MatDestroy(&A);CHKERRQ(ierr);
-  ierr = TSDestroy(&ts);CHKERRQ(ierr);
-  ierr = DMDestroy(&da);CHKERRQ(ierr);
-  ierr = PetscFinalize();
-  PetscFunctionReturn(0);
-}
-
-
-// since ODE sys is  du/dt = A u,  just compute "G(t,u) = A u"
-// FIXME: would like more generic  G(t,u) = a(t,x) A u + f(t,x) ??
-PetscErrorCode FormRHSFunction(TS ts, PetscReal t, Vec X, Vec G, void *ptr) {
-  PetscErrorCode ierr;
-  Mat            A;
-  PetscReal      hx,hy;
-  DM             da;
-  DMDALocalInfo  info;
-
-  PetscFunctionBeginUser;
-  ierr = TSGetDM(ts,&da);CHKERRQ(ierr);
-  ierr = DMDAGetLocalInfo(da,&info);CHKERRQ(ierr);
-  hx = 1.0/(PetscReal)(info.mx-1);
-  hy = 1.0/(PetscReal)(info.my-1);
-  ierr = TSGetRHSJacobian(ts,&A,NULL,NULL,NULL); CHKERRQ(ierr);
-  ierr = MatMult(A,X,G); CHKERRQ(ierr);
-  ierr = VecScale(G,-1.0/(hx*hy)); CHKERRQ(ierr);
-  PetscFunctionReturn(0);
-}
 
 
 // use both for initial condition at t=0 and exact solution at t=tf
 PetscErrorCode FormSolutionAtTime(DM da, PetscReal t, Vec U) {
   PetscErrorCode ierr;
   PetscInt       i,j;
-  PetscReal      **u, pi = PETSC_PI, hx,hy,x,y,
+  PetscReal      **u,hx,hy,x,y,
+                 pi = PETSC_PI,
                  c1 = 1.0, K1 = 1.0, L1 = 1.0,
                  c2 = 1.0, K2 = 3.0, L2 = 2.0;
   DMDALocalInfo  info;
@@ -97,9 +47,13 @@ PetscErrorCode FormSolutionAtTime(DM da, PetscReal t, Vec U) {
   for (j=info.ys; j<info.ys+info.ym; j++) {
     y = j*hy;
     for (i=info.xs; i<info.xs+info.xm; i++) {
-      x = i*hx;
-      u[j][i] =  c1 * PetscExpReal(-(K1*K1+L1*L1)*pi*pi * t) * sin(K1*pi*x) * sin(L1*pi*y);
-      u[j][i] += c2 * PetscExpReal(-(K2*K2+L2*L2)*pi*pi * t) * sin(K2*pi*x) * sin(L2*pi*y);
+      if ( (i == 0) || (i == info.mx-1) || (j == 0) || (j == info.my-1) )
+        u[j][i] = 0.0;
+      else {
+        x = i*hx;
+        u[j][i] =  c1 * PetscExpReal(-(K1*K1+L1*L1)*pi*pi * t) * sin(K1*pi*x) * sin(L1*pi*y);
+        u[j][i] += c2 * PetscExpReal(-(K2*K2+L2*L2)*pi*pi * t) * sin(K2*pi*x) * sin(L2*pi*y);
+      }
     }
   }
   ierr = DMDAVecRestoreArray(da,U,&u);CHKERRQ(ierr);
@@ -107,8 +61,7 @@ PetscErrorCode FormSolutionAtTime(DM da, PetscReal t, Vec U) {
 }
 
 
-PetscErrorCode MyTSMonitor(TS ts,PetscInt step,PetscReal ptime,Vec v,void *ctx)
-{
+PetscErrorCode MyTSMonitor(TS ts,PetscInt step,PetscReal t,Vec v,void *ctx) {
   PetscErrorCode ierr;
   PetscReal      norm;
   MPI_Comm       comm;
@@ -116,7 +69,90 @@ PetscErrorCode MyTSMonitor(TS ts,PetscInt step,PetscReal ptime,Vec v,void *ctx)
   PetscFunctionBeginUser;
   ierr = VecNorm(v,NORM_INFINITY,&norm);CHKERRQ(ierr);
   ierr = PetscObjectGetComm((PetscObject)ts,&comm);CHKERRQ(ierr);
-  ierr = PetscPrintf(comm,"t = %8g:   max |u(t,x,y)| = %g\n",ptime,norm);CHKERRQ(ierr);
+//  ierr = PetscPrintf(comm,"t = %10g:   max |u(t,x,y)| = %g\n",t,norm);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+
+int main(int argc,char **argv) {
+  TS             ts;                  // time integrator
+  DM             da;                  // grid topology
+  Vec            u, uexact;           // numerical and exact solution vectors
+  PetscInt       steps = 10;
+  PetscReal      t0 = 0.0, tf = 0.01, errnorm;
+  PetscErrorCode ierr;
+
+  PetscInitialize(&argc,&argv,(char*)0,help);
+  ierr = PetscOptionsBegin(PETSC_COMM_WORLD, "", "options for c2heat", ""); CHKERRQ(ierr);
+  ierr = PetscOptionsInt("-steps",
+                         "choose number of requested time steps\n", "", steps,
+                         &steps, NULL); CHKERRQ(ierr);
+  ierr = PetscOptionsEnd(); CHKERRQ(ierr);
+
+  // CREATE GRID
+  ierr = DMDACreate2d(PETSC_COMM_WORLD, DM_BOUNDARY_NONE, DM_BOUNDARY_NONE,
+                DMDA_STENCIL_STAR,-5,-5,PETSC_DECIDE,PETSC_DECIDE,1,1,NULL,NULL,
+                &da); CHKERRQ(ierr);
+  ierr = DMCreateGlobalVector(da,&u);CHKERRQ(ierr);
+  ierr = VecDuplicate(u,&uexact);CHKERRQ(ierr);
+
+  // CREATE A, CHANGING SCALING RELATIVE TO POISSON PROBLEM
+  Mat  A;
+  PetscReal      hx,hy,dtdefault;
+  DMDALocalInfo  info;
+  ierr = DMSetMatType(da,MATAIJ);CHKERRQ(ierr);
+  ierr = DMCreateMatrix(da,&A);CHKERRQ(ierr);
+  ierr = MatSetOptionsPrefix(A,"a_"); CHKERRQ(ierr);
+  ierr = formdirichletlaplacian(da,0.0,A); CHKERRQ(ierr);
+  ierr = DMDAGetLocalInfo(da,&info);CHKERRQ(ierr);
+  hx = 1.0/(PetscReal)(info.mx-1);
+  hy = 1.0/(PetscReal)(info.my-1);
+  ierr = MatScale(A,-1.0/(hx*hy)); CHKERRQ(ierr);
+
+  // CREATE AND SETUP TS
+  ierr = TSCreate(PETSC_COMM_WORLD,&ts);CHKERRQ(ierr);
+  ierr = TSSetProblemType(ts,TS_LINEAR);CHKERRQ(ierr);
+  ierr = TSSetRHSFunction(ts,NULL,TSComputeRHSFunctionLinear,NULL);CHKERRQ(ierr);
+  ierr = TSSetRHSJacobian(ts,A,A,TSComputeRHSJacobianConstant,NULL);CHKERRQ(ierr);
+  ierr = TSMonitorSet(ts,MyTSMonitor,0,0);CHKERRQ(ierr);
+  ierr = TSSetDM(ts,da);CHKERRQ(ierr);
+  ierr = TSSetExactFinalTime(ts,TS_EXACTFINALTIME_MATCHSTEP);CHKERRQ(ierr);
+
+  // SET INITIAL CONDITION AND AS FOR DURATION
+  ierr = FormSolutionAtTime(da,t0,u);CHKERRQ(ierr);
+  dtdefault = (tf-t0) / (double)steps;
+  ierr = TSSetInitialTimeStep(ts,t0,dtdefault);CHKERRQ(ierr);
+  ierr = TSSetSolution(ts,u);CHKERRQ(ierr);
+  ierr = TSSetDuration(ts,10*steps,tf);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,
+             "on %4d x %4d grid, from t0 = %g to tf = %g, with dt0 = %f:\n",
+             info.mx,info.my,t0,tf,dtdefault); CHKERRQ(ierr);
+  //FIXME: remove this when -ts_type beuler works
+  PetscReal dtEuler;
+  dtEuler = 1.0/(hx*hx) + 1.0/(hy*hy);
+  dtEuler = 1.0 / (2.0 * dtEuler);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,
+             "stability limit for Euler:  dt = %f:\n",
+             dtEuler); CHKERRQ(ierr);
+
+  // SOLVE (USING USER'S OPTIONS)
+  ierr = TSSetFromOptions(ts);CHKERRQ(ierr);
+  ierr = TSSolve(ts,u);CHKERRQ(ierr);
+
+  // show result:
+  ierr = TSGetSolveTime(ts,&tf);CHKERRQ(ierr);
+  ierr = FormSolutionAtTime(da,tf,uexact);CHKERRQ(ierr);
+  ierr = VecAXPY(u,-1.0,uexact); CHKERRQ(ierr);    // u <- u + (-1.0) uxact
+  ierr = VecNorm(u,NORM_INFINITY,&errnorm); CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,
+             "result at t = %g:     error |u-uexact|_inf = %g\n",
+             tf,errnorm); CHKERRQ(ierr);
+
+  ierr = VecDestroy(&u);CHKERRQ(ierr);
+  ierr = MatDestroy(&A);CHKERRQ(ierr);
+  ierr = TSDestroy(&ts);CHKERRQ(ierr);
+  ierr = DMDestroy(&da);CHKERRQ(ierr);
+  ierr = PetscFinalize();
   PetscFunctionReturn(0);
 }
 
