5bb461a1603e68604c6cd631d3700679be598ff3
Ed Bueler <elbueler@alaska.edu>
2014-11-06 20:43:21 -0700

    simplify c1e.c a bit ...

--- c/c1e.c
+++ c/c1e.c
@@ -6,12 +6,10 @@ int main(int argc,char **args) {
   PetscErrorCode  ierr;
   PetscMPIInt     rank;
   PetscScalar     localval, globalsum;
-  PetscLogDouble  flops;
   int             i;
 
   PetscInitialize(&argc,&args,(char*)0,help);  // <-- always call
 
-  // get my rank
   ierr = MPI_Comm_rank(PETSC_COMM_WORLD,&rank); CHKERRQ(ierr);
 
   // compute  1 / n!  where n = (one more than rank of process)
@@ -19,10 +17,6 @@ int main(int argc,char **args) {
   for (i = 1; i < rank; i++)
     localval *= i+1;
   localval = 1.0 / localval;
-  // record the flops we just did, for later reporting
-  if (rank > 0) {
-    ierr = PetscLogFlops(2 * (rank - 1) + 1); CHKERRQ(ierr);
-  }
 
   // sum the contributions over all processes
   ierr = MPI_Allreduce(&localval, &globalsum, 1, MPI_DOUBLE, MPI_SUM,
@@ -32,10 +26,9 @@ int main(int argc,char **args) {
   ierr = PetscPrintf(PETSC_COMM_WORLD,
                      "e is about %17.15f\n",globalsum); CHKERRQ(ierr);
 
-  // output report on work done on each rank
-  ierr = PetscGetFlops(&flops); CHKERRQ(ierr);
+  // from each process, output report on work done
   ierr = PetscPrintf(PETSC_COMM_SELF,
-                     "rank %d did %d flops\n",rank,(int)flops); CHKERRQ(ierr);
+                     "rank %d did %d flops\n",rank,2 * rank + 1); CHKERRQ(ierr);
 
   PetscFinalize();  // <-- always call
   return 0;
