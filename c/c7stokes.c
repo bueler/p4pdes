1755883ae2053d1e041aaf2d360ff762864592b6
Ed Bueler <elbueler@alaska.edu>
2014-11-09 15:37:09 -0700

    add an application context

--- c/c7stokes.c
+++ c/c7stokes.c
@@ -11,36 +11,42 @@ typedef struct {
   PetscReal p;
 } Field;
 
+typedef struct {
+  DM        da;
+  PetscInt  dof;  // number of degrees of freedom at each node
+  PetscReal L,    // length of domain in x direction
+            H;    // length of domain in y direction
+} AppCtx;
 
-extern PetscErrorCode FormFunctionLocal(DMDALocalInfo*,PetscScalar**,PetscScalar**,void*);
-//extern PetscErrorCode FormJacobianLocal(DMDALocalInfo*,PetscScalar**,Mat,Mat,AppCtx*);
+extern PetscErrorCode FormFunctionLocal(DMDALocalInfo*,Field**,Field**,AppCtx*);
+//extern PetscErrorCode FormJacobianLocal(DMDALocalInfo*,Field**,Mat,Mat,AppCtx*);
 
 int main(int argc,char **argv)
 {
   PetscErrorCode ierr;
-  DM             da;
+  AppCtx         user;                         /* user-defined work context */
   SNES           snes;                         /* nonlinear solver */
   Vec            x;                            /* solution vector */
-  //AppCtx         user;                         /* user-defined work context */
   PetscInt       its;                          /* iterations for convergence */
-  PetscReal      H, L;
 
   PetscInitialize(&argc,&argv,(char*)0,help);
 
   ierr = SNESCreate(PETSC_COMM_WORLD,&snes);CHKERRQ(ierr);
 
+  user.dof = 3;
+  user.L   = 10.0;
+  user.H   = 1.0;
   ierr = DMDACreate2d(PETSC_COMM_WORLD,
                       DM_BOUNDARY_NONE, DM_BOUNDARY_NONE, DMDA_STENCIL_STAR,
-                      -4,-4,
-                      PETSC_DECIDE,PETSC_DECIDE,
-                      1,1,NULL,NULL,&da); CHKERRQ(ierr);
-  ierr = DMDASetUniformCoordinates(da, 0.0, H, 0.0, L, -1.0, -1.0);CHKERRQ(ierr);
-  //ierr = DMSetApplicationContext(da,&user);CHKERRQ(ierr);
-  ierr = DMCreateGlobalVector(da,&x);CHKERRQ(ierr);
+                      -4,-4,PETSC_DECIDE,PETSC_DECIDE,
+                      user.dof,1,NULL,NULL,&user.da); CHKERRQ(ierr);
+  ierr = DMDASetUniformCoordinates(user.da, 0.0, user.H, 0.0, user.L, -1.0, -1.0);CHKERRQ(ierr);
+  ierr = DMSetApplicationContext(user.da,&user);CHKERRQ(ierr);
+  ierr = DMCreateGlobalVector(user.da,&x);CHKERRQ(ierr);
 
-  ierr = SNESSetDM(snes,da);CHKERRQ(ierr);
-  ierr = DMDASNESSetFunctionLocal(da,INSERT_VALUES,
-                                  (DMDASNESFunction)FormFunctionLocal,NULL);CHKERRQ(ierr);
+  ierr = SNESSetDM(snes,user.da);CHKERRQ(ierr);
+  ierr = DMDASNESSetFunctionLocal(user.da,INSERT_VALUES,
+                                  (DMDASNESFunction)FormFunctionLocal,&user);CHKERRQ(ierr);
 
   ierr = SNESSetFromOptions(snes);CHKERRQ(ierr);
 
@@ -51,7 +57,7 @@ int main(int argc,char **argv)
 
   ierr = VecDestroy(&x);CHKERRQ(ierr);
   ierr = SNESDestroy(&snes);CHKERRQ(ierr);
-  ierr = DMDestroy(&da);CHKERRQ(ierr);
+  ierr = DMDestroy(&user.da);CHKERRQ(ierr);
   ierr = PetscFinalize();
   return 0;
 }
