54e4059ee6eeafec8799b717259143eab3f16e1b
Ed Bueler <elbueler@alaska.edu>
2014-11-12 11:45:15 -0700

    add exact solution (as initial) and work on FD formulas; note FIXMEs remain

--- c/c7stokes.c
+++ c/c7stokes.c
@@ -2,7 +2,8 @@
 static char help[] =
 "Solves a structured-grid Stokes problem with DMDA and KSP using finite differences.\n"
 "Domain is rectangle with stress-free top, zero-velocity bottom, and\n"
-"periodic b.c.s in x.  System matrix is made symmetric by extracting divergence\n"
+"periodic b.c.s in x.  Exact solution is known.\n"
+"System matrix is made symmetric by extracting divergence\n"
 "approximation for incompressibility equation from gradient approximation in\n"
 "stress-balance equations.  Pressure-poisson equation allows adding Laplacian\n"
 "of pressure into incompressibility equations.\n"
@@ -23,11 +24,13 @@ typedef struct {
   PetscInt  dof;   // number of degrees of freedom at each node
   PetscReal L,     // length of domain in x direction
             H,     // length of domain in y direction
-            g,     // component of gravity in downward y-direction (positive)
-            gamma, // component of gravity in rightward x-direction (positive)
+            g1,    // signed component of gravity in x-direction
+            g2,    // signed component of gravity in y-direction
+            nu,    // viscosity
             ppeps; // amount of Laplacian of pressure to add to incompressibility
 } AppCtx;
 
+extern PetscErrorCode FormInitialGuess(Vec,AppCtx*);
 extern PetscErrorCode FormFunctionLocal(DMDALocalInfo*,Field**,Field**,AppCtx*);
 //extern PetscErrorCode FormJacobianLocal(DMDALocalInfo*,Field**,Mat,Mat,AppCtx*);
 
@@ -43,33 +46,58 @@ int main(int argc,char **argv)
   user.dof   = 3;
   user.L     = 10.0;
   user.H     = 1.0;
-  user.g     = 9.0;
-  user.gamma = 1.0;
+  user.g1    = 1.0;
+  user.g2    = -9.0;
+  user.nu    = 1.0;
   user.ppeps = 1.0;
 
   ierr = DMDACreate2d(PETSC_COMM_WORLD,
                       DM_BOUNDARY_PERIODIC, DM_BOUNDARY_NONE, DMDA_STENCIL_STAR,
                       -4,-4,PETSC_DECIDE,PETSC_DECIDE,
                       user.dof,1,NULL,NULL,&user.da); CHKERRQ(ierr);
-  ierr = DMDASetUniformCoordinates(user.da, 0.0, user.H, 0.0, user.L, -1.0, -1.0);CHKERRQ(ierr);
-  ierr = DMSetApplicationContext(user.da,&user);CHKERRQ(ierr);
-  ierr = DMCreateGlobalVector(user.da,&x);CHKERRQ(ierr);
+  ierr = DMDASetUniformCoordinates(user.da, 0.0, user.H, 0.0, user.L, -1.0, -1.0); CHKERRQ(ierr);
+  ierr = DMSetApplicationContext(user.da,&user); CHKERRQ(ierr);
+  ierr = DMCreateGlobalVector(user.da,&x); CHKERRQ(ierr);
 
-  ierr = SNESCreate(PETSC_COMM_WORLD,&snes);CHKERRQ(ierr);
-  ierr = SNESSetDM(snes,user.da);CHKERRQ(ierr);
+  ierr = SNESCreate(PETSC_COMM_WORLD,&snes); CHKERRQ(ierr);
+  ierr = SNESSetDM(snes,user.da); CHKERRQ(ierr);
   ierr = DMDASNESSetFunctionLocal(user.da,INSERT_VALUES,
-                                  (DMDASNESFunction)FormFunctionLocal,&user);CHKERRQ(ierr);
-  ierr = SNESSetFromOptions(snes);CHKERRQ(ierr);
+                                  (DMDASNESFunction)FormFunctionLocal,&user); CHKERRQ(ierr);
+  ierr = SNESSetFromOptions(snes); CHKERRQ(ierr);
 
-  //ierr = FormInitialGuess(da,&user,x);CHKERRQ(ierr);
-  ierr = VecSet(x,0.0);CHKERRQ(ierr);
+  ierr = FormInitialGuess(da,x,&user); CHKERRQ(ierr);
+  //ierr = VecSet(x,0.0); CHKERRQ(ierr);
 
-  ierr = SNESSolve(snes,NULL,x);CHKERRQ(ierr);
+  ierr = SNESSolve(snes,NULL,x); CHKERRQ(ierr);
 
-  ierr = VecDestroy(&x);CHKERRQ(ierr);
-  ierr = SNESDestroy(&snes);CHKERRQ(ierr);
-  ierr = DMDestroy(&user.da);CHKERRQ(ierr);
+  ierr = VecDestroy(&x); CHKERRQ(ierr);
+  ierr = SNESDestroy(&snes); CHKERRQ(ierr);
+  ierr = DMDestroy(&user.da); CHKERRQ(ierr);
   ierr = PetscFinalize();
+  return 0;
+}
+
+
+// FIXME: this version only sets x to the exact solution
+PetscErrorCode FormInitialGuess(Vec x, AppCtx* user) {
+{
+  PetscErrorCode ierr;
+  DMDALocalInfo *info;
+  PetscReal      **ax, hy, y;
+
+  ierr = DMDAGetLocalInfo(user->da,&info); CHKERRQ(ierr);
+  hy = user->H / (double)(info.my - 1);
+  ierr = DMGetCoordinates(user->da,&xycoords); CHKERRQ(ierr);
+  ierr = VecGetArray(x,&ax); CHKERRQ(ierr);
+  for (j = info.ys; j < info.ys+info.ym-1; j++) {
+    y = hy * (double) j;
+    for (i = info.xs; i < info.xs+info.xm-1; i++) {
+      ax[j][i].u = (user->g1 / user->nu) * y * (user->H - y/2.0);
+      ax[j][i].v = 0.0;
+      ax[j][i].p = - user->g2 * (user->H - y);
+    }
+  }
+  ierr = VecRestoreArray(x,&ax); CHKERRQ(ierr);
   return 0;
 }
 
@@ -78,7 +106,9 @@ PetscErrorCode FormFunctionLocal(DMDALocalInfo *info, Field **x, Field **f, AppC
 {
   Field          uLocal[3];
   PetscInt       i,j,k,l;
-  PetscReal      hx, hy, uUP, vUP;
+  PetscReal      hx, hy, uUP, vUP,
+                 H = user->H, g1 = user->g1, g2 = user->g2,
+                 nu = user->nu, eps = user->ppeps;
   PetscErrorCode ierr;
 
   PetscFunctionBeginUser;
@@ -87,7 +117,8 @@ PetscErrorCode FormFunctionLocal(DMDALocalInfo *info, Field **x, Field **f, AppC
   hy = user->H / (PetscReal)(info->my-1);  // non-periodic
 
   // zero-out locally-owned f;  from snes/examples/tutorials/ex7.c
-  ierr = PetscMemzero((void*) &(f[info->xs][info->ys]), info->xm*info->ym*sizeof(Field));CHKERRQ(ierr);
+  ierr = PetscMemzero((void*) &(f[info->xs][info->ys]),
+                      info->xm*info->ym*sizeof(Field)); CHKERRQ(ierr);
 
   for (j = info->ys; j < info->ys+info->ym-1; j++) {
     for (i = info->xs; i < info->xs+info->xm-1; i++) {
@@ -95,7 +126,7 @@ PetscErrorCode FormFunctionLocal(DMDALocalInfo *info, Field **x, Field **f, AppC
         // Dirichlet conditions at bottom
         f[j][i].u = x[j][i].u;
         f[j][i].v = x[j][i].v;
-        f[j][i].p = x[j][i].p - user->g * user->H;
+        f[j][i].p = x[j][i].p + g2 * H;
       } else if (j == info.my-1) {
         // stress-free, and pressure zero, conditions at top
         uUP = FIXME;
@@ -105,24 +136,24 @@ PetscErrorCode FormFunctionLocal(DMDALocalInfo *info, Field **x, Field **f, AppC
         f[j][i].p = x[j][i].p;
       } else {
         // FD eqn I:
-        f[j][i].u =   (x[j][i+1].u - 2.0 * x[j][i].u + x[j][i-1].u) / (hx*hx)
-                    + (x[j+1][i].u - 2.0 * x[j][i].u + x[j+1][i].u) / (hy*hy)
-                    - (x[j][i+1].p - x[j][i-1].p) / (2.0*hx)
-                    + user->gamma;
+        f[j][i].u = - nu * (x[j][i+1].u - 2.0 * x[j][i].u + x[j][i-1].u) / (hx*hx)
+                    - nu * (x[j+1][i].u - 2.0 * x[j][i].u + x[j+1][i].u) / (hy*hy)
+                    + (x[j][i+1].p - x[j][i-1].p) / (2.0*hx)
+                    - g1;
         // FD eqn II:
-        f[j][i].v =   (x[j][i+1].v - 2.0 * x[j][i].v + x[j][i-1].v) / (hx*hx)
-                    + (x[j+1][i].v - 2.0 * x[j][i].v + x[j+1][i].v) / (hy*hy)
-                    - (x[j+1][i].p - x[j-1][i].p) / (2.0*hy)
-                    - user->g;
+        f[j][i].v = - nu * (x[j][i+1].v - 2.0 * x[j][i].v + x[j][i-1].v) / (hx*hx)
+                    - nu * (x[j+1][i].v - 2.0 * x[j][i].v + x[j+1][i].v) / (hy*hy)
+                    + (x[j+1][i].p - x[j-1][i].p) / (2.0*hy)
+                    - g2;
         // FD eqn III:
-        f[j][i].p = - (x[j][i+1].u - x[j][i-1].u) / (2.0*hx)
-                    - (x[j+1][i].u - x[j-1][i].u) / (2.0*hy)
-                    + user->ppeps * (
+        f[j][i].p =   (x[j][i+1].u - x[j][i-1].u) / (2.0*hx)
+                    + (x[j+1][i].v - x[j-1][i].v) / (2.0*hy)
+                    - eps * (
                         (x[j][i+1].p - 2.0 * x[j][i].p + x[j][i-1].p) / (hx*hx)
                       + (x[j+1][i].p - 2.0 * x[j][i].p + x[j+1][i].p) / (hy*hy) );
     }
   }
 
-  //ierr = PetscLogFlops(68.0*(info->ym-1)*(info->xm-1));CHKERRQ(ierr);
+  //ierr = PetscLogFlops(68.0*(info->ym-1)*(info->xm-1)); CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
