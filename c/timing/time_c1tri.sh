5df835a68e10d6df9eb8591e23a87734051e5472
Ed Bueler <elbueler@alaska.edu>
2015-05-30 12:20:52 -0600

    In progress stuff.

--- c/timing/time_c1tri.sh
+++ c/timing/time_c1tri.sh
@@ -11,16 +11,19 @@
 
 function run() {
   rm -f tmp
-  /usr/bin/time -f "%e" mpiexec -n $1 ../c1tri -tri_m 10000000 $2 &> tmp
-  cat tmp |tail -n 1 &> $3
+  /usr/bin/time -f "%e" mpiexec -n $1 ../c1tri -tri_m 10000000 -ksp_type $2 -pc_type $3 &> tmp
+  cat tmp |tail -n 1 &> c1tri/$2.$3.$1
+  rm tmp
 }
 
 set -x
 
 for N in 1 4; do
-  for PC in ilu none jacobi; do
-  rm -f tmp
-  run $N '-ksp_type preonly -pc_type none' c1tri/preonly.lu.$N
+  run $N preonly lu
+  for KSP in gmres cg; do
+    for PC in ilu none jacobi; do
+    FIXME
+    done
   done
 done
 
