ad4b819e1f60511982d99c1d7135f816cdb2db78
Ed Bueler <elbueler@alaska.edu>
2015-05-30 09:47:59 -0600

    Update to -tri_m option.

--- c/c1tri.c
+++ c/c1tri.c
@@ -8,7 +8,7 @@ int main(int argc,char **args) {
   Vec            x, b, xexact;
   Mat            A;
   KSP            ksp;
-  PetscInt       N = 4, i, Istart, Iend, j[3];
+  PetscInt       m = 4, i, Istart, Iend, j[3];
   PetscReal      v[3], xval, errnorm;
   PetscErrorCode ierr;
 
@@ -17,18 +17,18 @@ int main(int argc,char **args) {
   ierr = PetscOptionsBegin(PETSC_COMM_WORLD,
         "tri_","options for c1tri",""); CHKERRQ(ierr);
   ierr = PetscOptionsInt(
-        "-n","dimension of linear system",NULL,N,&N,NULL); CHKERRQ(ierr);
+        "-m","dimension of linear system",NULL,m,&m,NULL); CHKERRQ(ierr);
   ierr = PetscOptionsEnd(); CHKERRQ(ierr);
 
   ierr = VecCreate(PETSC_COMM_WORLD,&x); CHKERRQ(ierr);
-  ierr = VecSetSizes(x,PETSC_DECIDE,N); CHKERRQ(ierr);
+  ierr = VecSetSizes(x,PETSC_DECIDE,m); CHKERRQ(ierr);
   ierr = VecSetFromOptions(x); CHKERRQ(ierr);
 
   ierr = VecDuplicate(x,&b); CHKERRQ(ierr);
   ierr = VecDuplicate(x,&xexact); CHKERRQ(ierr);
 
   ierr = MatCreate(PETSC_COMM_WORLD,&A); CHKERRQ(ierr);
-  ierr = MatSetSizes(A,PETSC_DECIDE,PETSC_DECIDE,N,N); CHKERRQ(ierr);
+  ierr = MatSetSizes(A,PETSC_DECIDE,PETSC_DECIDE,m,m); CHKERRQ(ierr);
   ierr = MatSetOptionsPrefix(A,"a_"); CHKERRQ(ierr);
   ierr = MatSetFromOptions(A); CHKERRQ(ierr);
   ierr = MatSetUp(A); CHKERRQ(ierr);
@@ -42,7 +42,7 @@ int main(int argc,char **args) {
     } else {
       v[0] = -1.0;  v[1] = 3.0;  v[2] = -1.0;
       j[0] = i-1;   j[1] = i;    j[2] = i+1;
-      if (i == N-1) {
+      if (i == m-1) {
         ierr = MatSetValues(A,1,&i,2,j,v,INSERT_VALUES); CHKERRQ(ierr);
       } else {
         ierr = MatSetValues(A,1,&i,3,j,v,INSERT_VALUES); CHKERRQ(ierr);
@@ -65,7 +65,7 @@ int main(int argc,char **args) {
   ierr = VecAXPY(x,-1.0,xexact); CHKERRQ(ierr);
   ierr = VecNorm(x,NORM_2,&errnorm); CHKERRQ(ierr);
   ierr = PetscPrintf(PETSC_COMM_WORLD,
-        "error norm |x-xexact|_2 = %.1e\n",errnorm); CHKERRQ(ierr);
+        "error for m = %d system is |x-xexact|_2 = %.1e\n",m,errnorm); CHKERRQ(ierr);
 
   KSPDestroy(&ksp);  MatDestroy(&A);
   VecDestroy(&x);  VecDestroy(&b);  VecDestroy(&xexact);
