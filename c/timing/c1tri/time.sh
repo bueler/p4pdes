e04385f843f49e682f00e9611f68b301716087d0
Ed Bueler <elbueler@alaska.edu>
2015-05-31 10:26:32 -0600

    Chapter 1 is done for now.  Move on.

--- c/timing/c1tri/time.sh
+++ c/timing/c1tri/time.sh
@@ -5,7 +5,7 @@ set +x
 function run() {
   rm -f tmp
   set -x
-  /usr/bin/time -f "%e" mpiexec -n $1 ../../c1tri -tri_m 10000000 -ksp_rtol 1.0e-10 -ksp_type $2 -pc_type $3 $4 &> tmp
+  /usr/bin/time -f "%e" mpiexec -n $1 ../../c1tri -tri_m 10000000 -ksp_rtol 1.0e-10 -ksp_converged_reason -ksp_type $2 -pc_type $3 $4 &> tmp
   set +x
   cat tmp
   NAME=$2.$3.$1
