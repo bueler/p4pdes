06108e2542ab3fe45d4045668fdabff2112fd223
Ed Bueler <elbueler@alaska.edu>
2015-05-30 22:48:22 -0600

    Update timings and include Richardson method.  Text is TO DO.

--- c/timing/c1tri/time.sh
+++ c/timing/c1tri/time.sh
@@ -5,17 +5,20 @@ set +x
 function run() {
   rm -f tmp
   set -x
-  /usr/bin/time -f "%e" mpiexec -n $1 ../../c1tri -tri_m 10000000 -ksp_type $2 -pc_type $3 $4 &> tmp
+  /usr/bin/time -f "%e" mpiexec -n $1 ../../c1tri -tri_m 10000000 -ksp_rtol 1.0e-10 -ksp_type $2 -pc_type $3 $4 &> tmp
   set +x
+  cat tmp
   NAME=$2.$3.$1
   rm -f $NAME
   cat tmp |tail -n 1 &> $NAME
   rm tmp
-  cat $NAME
 }
 
 for PC in lu cholesky; do
     run 1 preonly $PC ""
+done
+for N in 1 4; do
+    run $N richardson jacobi ""
 done
 for KSP in gmres cg; do
     for N in 1 4; do
