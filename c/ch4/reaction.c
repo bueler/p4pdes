2bddc3572a472de5d7eb59df33cd781620e36eac
Ed Bueler <elbueler@alaska.edu>
2015-06-24 09:43:59 -0600

    This is a working-well example of a nonlinear PDE in 1D.  Want Jacobian as well.

--- c/ch4/reaction.c
+++ c/ch4/reaction.c
@@ -4,7 +4,7 @@ static char help[] = "Solves a 1D reaction-diffusion problem with DMDA and SNES.
 
 //SETUP
 typedef struct {
-  PetscReal L, D, rho, uLEFT, uRIGHT;
+  PetscReal L, D, rho, M, uLEFT, uRIGHT;
 } AppCtx;
 
 PetscErrorCode FormInitialGuess(DM da, AppCtx *user, Vec u) {
@@ -15,12 +15,29 @@ PetscErrorCode FormInitialGuess(DM da, AppCtx *user, Vec u) {
 
     ierr = DMDAGetLocalInfo(da,&info); CHKERRQ(ierr);
     h = L / (info.mx-1);
-    ierr = DMDAVecGetArray(da,u,&au);CHKERRQ(ierr);
+    ierr = DMDAVecGetArray(da,u,&au); CHKERRQ(ierr);
     for (i=info.xs; i<info.xs+info.xm; i++) {
         x = h * i;
         au[i] = user->uLEFT * (L - x)/L + user->uRIGHT * x/L;
     }
-    ierr = DMDAVecRestoreArray(da,u,&au);CHKERRQ(ierr);
+    ierr = DMDAVecRestoreArray(da,u,&au); CHKERRQ(ierr);
+    return 0;
+}
+
+PetscErrorCode FormExactSolution(DM da, AppCtx *user, Vec uex) {
+    PetscErrorCode ierr;
+    PetscInt       i;
+    PetscReal      h, x, *auex;
+    DMDALocalInfo  info;
+
+    ierr = DMDAGetLocalInfo(da,&info); CHKERRQ(ierr);
+    h = user->L / (info.mx-1);
+    ierr = DMDAVecGetArray(da,uex,&auex); CHKERRQ(ierr);
+    for (i=info.xs; i<info.xs+info.xm; i++) {
+        x = h * i;
+        auex[i] = user->M * PetscPowReal(x + 1.0,4.0);
+    }
+    ierr = DMDAVecRestoreArray(da,uex,&auex); CHKERRQ(ierr);
     return 0;
 }
 //ENDSETUP
@@ -35,8 +52,8 @@ PetscErrorCode FormFunctionLocal(DMDALocalInfo *info, PetscReal *u, PetscReal *f
         } else if (i == info->mx-1) {
             f[i] = u[i] - user->uRIGHT;
         } else {
-            // generic interior location case
-            R = user->rho * u[i] * (1.0 - u[i]);
+            // generic interior location
+            R = - user->rho * PetscSqrtReal(u[i]);
             f[i] = user->D * (u[i+1] - 2.0 * u[i] + u[i-1]) / h + h * R;
         }
     }
@@ -48,43 +65,53 @@ PetscErrorCode FormFunctionLocal(DMDALocalInfo *info, PetscReal *u, PetscReal *f
 
 //MAIN
 int main(int argc,char **args) {
-  PetscErrorCode ierr;
-  DM             da;
-  SNES           snes;
-  AppCtx         user;
-  Vec            u;
-  PetscInt       iter;
-  DMDALocalInfo  info;
+  PetscErrorCode      ierr;
+  DM                  da;
+  SNES                snes;
+  AppCtx              user;
+  Vec                 u, uexact;
+  PetscReal           unorm, errnorm;
+  SNESConvergedReason reason;
+  DMDALocalInfo       info;
 
   PetscInitialize(&argc,&args,(char*)0,help);
+  user.L      = 1.0;
+  user.D      = 1.0;
+  user.rho    = 10.0;
+  user.M      = PetscSqr(user.rho / (12.0 * user.D));
+  user.uLEFT  = user.M;
+  user.uRIGHT = user.M * PetscPowReal(user.L + 1.0,4.0);
+
   ierr = DMDACreate1d(PETSC_COMM_WORLD,DM_BOUNDARY_NONE,-9,1,1,NULL,&da); CHKERRQ(ierr);
-  ierr = DMDASetUniformCoordinates(da,0.0,1.0,-1.0,-1.0,-1.0,-1.0); CHKERRQ(ierr);
-  ierr = DMSetApplicationContext(da,&user);CHKERRQ(ierr);
+  ierr = DMDASetUniformCoordinates(da,0.0,user.L,-1.0,-1.0,-1.0,-1.0); CHKERRQ(ierr);
+  ierr = DMSetApplicationContext(da,&user); CHKERRQ(ierr);
 
-  ierr = DMCreateGlobalVector(da,&u);CHKERRQ(ierr);
-  user.L = 1.0;
-  user.D = 1.0;
-  user.rho = 100.0;
-  user.uLEFT = 0.0;
-  user.uRIGHT = 1.0;
+  ierr = DMCreateGlobalVector(da,&u); CHKERRQ(ierr);
+  ierr = VecDuplicate(u,&uexact); CHKERRQ(ierr);
   ierr = FormInitialGuess(da,&user,u); CHKERRQ(ierr);
+  ierr = FormExactSolution(da,&user,uexact); CHKERRQ(ierr);
 
-  ierr = SNESCreate(PETSC_COMM_WORLD,&snes);CHKERRQ(ierr);
-  ierr = SNESSetDM(snes,da);CHKERRQ(ierr);
+  ierr = SNESCreate(PETSC_COMM_WORLD,&snes); CHKERRQ(ierr);
+  ierr = SNESSetDM(snes,da); CHKERRQ(ierr);
   ierr = DMDASNESSetFunctionLocal(da,INSERT_VALUES,
-             (DMDASNESFunction)FormFunctionLocal,&user);CHKERRQ(ierr);
+             (DMDASNESFunction)FormFunctionLocal,&user); CHKERRQ(ierr);
 //  ierr = DMDASNESSetJacobianLocal(da,
-//             (DMDASNESJacobian)FormJacobianLocal,&user);CHKERRQ(ierr);
-  ierr = SNESSetFromOptions(snes);CHKERRQ(ierr);
+//             (DMDASNESJacobian)FormJacobianLocal,&user); CHKERRQ(ierr);
+  ierr = SNESSetFromOptions(snes); CHKERRQ(ierr);
 
   ierr = SNESSolve(snes,NULL,u); CHKERRQ(ierr);
 
-  ierr = SNESGetIterationNumber(snes,&iter); CHKERRQ(ierr);
+  ierr = VecNorm(u,NORM_INFINITY,&unorm); CHKERRQ(ierr);
+  ierr = VecAXPY(u,-1.0,uexact); CHKERRQ(ierr);    // u <- u + (-1.0) uxact
+  ierr = VecNorm(u,NORM_INFINITY,&errnorm); CHKERRQ(ierr);
   ierr = DMDAGetLocalInfo(da,&info); CHKERRQ(ierr);
-  ierr = PetscPrintf(PETSC_COMM_WORLD, "on %d point grid:  %d Newton iterations\n",
-              info.mx,iter); CHKERRQ(ierr);
+  ierr = SNESGetConvergedReason(snes, &reason); CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,
+              "on %d point grid:  %s with |u-u_exact|_inf/|u|_inf = %g\n",
+              info.mx,SNESConvergedReasons[reason],errnorm/unorm); CHKERRQ(ierr);
 
-  VecDestroy(&u);  SNESDestroy(&snes);  DMDestroy(&da);
+  VecDestroy(&u);  VecDestroy(&uexact);
+  SNESDestroy(&snes);  DMDestroy(&da);
   PetscFinalize();
   return 0;
 }
