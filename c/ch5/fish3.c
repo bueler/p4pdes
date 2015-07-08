026e111241f3d089e112bd76f0994124fee6f158
Ed Bueler <elbueler@alaska.edu>
2015-07-08 14:59:54 -0600

    Add code which computes RHS.

--- c/ch5/fish3.c
+++ c/ch5/fish3.c
@@ -1,9 +1,32 @@
 static char help[] = "Solves a 3D structured-grid Poisson problem with DMDA\n"
 "and SNES.\n\n";
 
-// compare fish2.c
-
 #include <petsc.h>
+
+PetscErrorCode formExact(DM da, Vec uexact) {
+    PetscErrorCode ierr;
+    DMDALocalInfo  info;
+    PetscInt       i, j, k;
+    PetscReal      hx, hy, hz, x, y, z, ***auexact;
+
+    ierr = DMDAGetLocalInfo(da,&info); CHKERRQ(ierr);
+    hx = 1.0/(info.mx-1);  hy = 1.0/(info.my-1);  hz = 1.0/(info.mz-1);
+    ierr = DMDAVecGetArray(da, uexact, &auexact);CHKERRQ(ierr);
+    for (k=info.zs; k<info.zs+info.zm; k++) {
+        z = k * hz;
+        for (j=info.ys; j<info.ys+info.ym; j++) {
+            y = j * hy;
+            for (i=info.xs; i<info.xs+info.xm; i++) {
+                x = i * hx;
+                auexact[k][j][i] = x*x * (1.0 - x*x) * y*y * (y*y - 1.0) * z*z * (z*z - 1.0);
+            }
+        }
+    }
+    ierr = DMDAVecRestoreArray(da, uexact, &auexact);CHKERRQ(ierr);
+    ierr = VecAssemblyBegin(uexact); CHKERRQ(ierr);
+    ierr = VecAssemblyEnd(uexact); CHKERRQ(ierr);
+    return 0;
+}
 
 PetscErrorCode FormFunctionLocal(DMDALocalInfo *info, PetscReal ***u,
                                  PetscReal ***F, void *user) {
@@ -69,8 +92,8 @@ int main(int argc,char **argv)
   ierr = SNESSolve(snes,NULL,u); CHKERRQ(ierr);
 
   ierr = VecDuplicate(u,&uexact); CHKERRQ(ierr);
-// FIXME  ierr = formExact(da,uexact); CHKERRQ(ierr);
-  ierr = VecAXPY(u,-1.0,uexact); CHKERRQ(ierr);    // u <- u + (-1.0) uxact
+  ierr = formExact(da,uexact); CHKERRQ(ierr);
+  ierr = VecAXPY(u,-1.0,uexact); CHKERRQ(ierr);    // u <- u + (-1.0) uexact
   ierr = VecNorm(u,NORM_INFINITY,&errnorm); CHKERRQ(ierr);
   ierr = DMDAGetLocalInfo(da,&info);CHKERRQ(ierr);
   ierr = PetscPrintf(PETSC_COMM_WORLD,
