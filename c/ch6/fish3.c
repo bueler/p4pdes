e8357fceafb7b7e32d9762e980574f3f63057382
Ed Bueler <elbueler@alaska.edu>
2015-12-23 21:44:53 -1000

    This looks like a correct Jacobian.  Next: make a useful exact solution.

--- c/ch6/fish3.c
+++ c/ch6/fish3.c
@@ -68,12 +68,75 @@ PetscErrorCode FormFunctionLocal(DMDALocalInfo *info, PetscReal ***u,
 }
 
 PetscErrorCode FormJacobianLocal(DMDALocalInfo *info, PetscScalar ***u,
-                                 Mat J, Mat Jpre, void *user) {
-    SETERRQ(PETSC_COMM_WORLD,1,"NOT IMPLEMENTED");
+                                 Mat J, Mat Jpre, Ctx *usr) {
+
+    PetscErrorCode  ierr;
+    PetscInt        i,j,k,q;
+    PetscReal       v[7];
+    MatStencil      col[7],row;
+    const PetscReal hx2 = usr->hx*usr->hx,
+                    hy2 = usr->hy*usr->hy,
+                    hz2 = usr->hz*usr->hz,
+                    diag = 2.0*(1.0/hx2 + 1.0/hy2 + 1.0/hz2);
+    for (k=info->zs; k<info->zs+info->zm; k++) {
+        row.k = k;
+        col[0].k = k;
+        for (j=info->ys; j<info->ys+info->ym; j++) {
+            row.j = j;
+            col[0].j = j;
+            for (i=info->xs; i<info->xs+info->xm; i++) {
+                row.i = i;
+                col[0].i = i;
+                q = 1;   // will insert at least one element
+                if (i == 0 || j == 0 || k == 0
+                        || i == info->mx-1 || j == info->my-1 || k == info->mz-1) {
+                    v[0] = 1.0;
+                } else {
+                    v[0] = diag;
+                    if (i-1 != 0) {
+                        v[q] = - 1.0/hx2;
+                        col[q].k = k;  col[q].j = j;  col[q].i = i-1;
+                        q++;
+                    }
+                    if (i+1 != info->mx-1) {
+                        v[q] = - 1.0/hx2;
+                        col[q].k = k;  col[q].j = j;  col[q].i = i+1;
+                        q++;
+                    }
+                    if (j-1 != 0) {
+                        v[q] = - 1.0/hy2;
+                        col[q].k = k;  col[q].j = j-1;  col[q].i = i;
+                        q++;
+                    }
+                    if (j+1 != info->my-1) {
+                        v[q] = - 1.0/hy2;
+                        col[q].k = k;  col[q].j = j+1;  col[q].i = i;
+                        q++;
+                    }
+                    if (k-1 != 0) {
+                        v[q] = - 1.0/hz2;
+                        col[q].k = k-1;  col[q].j = j;  col[q].i = i;
+                        q++;
+                    }
+                    if (k+1 != info->mz-1) {
+                        v[q] = - 1.0/hz2;
+                        col[q].k = k+1;  col[q].j = j;  col[q].i = i;
+                        q++;
+                    }
+                }
+                ierr = MatSetValuesStencil(Jpre,1,&row,q,col,v,INSERT_VALUES); CHKERRQ(ierr);
+            }
+        }
+    }
+    ierr = MatAssemblyBegin(Jpre,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+    ierr = MatAssemblyEnd(Jpre,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+    if (J != Jpre) {
+        ierr = MatAssemblyBegin(J,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+        ierr = MatAssemblyEnd(J,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+    }
     return 0;
 }
 
-//MAIN
 int main(int argc,char **argv) {
     PetscErrorCode ierr;
     SNES           snes;
@@ -82,9 +145,6 @@ int main(int argc,char **argv) {
     Ctx            user;
 
     PetscInitialize(&argc,&argv,(char*)0,help);
-
-    //ierr = PetscOptionsBegin(PETSC_COMM_WORLD,"f3_","options to fish3","");CHKERRQ(ierr);
-    //ierr = PetscOptionsEnd();CHKERRQ(ierr);
 
     ierr = DMDACreate3d(PETSC_COMM_WORLD,
                 DM_BOUNDARY_NONE, DM_BOUNDARY_NONE, DM_BOUNDARY_NONE,
@@ -112,9 +172,8 @@ int main(int argc,char **argv) {
     ierr = DMCreateGlobalVector(user.da,&u); CHKERRQ(ierr);
     ierr = VecSet(u,0.0); CHKERRQ(ierr);
 
-    ierr = DMCreateGlobalVector(user.da,&user.f); CHKERRQ(ierr);
+    ierr = VecDuplicate(u,&user.f); CHKERRQ(ierr);
     ierr = formRHS(&info,&user); CHKERRQ(ierr);
-    //ierr = VecSet(user.f,1.0); CHKERRQ(ierr);
 
     ierr = SNESSolve(snes,NULL,u); CHKERRQ(ierr);
 
@@ -130,5 +189,4 @@ int main(int argc,char **argv) {
     ierr = PetscFinalize(); CHKERRQ(ierr);
     return 0;
 }
-//ENDMAIN
 
