0a491e5261f9928308f78a17e275621d58dbdee1
Ed Bueler <elbueler@alaska.edu>
2015-12-25 19:08:17 -1000

    I have a direction on this I think ... very much to do.

--- c/ch6/fish3.c
+++ c/ch6/fish3.c
@@ -1,5 +1,15 @@
-static char help[] = "Solves a 3D structured-grid Poisson problem with DMDA\n"
-"and SNES.\n\n";
+static char help[] =
+"Solves a 3D structured-grid diffusion-advection problem with DMDA\n"
+"and SNES.  The equation is\n"
+"    - eps Laplacian u + W . Grad u = f\n"
+"on the domain  [-1,1]^3\n"
+"FIXME but for now eps=1 and W and f are identically zero.\n"
+"The boundary conditions are\n"
+"   u(1,y,z) = g(y,z)\n"
+"   u(-1,y,z) = u(x,-1,z) = u(x,1,z) = 0\n"
+"   u periodic in z\n"
+"The exact solution is (FIXME: make optional)\n"
+"   u(x,y,z) = C sinh(D (x+1)) sin(E (y+1)) sin(F (z+1)).\n\n";
 
 /* in this version, these work:
   ./fish3 -snes_monitor -ksp_monitor
@@ -16,29 +26,37 @@ typedef struct {
 } Ctx;
 
 
-PetscErrorCode formRHS(DMDALocalInfo *info, Ctx *usr) {
-    PetscErrorCode ierr;
-    PetscInt       i, j, k;
-    //PetscReal      x, y, z;
-    PetscReal      ***af;
+PetscErrorCode formExact(DMDALocalInfo *info, Ctx *usr, Vec uex) {
+    PetscErrorCode  ierr;
+    PetscInt        i, j, k;
+    const PetscReal hx = 2.0/(info->mx-1),
+                    hy = 2.0/(info->my-1),
+                    hz = 2.0/(info->mz-1),
+                    D = sqrt(17.0) * PETSC_PI / 2.0,
+                    C = 1.0 / sinh(2.0 * D),
+                    E = PETSC_PI / 2.0,
+                    F = 2.0 * PETSC_PI;
+    PetscReal       x, y, z, ***auex;
 
-    ierr = DMDAVecGetArray(usr->da, usr->f, &af);CHKERRQ(ierr);
+    ierr = DMDAVecGetArray(usr->da, uex, &auex);CHKERRQ(ierr);
     for (k=info->zs; k<info->zs+info->zm; k++) {
-        //z = k * usr->hz;
+        z = -1.0 + k * hz;
         for (j=info->ys; j<info->ys+info->ym; j++) {
-            //y = j * usr->hy;
+            y = -1.0 + j * hy;
             for (i=info->xs; i<info->xs+info->xm; i++) {
-                //x = i * usr->hx;
-                af[k][j][i] = 1.0;
+                x = -1.0 + i * hx;
+                auex[k][j][i] = C * sinh(D*(x+1.0)) * sin(E*(y+1.0)) * sin(F*(z+1.0));
             }
         }
     }
-    ierr = DMDAVecRestoreArray(usr->da, usr->f, &af);CHKERRQ(ierr);
-    ierr = VecAssemblyBegin(usr->f); CHKERRQ(ierr);
-    ierr = VecAssemblyEnd(usr->f); CHKERRQ(ierr);
+    ierr = DMDAVecRestoreArray(usr->da, uex, &auex);CHKERRQ(ierr);
+    ierr = VecAssemblyBegin(uex); CHKERRQ(ierr);
+    ierr = VecAssemblyEnd(uex); CHKERRQ(ierr);
     return 0;
 }
 
+
+// FIXME:  generally wrong from here on
 
 PetscErrorCode FormFunctionLocal(DMDALocalInfo *info, PetscReal ***u,
                                  PetscReal ***F, Ctx *usr) {
@@ -145,7 +163,7 @@ PetscErrorCode FormJacobianLocal(DMDALocalInfo *info, PetscScalar ***u,
 int main(int argc,char **argv) {
     PetscErrorCode ierr;
     SNES           snes;
-    Vec            u;
+    Vec            u, uexact;
     DMDALocalInfo  info;
     Ctx            user;
 
@@ -175,18 +193,18 @@ int main(int argc,char **argv) {
     ierr = VecSet(u,0.0); CHKERRQ(ierr);
 
     ierr = VecDuplicate(u,&user.f); CHKERRQ(ierr);
-    ierr = formRHS(&info,&user); CHKERRQ(ierr);
+    ierr = VecSet(user.f,0.0); CHKERRQ(ierr);
+
+    ierr = VecDuplicate(u,&uexact); CHKERRQ(ierr);
+    ierr = formExact(&info,&user,uexact); CHKERRQ(ierr);
 
     ierr = SNESSolve(snes,NULL,u); CHKERRQ(ierr);
-
-    //ierr = VecDuplicate(u,&uexact); CHKERRQ(ierr);
-    //ierr = formExact(user.da,uexact); CHKERRQ(ierr);
 
     ierr = PetscPrintf(PETSC_COMM_WORLD,
              "on %d x %d x %d grid ...\n",
              info.mx,info.my,info.mz); CHKERRQ(ierr);
 
-    VecDestroy(&u);  VecDestroy(&user.f);
+    VecDestroy(&u);  VecDestroy(&user.f);  VecDestroy(&uexact);
     SNESDestroy(&snes);  DMDestroy(&user.da);
     ierr = PetscFinalize(); CHKERRQ(ierr);
     return 0;
