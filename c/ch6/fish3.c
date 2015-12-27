7a1f87486cd0e289ad2108a27a3a1dbe1f48e411
Ed Bueler <elbueler@alaska.edu>
2015-12-27 10:35:13 -1000

    Remove code duplication by using a struct.  But Jacobian still off ...

--- c/ch6/fish3.c
+++ c/ch6/fish3.c
@@ -3,7 +3,7 @@ static char help[] =
 "and SNES.  The equation is\n"
 "    - eps Laplacian u + W . Grad u = f\n"
 "on the domain  [-1,1]^3\n"
-"FIXME but for now eps=1 and W and f are identically zero.\n"
+"FIXME: for now eps=1 and W=0 and f=0\n"
 "The boundary conditions are\n"
 "   u(1,y,z) = g(y,z)\n"
 "   u(-1,y,z) = u(x,-1,z) = u(x,1,z) = 0\n"
@@ -32,28 +32,39 @@ typedef struct {
   Vec       g,f;
 } Ctx;
 
+typedef struct {
+  PetscReal hx, hy, hz, hx2, hy2, hz2;
+} Spacings;
+
+void fillSpacings(DMDALocalInfo *info, Spacings *s) {
+    s->hx = 2.0/(info->mx-1);
+    s->hy = 2.0/(info->my-1);
+    s->hz = 2.0/(info->mz);    // periodic direction
+    s->hx2 = s->hx * s->hx;
+    s->hy2 = s->hy * s->hy;
+    s->hz2 = s->hz * s->hz;
+}
 
 PetscErrorCode formExact(DMDALocalInfo *info, Ctx *usr, Vec uex, Vec g) {
     PetscErrorCode  ierr;
     PetscInt        i, j, k;
-    const PetscReal hx = 2.0/(info->mx-1),
-                    hy = 2.0/(info->my-1),
-                    hz = 2.0/(info->mz),    // periodic direction
-                    D = sqrt(17.0) * PETSC_PI / 2.0,
+    Spacings        s;
+    const PetscReal D = sqrt(17.0) * PETSC_PI / 2.0,
                     C = 1.0 / sinh(2.0 * D),
                     E = PETSC_PI / 2.0,
                     F = 2.0 * PETSC_PI;
     PetscReal       x, y, z, gg, ***auex, ***ag;
 
+    fillSpacings(info,&s);
     ierr = DMDAVecGetArray(usr->da, uex, &auex);CHKERRQ(ierr);
     ierr = DMDAVecGetArray(usr->da, g, &ag);CHKERRQ(ierr);
     for (k=info->zs; k<info->zs+info->zm; k++) {
-        z = -1.0 + k * hz;
+        z = -1.0 + k * s.hz;
         for (j=info->ys; j<info->ys+info->ym; j++) {
-            y = -1.0 + j * hy;
+            y = -1.0 + j * s.hy;
             gg = sin(E*(y+1.0)) * sin(F*(z+1.0));
             for (i=info->xs; i<info->xs+info->xm; i++) {
-                x = -1.0 + i * hx;
+                x = -1.0 + i * s.hx;
                 auex[k][j][i] = C * sinh(D*(x+1.0)) * gg;
                 if (i == info->mx-1)
                     ag[k][j][i] = gg;
@@ -77,11 +88,9 @@ PetscErrorCode FormFunctionLocal(DMDALocalInfo *info, PetscReal ***u,
     PetscErrorCode  ierr;
     PetscInt        i, j, k;
     PetscReal       uxx, uyy, uzz, ***af, ***ag;
-    const PetscReal hx = 2.0/(info->mx-1),
-                    hy = 2.0/(info->my-1),
-                    hz = 2.0/(info->mz),    // periodic direction
-                    hx2 = hx*hx,  hy2 = hy*hy,  hz2 = hz*hz;
+    Spacings        s;
 
+    fillSpacings(info,&s);
     ierr = DMDAVecGetArray(usr->da, usr->f, &af);CHKERRQ(ierr);
     ierr = DMDAVecGetArray(usr->da, usr->g, &ag);CHKERRQ(ierr);
     for (k=info->zs; k<info->zs+info->zm; k++) {
@@ -92,9 +101,9 @@ PetscErrorCode FormFunctionLocal(DMDALocalInfo *info, PetscReal ***u,
                 } else if (i == 0 || j == 0 || j == info->my-1) {
                     F[k][j][i] = u[k][j][i];
                 } else {
-                    uxx = (u[k][j][i-1] - 2.0 * u[k][j][i] + u[k][j][i+1]) / hx2;
-                    uyy = (u[k][j-1][i] - 2.0 * u[k][j][i] + u[k][j+1][i]) / hy2;
-                    uzz = (u[k-1][j][i] - 2.0 * u[k][j][i] + u[k+1][j][i]) / hz2;
+                    uxx = (u[k][j][i-1] - 2.0 * u[k][j][i] + u[k][j][i+1]) / s.hx2;
+                    uyy = (u[k][j-1][i] - 2.0 * u[k][j][i] + u[k][j+1][i]) / s.hy2;
+                    uzz = (u[k-1][j][i] - 2.0 * u[k][j][i] + u[k+1][j][i]) / s.hz2;
                     F[k][j][i] = - uxx - uyy - uzz - af[k][j][i];
                 }
             }
@@ -110,14 +119,12 @@ PetscErrorCode FormJacobianLocal(DMDALocalInfo *info, PetscScalar ***u,
 
     PetscErrorCode  ierr;
     PetscInt        i,j,k,q;
-    PetscReal       v[7];
+    PetscReal       v[7],diag;
     MatStencil      col[7],row;
-    const PetscReal hx = 2.0/(info->mx-1),
-                    hy = 2.0/(info->my-1),
-                    hz = 2.0/(info->mz),     // periodic direction
-                    hx2 = hx*hx,  hy2 = hy*hy,  hz2 = hz*hz;
-    const PetscReal diag = 2.0*(1.0/hx2 + 1.0/hy2 + 1.0/hz2);
+    Spacings        s;
 
+    fillSpacings(info,&s);
+    diag = 2.0*(1.0/s.hx2 + 1.0/s.hy2 + 1.0/s.hz2);
     for (k=info->zs; k<info->zs+info->zm; k++) {
         row.k = k;
         col[0].k = k;
@@ -133,27 +140,27 @@ PetscErrorCode FormJacobianLocal(DMDALocalInfo *info, PetscScalar ***u,
                 } else {
                     v[0] = diag;
                     col[1].k = k-1;  col[1].j = j;  col[1].i = i;
-                    v[1] = - 1.0/hz2;
+                    v[1] = - 1.0/s.hz2;
                     col[2].k = k+1;  col[2].j = j;  col[2].i = i;
-                    v[2] = - 1.0/hz2;
+                    v[2] = - 1.0/s.hz2;
                     q = 3;
                     if (i-1 != 0) {
-                        v[q] = - 1.0/hx2;
+                        v[q] = - 1.0/s.hx2;
                         col[q].k = k;  col[q].j = j;  col[q].i = i-1;
                         q++;
                     }
                     if (i+1 != info->mx-1) {
-                        v[q] = - 1.0/hx2;
+                        v[q] = - 1.0/s.hx2;
                         col[q].k = k;  col[q].j = j;  col[q].i = i+1;
                         q++;
                     }
                     if (j-1 != 0) {
-                        v[q] = - 1.0/hy2;
+                        v[q] = - 1.0/s.hy2;
                         col[q].k = k;  col[q].j = j-1;  col[q].i = i;
                         q++;
                     }
                     if (j+1 != info->my-1) {
-                        v[q] = - 1.0/hy2;
+                        v[q] = - 1.0/s.hy2;
                         col[q].k = k;  col[q].j = j+1;  col[q].i = i;
                         q++;
                     }
@@ -184,7 +191,7 @@ int main(int argc,char **argv) {
     ierr = DMDACreate3d(PETSC_COMM_WORLD,
                 DM_BOUNDARY_NONE, DM_BOUNDARY_NONE, DM_BOUNDARY_PERIODIC,
                 DMDA_STENCIL_STAR,
-                -3,-3,-2,
+                -3,-3,-3,
                 PETSC_DECIDE,PETSC_DECIDE,PETSC_DECIDE,
                 1,1,
                 NULL,NULL,NULL,
@@ -192,6 +199,9 @@ int main(int argc,char **argv) {
     ierr = DMDASetUniformCoordinates(user.da,-1.0,1.0,-1.0,1.0,-1.0,1.0); CHKERRQ(ierr);
     ierr = DMSetApplicationContext(user.da,&user); CHKERRQ(ierr);
     ierr = DMDAGetLocalInfo(user.da,&info); CHKERRQ(ierr);
+    if ((info.mx < 2) || (info.my < 2) || (info.mz < 3)) {
+        SETERRQ(PETSC_COMM_WORLD,1,"grid too coarse ... require (mx,my,mz) > (2,2,3)");
+    }
 
     ierr = SNESCreate(PETSC_COMM_WORLD,&snes);CHKERRQ(ierr);
     ierr = SNESSetDM(snes,user.da);CHKERRQ(ierr);
