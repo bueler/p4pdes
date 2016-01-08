4ecb3f464e09ade5131fb8ed7c394d4f92c05a3d
Ed Bueler <elbueler@alaska.edu>
2016-01-08 11:36:45 -0900

    This seems to work, namely putting boundary value g(x,y) in separate local Vec.  Note additional utility function GetUorG().  Lots of changes, some of which are whitespace.

--- c/ch5/plap.c
+++ c/ch5/plap.c
@@ -1,24 +1,25 @@
-static char help[] = "Solve the p-laplacian equation in 2D using Q^1 FEM\n"
-"and an objective function.  Uses a manufactured solution.\n\n";
+static char help[] = "Solve the p-Laplacian equation in 2D using Q^1 FEM\n"
+"and an objective function.  No Jacobian implementation so run as one of:\n"
+"   ./plap -snes_fd_color\n"
+"   ./plap -snes_fd\n"
+"   ./plap -snes_mf\n"
+"   ./plap -snes_fd_function -snes_fd\n"
+"Uses a manufactured solution.\n\n";
 
-// RUN AS
-//   ./plap -snes_fd
-//   ./plap -snes_mf
-//   ./plap -snes_fd_function -snes_fd
-// because there is no Jacobian (= Hessian)
+// EVIDENCE OF CONVERGENCE WITH OBJECTIVE-ONLY AND DIRECT SOLVE
+// note diverged linear solve from bad gradient
+// redo with -snes_fd_color to see total success
+// this also tests symmetry and positive-definiteness because use Cholesky
+// for LEV in 0 1 2; do ./plap -plap_p 4 -snes_fd_function -snes_fd -snes_monitor -snes_converged_reason -snes_linesearch_type basic -ksp_type preonly -pc_type cholesky -da_refine $LEV; done
 
-// EVIDENCE OF CONVERGENCE WITH OBJECTIVE-ONLY (note diverged linear solve and crappy errors):
-// for LEV in 0 1 2; do ./plap -snes_fd_function -snes_fd -snes_monitor -snes_converged_reason -ksp_type preonly -pc_type cholesky -da_refine $LEV; done
+// CHECK FINE-GRID SNES CONVERGENCE WITH -snes_fd_color:
+// timer ./plap -plap_p 4 -snes_fd_color -ksp_converged_reason -snes_monitor -snes_converged_reason -ksp_type cg -pc_type icc -da_refine 6
 
-// CHECK SNES CONVERGENCE WITH -snes_fd_color AND LINEAR CASE p=2:
-// timer ./plap -snes_fd_color -ksp_rtol 1.0e-14 -ksp_converged_reason -snes_monitor -snes_converged_reason -ksp_type cg -pc_type icc -da_refine 6
+// EVIDENCE OF CONVERGENCE WITH -snes_fd_color AND IN PARALLEL;
+// bt line search fails in parallel?
+// note nonlinear iterations increase with grid
+// for LEV in 0 1 2 3 4 5 6 7; do mpiexec -n 4 ./plap -plap_p 4 -snes_fd_color -snes_linesearch_type basic -ksp_type cg -pc_type bjacobi -sub_pc_type icc -snes_converged_reason -da_refine $LEV; done
 
-// EVIDENCE OF CONVERGENCE WITH -snes_fd_color AND IN PARALLEL AND LINEAR CASE (p=2):
-// for LEV in 0 1 2 3 4 5 6 7; do mpiexec -n 4 ./plap -snes_fd_color -ksp_type cg -pc_type bjacobi -sub_pc_type icc -ksp_rtol 1.0e-14 -ksp_converged_reason -snes_converged_reason -da_refine $LEV; done
-
-// EVIDENCE OF CONVERGENCE WITH -snes_fd_color NOT PARALLEL AND NONLINEAR CASE (p=4):
-// (note number of snes iterations grows)
-// for LEV in 0 1 2 3 4 ; do ./plap -snes_fd_color -plap_p 4 -ksp_type preonly -pc_type cholesky -snes_converged_reason -da_refine $LEV; done
 
 #include <petsc.h>
 
@@ -26,9 +27,9 @@ static char help[] = "Solve the p-laplacian equation in 2D using Q^1 FEM\n"
 
 //STARTDECLARE
 typedef struct {
-  DM        da;
-  PetscReal p;
-  Vec       f,g;
+    DM        da;
+    PetscReal p;
+    Vec       f,g;
 } PLapCtx;
 
 PetscReal BoundaryG(PetscReal x, PetscReal y) {
@@ -36,51 +37,46 @@ PetscReal BoundaryG(PetscReal x, PetscReal y) {
 }
 //ENDDECLARE
 
-//STARTBOUNDARY
+//STARTBOUNDARYINITIAL
 PetscErrorCode SetGLocal(DMDALocalInfo *info, Vec g, PLapCtx *user) {
-  PetscErrorCode ierr;
-  const PetscReal hx = 1.0 / (info->mx+1), hy = 1.0 / (info->my+1);
-  PetscInt        i,j;
-  PetscReal       x,y, **ag;
+    PetscErrorCode ierr;
+    const PetscReal hx = 1.0 / (info->mx+1), hy = 1.0 / (info->my+1);
+    PetscInt        i,j;
+    PetscReal       x,y, **ag;
 
-  ierr = DMDAVecGetArray(user->da,g,&ag); CHKERRQ(ierr);
-  for (j = info->ys-1; j <= info->ys + info->ym; j++) {
-      y = hy * (j + 1);
-      for (i = info->xs-1; i <= info->xs + info->xm; i++) {
-          x = hx * (i + 1);
-          if ((j == -1) || (j == info->my)) {
-              ag[j][i] = BoundaryG(x,y);    // bottom or top
-          } else if (i == -1) {
-              ag[j][i] = BoundaryG(0.0,y);  // left
-          } else if (i == info->mx) {
-              ag[j][i] = BoundaryG(1.0,y);  // right
-          } else {
-              ag[j][i] = NAN;
-          }
-      }
-  }
-  ierr = DMDAVecRestoreArray(user->da,g,&ag); CHKERRQ(ierr);
-  return 0;
+    ierr = DMDAVecGetArray(user->da,g,&ag); CHKERRQ(ierr);
+    for (j = info->ys - 1; j <= info->ys + info->ym; j++) {
+        for (i = info->xs - 1; i <= info->xs + info->xm; i++) {
+            if ((i >= 0) && (i < info->mx) && (j >= 0) && (j < info->my)) {
+                ag[j][i] = NAN;               // invalidate interior
+            } else {
+                x = hx * (i + 1);  y = hy * (j + 1);
+                ag[j][i] = BoundaryG(x,y);    // along boundary
+            }
+        }
+    }
+    ierr = DMDAVecRestoreArray(user->da,g,&ag); CHKERRQ(ierr);
+    return 0;
 }
 
 PetscErrorCode InitialIterate(DMDALocalInfo *info, Vec u, PLapCtx *user) {
-  PetscErrorCode ierr;
-  const PetscReal hx = 1.0 / (info->mx+1), hy = 1.0 / (info->my+1);
-  PetscInt        i,j;
-  PetscReal       x,y, **au;
+    PetscErrorCode ierr;
+    const PetscReal hx = 1.0 / (info->mx+1), hy = 1.0 / (info->my+1);
+    PetscInt        i,j;
+    PetscReal       x,y, **au;
 
-  ierr = DMDAVecGetArray(user->da,u,&au); CHKERRQ(ierr);
-  for (j = info->ys; j < info->ys + info->ym; j++) {
-    y = hy * (j + 1);
-    for (i = info->xs; i < info->xs + info->xm; i++) {
-      x = hx * (i + 1);
-      au[j][i] = (1.0 - x) * BoundaryG(0.0,y) + x * BoundaryG(1.0,y);
+    ierr = DMDAVecGetArray(user->da,u,&au); CHKERRQ(ierr);
+    for (j = info->ys; j < info->ys + info->ym; j++) {
+        y = hy * (j + 1);
+        for (i = info->xs; i < info->xs + info->xm; i++) {
+            x = hx * (i + 1);
+            au[j][i] = (1.0 - x) * BoundaryG(0.0,y) + x * BoundaryG(1.0,y);
+        }
     }
-  }
-  ierr = DMDAVecRestoreArray(user->da,u,&au); CHKERRQ(ierr);
-  return 0;
+    ierr = DMDAVecRestoreArray(user->da,u,&au); CHKERRQ(ierr);
+    return 0;
 }
-//ENDBOUNDARY
+//ENDBOUNDARYINITIAL
 
 //STARTEXACT
 PetscErrorCode ExactRHSLocal(DMDALocalInfo *info, Vec uex, Vec f,
@@ -92,7 +88,6 @@ PetscErrorCode ExactRHSLocal(DMDALocalInfo *info, Vec uex, Vec f,
     PetscInt        i,j;
     PetscReal       x,y, XX,YY, C,D2,gamma1,gamma2, **auex, **af;
 
-// PetscPrintf(COMM,"xs=%d, xm=%d, mx=%d\n",info->xs,info->xm,info->mx);
     ierr = DMDAVecGetArray(user->da,uex,&auex); CHKERRQ(ierr);
     ierr = DMDAVecGetArray(user->da,f,&af); CHKERRQ(ierr);
     // loop over ALL grid points; f has ghosts but uex does not
@@ -107,7 +102,7 @@ PetscErrorCode ExactRHSLocal(DMDALocalInfo *info, Vec uex, Vec f,
             af[j][i] = - (p-2.0) * C * (gamma1*(x+1.0)*YY + gamma2*XX*(y+1.0))
                        - C * D2;
             if ((i >= info->xs) && (i < XE) && (j >= info->ys) && (j < YE)) {
-                auex[j][i] = BoundaryG(x,y);
+                auex[j][i] = BoundaryG(x,y);   // use as exact solution
             }
         }
     }
@@ -125,71 +120,85 @@ static PetscReal xiL[4]  = { 1.0, -1.0, -1.0,  1.0},
                  etaL[4] = { 1.0,  1.0, -1.0, -1.0};
 
 PetscReal chi(PetscInt L, PetscReal xi, PetscReal eta) {
-  return 0.25 * (1.0 + xiL[L] * xi) * (1.0 + etaL[L] * eta);
+    return 0.25 * (1.0 + xiL[L] * xi) * (1.0 + etaL[L] * eta);
 }
 
 typedef struct {
-  PetscReal xi, eta;
+    PetscReal xi, eta;
 } gradRef;
 
 gradRef dchi(PetscInt L, PetscReal xi, PetscReal eta) {
-  gradRef result;
-  result.xi  = 0.25 * xiL[L]  * (1.0 + etaL[L] * eta);
-  result.eta = 0.25 * etaL[L] * (1.0 + xiL[L]  * xi);
-  return result;
+    gradRef result;
+    result.xi  = 0.25 * xiL[L]  * (1.0 + etaL[L] * eta);
+    result.eta = 0.25 * etaL[L] * (1.0 + xiL[L]  * xi);
+    return result;
 }
 
-// evaluate v(xi,eta) on ref. element using local node numbering
+// evaluate v(xi,eta) on reference element using local node numbering
 PetscReal eval(const PetscReal v[4], PetscReal xi, PetscReal eta) {
-  PetscReal sum = 0.0;
-  PetscInt  L;
-  for (L=0; L<4; L++)
-    sum += v[L] * chi(L,xi,eta);
-  return sum;
+    PetscReal sum = 0.0;
+    PetscInt  L;
+    for (L=0; L<4; L++)
+        sum += v[L] * chi(L,xi,eta);
+    return sum;
 }
 
-// evaluate partial derivs of v(xi,eta) on ref. element
+// evaluate partial derivs of v(xi,eta) on reference element
 gradRef deval(const PetscReal v[4], PetscReal xi, PetscReal eta) {
-  gradRef   sum = {0.0,0.0}, tmp;
-  PetscInt  L;
-  for (L=0; L<4; L++) {
-    tmp = dchi(L,xi,eta);
-    sum.xi  += v[L] * tmp.xi;
-    sum.eta += v[L] * tmp.eta;
-  }
-  return sum;
+    gradRef   sum = {0.0,0.0}, tmp;
+    PetscInt  L;
+    for (L=0; L<4; L++) {
+        tmp = dchi(L,xi,eta);
+        sum.xi  += v[L] * tmp.xi;
+        sum.eta += v[L] * tmp.eta;
+    }
+    return sum;
 }
 //ENDFEM
 
+//STARTTOOLS
+void GetUorG(DMDALocalInfo *info, PetscInt i, PetscInt j,
+             PetscReal **au, PetscReal **ag, PetscReal *u) {
+    u[0] = ((i == info->mx) || (j == info->my))
+             ? ag[j][i]     : au[j][i];
+    u[1] = ((i == 0)  || (j == info->my))
+             ? ag[j][i-1]   : au[j][i-1];
+    u[2] = ((i == 0)  || (j == 0))
+             ? ag[j-1][i-1] : au[j-1][i-1];
+    u[3] = ((i == info->mx) || (j == 0))
+             ? ag[j-1][i]   : au[j-1][i];
+}
+
+PetscReal GradInnerProd(DMDALocalInfo *info, gradRef du, gradRef dv) {
+    const PetscReal hx = 1.0 / (info->mx+1), hy = 1.0 / (info->my+1);
+    PetscReal       z;
+    z =  (4.0 / (hx * hx)) * du.xi  * dv.xi;
+    z += (4.0 / (hy * hy)) * du.eta * dv.eta;
+    return z;
+}
+
+PetscReal GradPow(DMDALocalInfo *info, gradRef du, PetscReal P) {
+    return PetscPowScalar(GradInnerProd(info,du,du), P / 2.0);
+}
+//ENDTOOLS
+
 //STARTOBJECTIVE
-PetscReal GradInnerProd(gradRef du, gradRef dv, DMDALocalInfo *info) {
-  const PetscReal hx = 1.0 / (info->mx+1), hy = 1.0 / (info->my+1);
-  PetscReal       z;
-  z =  (4.0 / (hx * hx)) * du.xi  * dv.xi;
-  z += (4.0 / (hy * hy)) * du.eta * dv.eta;
-  return z;
-}
-
-PetscReal GradPow(gradRef du, PetscReal P, DMDALocalInfo *info) {
-  return PetscPowScalar(GradInnerProd(du,du,info), P / 2.0);
-}
-
-PetscReal ObjIntegrand(const PetscReal f[4], const PetscReal u[4],
-                       PetscReal xi, PetscReal eta, PetscReal P,
-                       DMDALocalInfo *info) {
-  const gradRef du = deval(u,xi,eta);
-  return GradPow(du,P,info) / P - eval(f,xi,eta) * eval(u,xi,eta);
+PetscReal ObjIntegrand(DMDALocalInfo *info,
+                       const PetscReal f[4], const PetscReal u[4],
+                       PetscReal xi, PetscReal eta, PetscReal P) {
+    const gradRef du = deval(u,xi,eta);
+    return GradPow(info,du,P) / P - eval(f,xi,eta) * eval(u,xi,eta);
 }
 
 PetscErrorCode FormObjectiveLocal(DMDALocalInfo *info, PetscReal **au,
                                   PetscReal *obj, PLapCtx *user) {
   PetscErrorCode ierr;
   const PetscReal hx = 1.0 / (info->mx+1), hy = 1.0 / (info->my+1);
-  PetscReal      lobj = 0.0, **af, **ag, f[4], u[4];
-  const PetscInt n = 2;  // FIXME: quad deg
-  const PetscInt XE = info->xs + info->xm, YE = info->ys + info->ym;
-  PetscInt       i,j,r,s;
-  MPI_Comm       com;
+  PetscReal       lobj = 0.0, **af, **ag, f[4], u[4];
+  const PetscInt  n = 2;  // FIXME: quad deg
+  const PetscInt  XE = info->xs + info->xm, YE = info->ys + info->ym;
+  PetscInt        i,j,r,s;
+  MPI_Comm        com;
 
   // sum over all elements
   ierr = DMDAVecGetArray(user->da,user->f,&af); CHKERRQ(ierr);
@@ -197,17 +206,15 @@ PetscErrorCode FormObjectiveLocal(DMDALocalInfo *info, PetscReal **au,
   for (j = info->ys; j <= YE; j++) {
       for (i = info->xs; i <= XE; i++) {
           if ((i < XE) || (j < YE) || (i == info->mx) || (j == info->my)) {
-//FIXME: must modify the u evals to use g
-//PetscPrintf(COMM,"element (i,j)=(%d,%d)\n",i,j);
               // because of ghosts, these values are always valid even in the
               //     "right" and "top" cases where i==info->mx or j==info->my
               f[0] = af[j][i];  f[1] = af[j][i-1];
                   f[2] = af[j-1][i-1];  f[3] = af[j-1][i];
-              u[0] = au[j][i];  u[1] = au[j][i-1];
-                  u[2] = au[j-1][i-1];  u[3] = au[j-1][i];
+              GetUorG(info,i,j,au,ag,u);
               for (r=0; r<n; r++) {
                   for (s=0; s<n; s++) {
-                      lobj += wq[r] * wq[s] * ObjIntegrand(f,u,zq[r],zq[s],user->p,info);
+                      lobj += wq[r] * wq[s]
+                              * ObjIntegrand(info,f,u,zq[r],zq[s],user->p);
                   }
               }
           }
@@ -224,13 +231,12 @@ PetscErrorCode FormObjectiveLocal(DMDALocalInfo *info, PetscReal **au,
 //ENDOBJECTIVE
 
 //STARTFUNCTION
-PetscReal FunIntegrand(PetscInt L,
+PetscReal FunIntegrand(DMDALocalInfo *info, PetscInt L,
                        const PetscReal f[4], const PetscReal u[4],
-                       PetscReal xi, PetscReal eta, PetscReal P,
-                       DMDALocalInfo *info) {
+                       PetscReal xi, PetscReal eta, PetscReal P) {
   const gradRef du    = deval(u,xi,eta),
                 dchiL = dchi(L,xi,eta);
-  return GradPow(du,P - 2.0,info) * GradInnerProd(du,dchiL,info)
+  return GradPow(info,du,P - 2.0) * GradInnerProd(info,du,dchiL)
          - eval(f,xi,eta) * chi(L,xi,eta);
   return 0;
 }
@@ -253,14 +259,14 @@ PetscErrorCode FormFunctionLocal(DMDALocalInfo *info, PetscReal **au,
           z = 0.0;
           for (c = 0; c < 2; c++) {
               for (d = 0; d < 2; d++) {
-//FIXME: must modify the u evals to use g
                   f[0] = af[j+d][i+c];  f[1] = af[j+d][i+c-1];
                       f[2] = af[j+d-1][i+c-1];  f[3] = af[j+d-1][i+c];
-                  u[0] = au[j+d][i+c];  u[1] = au[j+d][i+c-1];
-                      u[2] = au[j+d-1][i+c-1];  u[3] = au[j+d-1][i+c];
+                  GetUorG(info,i+c,j+d,au,ag,u);
                   for (r=0; r<n; r++) {
                       for (s=0; s<n; s++) {
-                          z += wq[r] * wq[s] * FunIntegrand(ell[c][d],f,u,zq[r],zq[s],user->p,info);
+                          z += wq[r] * wq[s]
+                               * FunIntegrand(info,ell[c][d],f,u,
+                                              zq[r],zq[s],user->p);
                       }
                   }
               }
