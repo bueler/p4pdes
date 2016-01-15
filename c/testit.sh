2dc55deb77c1d49a6cfacd1c472d8abd6d4e346a
Ed Bueler <elbueler@alaska.edu>
2016-01-15 12:14:08 -0900

    Now "make test" does something reasonable in c/.

--- c/testit.sh
+++ c/testit.sh
@@ -2,9 +2,6 @@
 
 # ./testit.sh PROGRAM OPTS PROCESSES TESTNUM
 
-# if passes: silent
-# if fails: shows diffs
-
 rm -f maketmp tmp difftmp
 
 make $1 > maketmp 2>&1;
@@ -13,10 +10,13 @@ mpiexec -n $3 ./$1 $2 > tmp
 
 diff output/$1.test$4 tmp > difftmp
 
+CURRDIR=${PWD##*/}
 if [[ -s difftmp ]] ; then
-   echo "ERROR: test #$4 of $1 on $3 processes FAILED; diffs follow:"
+   echo "ERROR: Test #$4 of $CURRDIR/$1"
+   echo "       on $3 processes FAILED; diffs follow:"
    cat difftmp
 else
+   echo "PASS: Test #$4 of $CURRDIR/$1"
    rm -f maketmp tmp difftmp
 fi ;
 
