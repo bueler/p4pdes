dc30e6621106a929e2079ccdc86e11a3eb195b68
Ed Bueler <elbueler@alaska.edu>
2016-01-19 15:11:34 -0900

    testit.sh fails if output/FILE is missing

--- c/testit.sh
+++ c/testit.sh
@@ -8,18 +8,27 @@ make $1 > maketmp 2>&1;
 
 CMD="mpiexec -n $3 ./$1 $2"
 
-$CMD > tmp
+if [[ ! -f output/$1.test$4 ]]; then
+    echo "FAIL: Test #$4 of $CURRDIR/$1"
+    echo "       command = '$CMD'"
+    echo "       OUTPUT MISSING"
 
-diff output/$1.test$4 tmp > difftmp
-
-CURRDIR=${PWD##*/}
-if [[ -s difftmp ]] ; then
-   echo "FAIL: Test #$4 of $CURRDIR/$1"
-   echo "       command = '$CMD'"
-   echo "       diffs follow:"
-   cat difftmp
 else
-   echo "PASS: Test #$4 of $CURRDIR/$1"
-   rm -f maketmp tmp difftmp
-fi ;
+
+    $CMD > tmp
+
+    diff output/$1.test$4 tmp > difftmp
+
+    CURRDIR=${PWD##*/}
+    if [[ -s difftmp ]] ; then
+       echo "FAIL: Test #$4 of $CURRDIR/$1"
+       echo "       command = '$CMD'"
+       echo "       diffs follow:"
+       cat difftmp
+    else
+       echo "PASS: Test #$4 of $CURRDIR/$1"
+       rm -f maketmp tmp difftmp
+    fi
+
+fi
 
