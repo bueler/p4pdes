684d13afbec9537da3167160a97ded6024103bc2
Ed Bueler <elbueler@alaska.edu>
2016-02-02 16:30:10 -0900

    Fix things Constantine noticed in Chapter 3.

--- c/ch3/poisson.c
+++ c/ch3/poisson.c
@@ -107,7 +107,6 @@ int main(int argc,char **args) {
                DM_BOUNDARY_NONE, DM_BOUNDARY_NONE, DMDA_STENCIL_STAR,
                -9,-9,PETSC_DECIDE,PETSC_DECIDE,1,1,NULL,NULL,
                &da); CHKERRQ(ierr);
-  ierr = DMDASetUniformCoordinates(da,0.0,1.0,0.0,1.0,-1.0,-1.0); CHKERRQ(ierr);
 
   // create linear system matrix A
   ierr = DMCreateMatrix(da,&A);CHKERRQ(ierr);
--- tex/misc/dmdacreate2d.frag
+++ tex/misc/dmdacreate2d.frag
@@ -4,6 +4,5 @@
                DM_BOUNDARY_NONE, DM_BOUNDARY_NONE, DMDA_STENCIL_STAR,
                -9,-9,PETSC_DECIDE,PETSC_DECIDE,1,1,NULL,NULL,
                &da);
-  DMDASetUniformCoordinates(da,0.0,1.0,0.0,1.0,-1.0,-1.0);
 //STOP
 
