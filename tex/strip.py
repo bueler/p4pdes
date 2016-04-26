b3d4e3ebfef147def0ed3845c4cbddda1534f1ee
Ed Bueler <elbueler@alaska.edu>
2016-04-26 15:40:03 -0800

    hide old sources for unstructured mesh stuff in c/ch8/old/; isolate mesh examples in c/ch8/meshes/; stop including old sources into book chapter 8

--- tex/strip.py
+++ tex/strip.py
@@ -18,7 +18,6 @@ files = { 2 : "vecmatksp.c tri.c",
           5 : "plap.c",
           6 : "ode.c odejac.c heat.c pattern.c",
           7 : "ad3.c",
-          8 : "readmesh.c poissontools.c poissonfem.c",
          12 : "obstacle.c"}
 
 if not os.path.exists("cstrip/"):
