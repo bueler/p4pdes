6ea7bfe3df1eb0fa02408fd1fd44111700e856d5
Ed Bueler <elbueler@alaska.edu>
2016-04-28 08:49:02 -0800

    lots of cleanup; next steps: fix tri2tikz.py, read from C PETSc code

--- c/ch8/tri2petsc.py
+++ c/ch8/tri2petsc.py
@@ -10,39 +10,9 @@
 #    $ ./tri2petsc.py meshes/blob.1 foo.dat
 
 # TODO:
-#    * create functions triangle_read_{poly,node,ele}() and set up as module
-#      which can be used from tex/tri2tikz.py [avoids code duplication]
 #    * test output with a minimal PETSc code that does nothing more than
 #      VecLoad(), VecView(), ISLoad(), ISView()
-#    * make print statements optional and verbosity dependent
 #    * decide on how boundary edges will be indicated
-#    * remove clutter
-
-# FIXME development clutter
-#    myvec = np.array([1., 2., 3.]).view(pbio.Vec)
-#    myis = np.array([9, 11, 15],dtype=np.int32).view(pbio.IS)
-#    io = pbio.PetscBinaryIO()
-#    io.writeBinaryFile('file.dat', [myvec,myis,])
-#    print 'file.dat written'
-#    fh = open('file.dat')
-#    while True:
-#        try:
-#            objecttype = io.readObjectType(fh)
-#        except:
-#            break
-#        if objecttype == 'Vec':
-#            v = io.readVec(fh)
-#            print 'vec: ',
-#            print v
-#        elif objecttype == 'IS':
-#            i = io.readIS(fh)
-#            print 'IS: ',
-#            print i
-#        else:
-#            print 'UNEXPECTED'
-#    print 'file.dat read'
-#    fh.close()
-#    sys.exit(0)
 
 import numpy as np
 import sys
@@ -53,11 +23,19 @@ def dprint(d,s):
     if d < VERBOSITY:
         print s
 
-
+# N,x,y,bfn = triangle_read_node(filename)
+#     N = number of nodes
+#     x = x coordinates of nodes
+#     y = y coordinates of nodes
+#     bfn = integer flag for boundary nodes; 2 = dirichlet, 3 = neumann
 def triangle_read_node(filename):
     nodefile = open(filename, 'r')
     headersread = 0
     count = 0
+    N = 0
+    x = []
+    y = []
+    bfn = []
     for line in nodefile:
         # strip comment
         line = line.partition('#')[0]
@@ -71,7 +49,7 @@ def triangle_read_node(filename):
                 dprint(1,'  reading N = %d nodes ...' % N)
                 x = np.zeros(N)
                 y = np.zeros(N)
-                bt = np.zeros(N,dtype=np.int32)
+                bfn = np.zeros(N,dtype=np.int32)
                 headersread += 1
                 continue
             elif (headersread == 1) and (count >= N):
@@ -83,17 +61,24 @@ def triangle_read_node(filename):
                 sys.exit(2)
             x[count] = float(nxyb[1])
             y[count] = float(nxyb[2])
-            bt[count] = int(nxyb[3])
+            bfn[count] = int(nxyb[3])
             count += 1
     nodefile.close()
-    return N,x,y,bt
+    return N,x,y,bfn
 
-
+# K,e,xc,yc = triangle_read_ele(filename)
+#     K = number of elements
+#     e = K x 3 integer array of node indices
+#     xc = x coordinate of element centroid
+#     yc = y coordinate of element centroid
 def triangle_read_ele(filename):
     elefile  = open(filename,  'r')
     headersread = 0
     count = 0
+    K = 0
     e = []
+    xc = []
+    yc = []
     for line in elefile:
         # strip comment
         line = line.partition('#')[0]
@@ -118,7 +103,7 @@ def triangle_read_ele(filename):
                 print 'ERROR: INDEXING WRONG IN READING ELEMENTS'
                 sys.exit(2)
             # node indices for this element
-            pp = np.array([int(emabc[1])-1, int(emabc[2])-1, int(emabc[3])-1],dtype=np.int32)
+            pp = [int(emabc[1])-1, int(emabc[2])-1, int(emabc[3])-1]
             # compute element centroid
             kk = [0, 1, 2, 0]  # cycle through nodes
             xc = 0.0
@@ -129,22 +114,7 @@ def triangle_read_ele(filename):
                 yc = yc + y[jj]
             xc = xc/3.0
             yc = yc/3.0
-            # identify element and edges
             dprint(1,'element %d centered at (%f,%f):' % (count,xc,yc))
-            for k in range(3):
-                jfrom = pp[kk[k]]
-                jto   = pp[kk[k+1]]
-                # FIXME: following test can mis-label an edge as BOUNDARY when it is
-                #        not, in the case where its ends (nodes) are both in the boundary
-                #        the solution seems to also require reading the .poly file so
-                #        we know which segments are in the boundary
-                bdrystr = ''
-                if (bt[jfrom] > 0) and (bt[jto] > 0):
-                    if (bt[jfrom] == 2) and (bt[jto] == 2):
-                        bdrystr = ' DIRICHLET BOUNDARY'
-                    else:
-                        bdrystr = ' NEUMANN BOUNDARY'
-                dprint(1,'  edge (%d,%d)%s' % (jfrom,jto,bdrystr))
             # append this element
             e[count,:] = pp
             xcentroid[count] = xc
@@ -153,15 +123,23 @@ def triangle_read_ele(filename):
     elefile.close()
     return K,e,xc,yc
 
-
+# PN,PS,px,py,s,bfs = triangle_read_poly(filename)
+#     PN = number of node locations (in .poly file)
+#     PS = number of segment pairs
+#     px = x coordinate of node; length PN
+#     py = y coordinate of node; length PN
+#     s = segment pairs; PS x 2
+#     bfs = integer flag for boundary segments; length PS; 2 = dirichlet, 3 = neumann
 def triangle_read_poly(filename):
     polyfile = open(filename, 'r')
     headersread = 0
     count = 0
-    bx = []
-    by = []
     PN = 0
     PS = 0
+    px = []
+    py = []
+    s = []
+    bfs = []
     for line in polyfile:
         # strip comment
         line = line.partition('#')[0]
@@ -169,22 +147,22 @@ def triangle_read_poly(filename):
         if line: # only act if line content remains
             # read headers and allocate when we can
             if headersread == 0:
-                polyheader1 = line
-                dprint(1,'  header 1 says:  ' + polyheader1)
-                PN = (int)(polyheader1.split()[0])
+                dprint(1,'  header 1 says:  ' + line)
+                PN = (int)(line.split()[0])
                 if PN > 0:
                     dprint(1,'  reading PN = %d nodes on boundary ...' % PN)
-                    bx = np.zeros(PN)
-                    by = np.zeros(PN)
+                    px = np.zeros(PN)
+                    py = np.zeros(PN)
                 headersread += 1
                 ncount = 0
                 continue
             if (headersread == 1) and (ncount >= PN):
-                polyheader2 = line
-                dprint(1,'  header 2 says:  ' + polyheader2)
-                PS = (int)(polyheader2.split()[0])
+                dprint(1,'  header 2 says:  ' + line)
+                PS = (int)(line.split()[0])
                 if PS > 0:
                     dprint(1,'  reading PS = %d boundary segments (ignoring markers) ...' % PS)
+                    s = np.zeros((PS,2),dtype=np.int32)
+                    bfs = np.zeros(PS,dtype=np.int32)
                 headersread += 1
                 scount = 0
                 continue
@@ -192,32 +170,33 @@ def triangle_read_poly(filename):
                 break # nothing more to read
             # read content if headersread = 1 or 2 and count is small
             if (headersread == 1) and (ncount < PN):
-                # read a line describing a node on the boundary; should only occur
-                #   in cases where bx[], by[] exist
+                # read a line describing a node on the boundary
+                # note px[], py[] should exist
                 nxyb = line.split()
                 if ncount+1 != int(nxyb[0]):
                     print 'ERROR: INDEXING WRONG IN READING NODES ON POLYGON'
                     sys.exit(2)
-                bx[ncount] = float(nxyb[1])
-                by[ncount] = float(nxyb[2])
-                dprint(1,'  polygon node (%f,%f)' % (bx[ncount],by[ncount]))
+                px[ncount] = float(nxyb[1])
+                py[ncount] = float(nxyb[2])
+                dprint(1,'  polygon node (%f,%f)' % (px[ncount],py[ncount]))
                 scount += 1
                 continue
             if (headersread == 2) and (scount < PS):
                 # read a line describing a boundary segment
+                # note s[] should exist
                 pjkb = line.split()
                 if scount+1 != int(pjkb[0]):
                     print 'ERROR: INDEXING WRONG IN READING POLYGON SEGMENTS'
                     sys.exit(2)
-                jfrom = int(pjkb[1])-1
-                jto   = int(pjkb[2])-1
-                dprint(1,'polygon segment (%d,%d)' % (jfrom,jto))
+                s[scount,:] = [int(pjkb[1])-1, int(pjkb[2])-1]
+                bfs[scount] = int(pjkb[3])
+                dprint(1,'polygon segment (%d,%d)' % tuple(s[scount,:]))
                 scount += 1
             else:
                 print 'ERROR:  headersread not 1 or 2 and yet trying to read; something wrong ... exiting'
                 sys.exit(1)
     polyfile.close()
-    return PN,PS,bx,by
+    return PN,PS,px,py,s,bfs
 
 
 if __name__ == "__main__":
@@ -238,7 +217,7 @@ if __name__ == "__main__":
 
     nodename = args.inroot + '.node'
     print 'reading nodes from %s ' % nodename,
-    N,x,y,bt = triangle_read_node(nodename)
+    N,x,y,bfn = triangle_read_node(nodename)
     print '... N=%d nodes' % N
 
     elename = args.inroot + '.ele'
@@ -248,13 +227,11 @@ if __name__ == "__main__":
 
     polyname = args.inroot + '.poly'
     print 'reading polygon from %s ' % polyname,
-    PN,PS,bx,by = triangle_read_poly(polyname)
+    PN,PS,px,py,s,bfs = triangle_read_poly(polyname)
     print '... PN=%d nodes and PS=%d segments' % (PN,PS)
 
-    #FIXME need to write out indications that edges are in boundary
-    #FIXME possibly write out element centroid
-
-    print 'writing to petsc binary file %s' % args.outfile
+    # FIXME presumably need to write more
+    print 'writing node locations and element triples to petsc binary file %s' % args.outfile
     ox = x.view(PetscBinaryIO.Vec)
     oy = y.view(PetscBinaryIO.Vec)
     e = e.flatten()
