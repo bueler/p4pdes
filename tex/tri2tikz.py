5f8979cfc6fd4820f3747b52962b64dcc9754fe2
Ed Bueler <elbueler@alaska.edu>
2016-04-28 10:12:16 -0800

    apparently functional tri2tikz.py which depends on c/ch8/tri2petsc.py (removes code duplication)

--- tex/tri2tikz.py
+++ tex/tri2tikz.py
@@ -50,7 +50,6 @@ parser.add_argument('outfile', metavar='FILENAME',
 
 # process options: simpler names, and type conversions
 args = parser.parse_args()
-outname  = args.outfile
 dolabelnodes = args.labelnodes
 dolabeleles  = args.labelelements
 polyonly     = args.polyonly
@@ -73,186 +72,53 @@ if not polyonly:
 
     elename = args.inroot + '.ele'
     print 'reading element triples from %s ' % elename,
-    K,e,xc,yc = triangle_read_ele(elename)
+    K,e,xc,yc = triangle_read_ele(elename,x,y)
     print '... K=%d elements' % K
 
-tikz = open(outname, 'w')
-print 'writing to %s ...' % outname
+tikz = open(args.outfile, 'w')
+print 'writing to %s ...' % args.outfile
 tikz.write('%% created by script tri2tikz.py command line:%s\n' % '')
 tikz.write('%%   %s\n' % commandline)
 tikz.write('%%%s\n' % '')
 tikz.write('\\begin{tikzpicture}[scale=%f]\n' % scale)
 
-FIXME FROM HERE
-
-# READ .node FILE BUT DO NO PLOTTING
 if not polyonly:
-    print 'reading from %s ' % nodename
-    headersread = 0
-    count = 0
-    for line in nodefile:
-        # strip comment
-        line = line.partition('#')[0]
-        line = line.rstrip()
-        if line: # only act if line content remains
-            # read headers
-            if headersread == 0:
-                nodeheader = line
-                print '  header says:  ' + nodeheader,
-                N = (int)(nodeheader.split()[0])
-                print '... reading N = %d nodes from interior and bdry ...' % N
-                x = numpy.zeros(N)
-                y = numpy.zeros(N)
-                bt = numpy.zeros(N,dtype=numpy.int32)
-                headersread += 1
-                continue
-            elif (headersread == 1) and (count >= N):
-                break # nothing more to read
-            # read content if headersread = 1 and count is small
-            #DEBUG print 'reading nodefile content line with count = %d ...' % count
-            nxyb = line.split()
-            #DEBUG print nxyb
-            if count+1 != int(nxyb[0]):
-                print 'ERROR: INDEXING WRONG IN READING NODES IN NODEFILE'
-                sys.exit(2)
-            x[count] = float(nxyb[1])
-            y[count] = float(nxyb[2])
-            bt[count] = int(nxyb[3])
-            count += 1
-
-# READ .ele FILE AND PLOT TRIANGLES
-if not polyonly:
-    print 'reading from %s ' % elename
-    headersread = 0
-    count = 0
-    for line in elefile:
-        # strip comment
-        line = line.partition('#')[0]
-        line = line.rstrip()
-        if line: # only act if line content remains
-            # read headers
-            if headersread == 0:
-                eleheader = line
-                print '  header says:  ' + eleheader,
-                K = (int)(eleheader.split()[0])
-                print '... reading K = %d elements ...' % K
-                headersread += 1
-                continue
-            elif (headersread == 1) and (count >= K):
-                break # nothing more to read
-            # read content if headersread = 1 and count is small
-            #DEBUG print 'reading elefile content line with count = %d ...' % count
-            emabc = line.split()
-            #DEBUG print emabc
-            if count+1 != int(emabc[0]):
-              print 'ERROR: INDEXING WRONG IN READING ELEMENTS'
-              sys.exit(2)
-            pp = [int(emabc[1])-1, int(emabc[2])-1, int(emabc[3])-1] # node indices for this element
-            kk = [0, 1, 2, 0]  # cycle through nodes
-            for k in range(3):
-              jfrom = pp[kk[k]]
-              jto   = pp[kk[k+1]]
-              if (not noboundary) or (bt[jfrom] == 0) or (bt[jto] == 0):
+    # go through elements and draw edges and label centroids
+    for ke in range(K):
+        l = [0, 1, 2, 0]  # cycle through local node index
+        for k in range(3):
+            jfrom = e[ke,l[k]]
+            jto   = e[ke,l[k+1]]
+            if (not noboundary) or (bfn[jfrom] == 0) or (bfn[jto] == 0):
                 tikz.write('  \\draw[gray,very thin] (%f,%f) -- (%f,%f);\n' \
                            % (x[jfrom],y[jfrom],x[jto],y[jto]))
             if dolabeleles:
-              xc = 0.0
-              yc = 0.0
-              for k in range(3):
-                jj = pp[kk[k]]
-                xc = xc + x[jj]
-                yc = yc + y[jj]
-              xc = xc/3.0
-              yc = yc/3.0
-              tikz.write( '  \\draw (%f,%f) node {$%d$};\n' \
-                         % (xc+0.7*eleoffset,yc-eleoffset,count))
-            count += 1
+                tikz.write( '  \\draw (%f,%f) node {$%d$};\n' \
+                           % (xc[ke]+0.7*eleoffset,yc[ke]-eleoffset,ke))
+    # plot all nodes, with labels if wanted; looks better if *after* edges
+    for j in range(N):
+        tikz.write('  \\filldraw (%f,%f) circle (%fpt);\n' % (x[j],y[j],nodesize))
+        if dolabelnodes:
+            tikz.write( '  \\draw (%f,%f) node {$%d$};\n' \
+                       % (x[j]+0.7*nodeoffset,y[j]-nodeoffset,j))
 
-# READ .poly FILE
 if not noboundary:
-  print 'reading from %s ' % polyname
-  headersread = 0
-  count = 0
-  for line in polyfile:
-    # strip comment
-    line = line.partition('#')[0]
-    line = line.rstrip()
-    if line: # only act if line content remains
-      # read headers
-      if headersread == 0:
-          polyheader1 = line
-          print '  header 1 says:  ' + polyheader1,
-          NP = (int)(polyheader1.split()[0])
-          if NP > 0:
-            print '... reading NP = %d nodes on boundary ...' % NP
-            # read node info into arrays
-            bx = numpy.zeros(NP)
-            by = numpy.zeros(NP)
-          else:
-            print '...'
-          headersread += 1
-          count = 0
-          continue
-      elif (headersread == 1) and (count >= NP):
-          polyheader2 = line
-          print '  header 2 says:  ' + polyheader2,
-          P = (int)(polyheader2.split()[0])
-          print '... reading P = %d boundary segments ...' % P
-          headersread += 1
-          count = 0
-          continue
-      elif (headersread == 2) and (count >= P):
-          break # nothing more to read
-      # read content if headersread = 1 or 2 and count is small
-      #DEBUG  print 'reading content line with count = %d ...' % count
-      if headersread == 1:
-          # read a line describing a node on the boundary; should only occur
-          #   in cases where bx[], by[] exist
-          nxyb = line.split()
-          if count+1 != int(nxyb[0]):
-              print 'ERROR: INDEXING WRONG IN READING NODES ON POLYGON'
-              sys.exit(2)
-          bx[count] = float(nxyb[1])
-          by[count] = float(nxyb[2])
-          tikz.write('  \\filldraw (%f,%f) circle (%fpt);\n' % (bx[count],by[count],nodesize))
-          count += 1
-      elif headersread == 2:
-          # read a line describing a boundary segment
-          pjkb = line.split()
-          if count+1 != int(pjkb[0]):
-              print 'ERROR: INDEXING WRONG IN READING POLYGON SEGMENTS'
-              sys.exit(2)
-          jfrom = int(pjkb[1])-1
-          jto   = int(pjkb[2])-1
-          if int(pjkb[3]) == 2: # check boundary type
-              mywidth = '2.5pt'   # strong line for Dirichlet part
-          else:
-              mywidth = '0.75pt'  # weak line for Neumann
-          if polyonly:
-              tikz.write('  \\draw[line width=%s] (%f,%f) -- (%f,%f);\n' \
-                         % (mywidth,bx[jfrom],by[jfrom],bx[jto],by[jto]))
-          else:
-              tikz.write('  \\draw[line width=%s] (%f,%f) -- (%f,%f);\n' \
-                         % (mywidth,x[jfrom],y[jfrom],x[jto],y[jto]))
-          count += 1
-      else:
-          print 'ERROR:  headersread not 1 or 2 and yet trying to read; something wrong ... exiting'
-          sys.exit(1)
-
-if not polyonly:
-  # plot interior and boundary nodes themselves
-  for j in range(N):
-    if dolabelnodes:
-      tikz.write( '  \\draw (%f,%f) node {$%d$};\n' \
-                 % (x[j]+0.7*nodeoffset,y[j]-nodeoffset,j))
-    tikz.write('  \\filldraw (%f,%f) circle (%fpt);\n' % (x[j],y[j],nodesize))
+    # go through boundary segments and plot with weight from type
+    for js in range(PS):
+        jfrom = s[js,0]
+        jto = s[js,1]
+        if bfs[js] == 2: # check boundary type
+            mywidth = '2.5pt'   # strong line for Dirichlet part
+        else:
+            mywidth = '0.75pt'  # weak line for Neumann
+        if polyonly:
+            tikz.write('  \\draw[line width=%s] (%f,%f) -- (%f,%f);\n' \
+                       % (mywidth,px[jfrom],py[jfrom],px[jto],py[jto]))
+        else:
+            tikz.write('  \\draw[line width=%s] (%f,%f) -- (%f,%f);\n' \
+                       % (mywidth,x[jfrom],y[jfrom],x[jto],y[jto]))
 
 tikz.write('\\end{tikzpicture}\n')
 tikz.close()
 print 'done'
-
-polyfile.close()
-if not polyonly:
-  nodefile.close()
-  elefile.close()
 
