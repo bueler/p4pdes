f73275fdab69635345bfc781f8883766a9c60788
Ed Bueler <elbueler@alaska.edu>
2016-05-04 12:42:29 -0800

    it now works ... this commit has major simplifications of various pieces, but: (1) fixed P^1 degree, (2) fixed quadrature degree, (3) TODO non-homogeneous Neumann

--- c/ch8/unfem.c
+++ c/ch8/unfem.c
@@ -4,12 +4,15 @@ static char help[] = "Unstructured 2D FEM solution of nonlinear Poisson equation
 "domain, with Dirichlet data g_D(x,y) on portion of boundary.\n"
 "Functions a(), f(), and g_D() are given as formulas.\n"
 "Input files in PETSc binary format contain node coordinates, elements, and\n"
-"boundary flags stored in files.  Allows arbitrary non-homogeneous Dirichlet\n"
-"and Neumann conditions along parts of boundary.\n\n";
+"boundary flags stored in files.  Allows non-homogeneous Dirichlet\n"
+"and Neumann conditions along subsets of boundary.\n\n";
 
 // example:
 //   $ ./tri2petsc.py meshes/blob.1 foo.dat
-//   $ ./unfem -un_view -un_mesh foo.dat
+//   $ ./unfem -un_view -un_mesh foo.dat -snes_fd
+
+// with view of mat:
+//   $ ./unfem -un_mesh foo.dat -snes_fd -snes_monitor -ksp_monitor -mat_view draw -draw_pause 1
 
 #include <petsc.h>
 #include "um.h"
@@ -17,6 +20,7 @@ static char help[] = "Unstructured 2D FEM solution of nonlinear Poisson equation
 typedef struct {
     UM  mesh;
     Vec gD;
+    //Vec gN;  <--- FIXME TODO
 } unfemCtx;
 
 double a_fcn(double u, double x, double y) {
@@ -24,169 +28,76 @@ double a_fcn(double u, double x, double y) {
 }
 
 double f_fcn(double u, double x, double y) {
-    return -2.0 + 12.0 * y * y;
+    return 2.0 + 3.0 * y * y;
 }
 
 double uexact_fcn(double x, double y) {
     const double y2 = y * y;
-    return 1.0 - y2 - y2 * y2;
+    return 1.0 - y2 - 0.25 * y2 * y2;
 }
 
-PetscErrorCode FillExact(Vec uexact, unfemCtx *ctx) {
+PetscErrorCode FillExactAndDirichlet(Vec uexact, Vec gD, unfemCtx *ctx) {
     PetscErrorCode ierr;
     const double *ax, *ay;
-    double       *auexact;
+    const int    *abf;
+    double       *auexact, *agD;
     int          i;
-    ierr = UMAssertValid(&(ctx->mesh)); CHKERRQ(ierr);
+    ierr = ISGetIndices(ctx->mesh.bf,&abf); CHKERRQ(ierr);
     ierr = VecGetArrayRead(ctx->mesh.x,&ax); CHKERRQ(ierr);
     ierr = VecGetArrayRead(ctx->mesh.y,&ay); CHKERRQ(ierr);
     ierr = VecGetArray(uexact,&auexact); CHKERRQ(ierr);
+    ierr = VecGetArray(gD,&agD); CHKERRQ(ierr);
     for (i = 0; i < ctx->mesh.N; i++) {
         auexact[i] = uexact_fcn(ax[i],ay[i]);
+        agD[i] = (abf[i] == 2) ? auexact[i] : NAN;
     }
     ierr = VecRestoreArrayRead(ctx->mesh.y,&ay); CHKERRQ(ierr);
     ierr = VecRestoreArrayRead(ctx->mesh.x,&ax); CHKERRQ(ierr);
     ierr = VecRestoreArray(uexact,&auexact); CHKERRQ(ierr);
-    return 0;
-}
-
-PetscErrorCode FillDirichletFromExact(Vec uexact, Vec gD, unfemCtx *ctx) {
-    PetscErrorCode ierr;
-    const double *auexact;
-    const int    *abf;
-    double       *agD;
-    int          i;
-    ierr = UMAssertValid(&(ctx->mesh)); CHKERRQ(ierr);
-    ierr = ISGetIndices(ctx->mesh.bf,&abf); CHKERRQ(ierr);
-    ierr = VecGetArrayRead(uexact,&auexact); CHKERRQ(ierr);
-    ierr = VecGetArray(gD,&agD); CHKERRQ(ierr);
-    for (i = 0; i < ctx->mesh.N; i++) {
-        if (abf[i] == 2)
-            agD[i] = auexact[i];
-        else
-            agD[i] = NAN;
-    }
-    ierr = VecGetArray(gD,&agD); CHKERRQ(ierr);
-    ierr = VecRestoreArrayRead(uexact,&auexact); CHKERRQ(ierr);
+    ierr = VecRestoreArray(gD,&agD); CHKERRQ(ierr);
     ierr = ISRestoreIndices(ctx->mesh.bf,&abf); CHKERRQ(ierr);
     return 0;
 }
-
-
-// evaluate u or g, according to whether the node is on
-// the Dirichlet boundary or not, at the 3 vertices of triangle k
-PetscErrorCode GetUorG(Vec u, int k, double *uvertex, unfemCtx *ctx) {
-    PetscErrorCode ierr;
-    const int    *ae, *abf;
-    const double *au, *agD;
-    int          i, m;
-    ierr = ISGetIndices(ctx->mesh.e,&ae); CHKERRQ(ierr);
-    ierr = ISGetIndices(ctx->mesh.bf,&abf); CHKERRQ(ierr);
-    ierr = VecGetArrayRead(ctx->gD,&agD); CHKERRQ(ierr);
-    ierr = VecGetArrayRead(u,&au); CHKERRQ(ierr);
-    for (m = 0; m < 3; m++) {
-        i = ae[3*k+m];   // node index for vertex m of triangle k
-        uvertex[m] = (abf[i] == 2) ? agD[i] : au[i];
-    }
-    ierr = VecRestoreArrayRead(u,&au); CHKERRQ(ierr);
-    ierr = VecRestoreArrayRead(ctx->gD,&agD); CHKERRQ(ierr);
-    ierr = ISRestoreIndices(ctx->mesh.bf,&abf); CHKERRQ(ierr);
-    ierr = ISRestoreIndices(ctx->mesh.e,&ae); CHKERRQ(ierr);
-    return 0;
-}
-
-#define DEBUG_REFERENCE_EVAL 1
 
 double chi(int L, double xi, double eta) {
-#ifdef DEBUG_REFERENCE_EVAL
-    if ((xi < 0.0) || (xi > 1.0) || (eta < 0.0) || (eta > 1.0 - xi)) {
-        PetscPrintf(PETSC_COMM_WORLD,"chi(): coordinates (xi,eta) outside of reference element\n");
-        PetscEnd();
-    }
-#endif
-    switch (L) {
-        case 1 :
-            return xi;
-        case 2 :
-            return eta;
-        default :
-            return 1.0 - xi - eta;
+    if (L == 0) {
+        return 1.0 - xi - eta;
+    } else if (L == 1) {
+        return xi;
+    } else {
+        return eta;
     }
 }
+
+const double dchi[3][2] = {{-1.0,-1.0},
+                           { 1.0, 0.0},
+                           { 0.0, 1.0}};
 
 // evaluate v(xi,eta) on reference element using local node numbering
 double eval(const double v[3], double xi, double eta) {
     double sum = 0.0;
-    int    q;
-    for (q=0; q<3; q++)
-        sum += v[q] * chi(q,xi,eta);
+    int    L;
+    for (L = 0; L < 3; L++)
+        sum += v[L] * chi(L,xi,eta);
     return sum;
 }
 
-typedef struct {
-    double  xi, eta;
-} gradRef;
-
-gradRef dchi(int q, double xi, double eta) {
-#ifdef DEBUG_REFERENCE_EVAL
-    if ((xi < 0.0) || (xi > 1.0) || (eta < 0.0) || (eta > 1.0 - xi)) {
-        PetscPrintf(PETSC_COMM_WORLD,"chi(): coordinates (xi,eta) outside of reference element\n");
-        PetscEnd();
-    }
-#endif
-    switch (q) {
-        case 1 :
-            return (gradRef){1.0, 0.0};
-        case 2 :
-            return (gradRef){0.0, 1.0};
-        default :
-            return (gradRef){-1.0, -1.0};
-    }
+double InnerProd(const double V[2], const double W[2]) {
+    return V[0] * W[0] + V[1] * W[1];
 }
 
-typedef struct {
-    double  x, y;
-} grad;
-
-//FIXME should evaluate w.r.t. (x,y)
-// evaluate partial derivs of v(xi,eta) on reference element
-grad gradeval(const double v[3], double xi, double eta) {
-    gradRef tmp;
-    grad    sum = {0.0,0.0};
-    int     q;
-    for (q=0; q<3; q++) {
-        tmp = dchi(q,xi,eta);
-        // FIXME in here
-        sum.x += v[q] * tmp.xi;
-        sum.y += v[q] * tmp.eta;
-    }
-    return sum;
-}
-
-double GradInnerProd(grad du, grad dv) {
-    return du.x * dv.x + du.y * dv.y;
-}
-
-double FunIntegrand(int ell, const double u[3],
-                    double a, double f, double xi, double eta) {
-  grad dpsi;
-  //FIXME: dpsi from  dchi(ell,xi,eta)
-  dpsi.x = 0.0;
-  dpsi.y = 0.0;
-  return a * GradInnerProd(gradeval(u,xi,eta),dpsi) - f * chi(ell,xi,eta);
-}
+// quadrature points and weights from bottom page 7 of Shaodeng notes
+const int    Q = 3; // number of quadrature points
+const double xi[3]  = {1.0/6.0, 2.0/3.0, 1.0/6.0},
+             eta[3] = {1.0/6.0, 1.0/6.0, 2.0/3.0},
+             w[3]   = {1.0/6.0, 1.0/6.0, 1.0/6.0};
 
 PetscErrorCode FormFunction(SNES snes, Vec u, Vec F, void *ctx) {
     PetscErrorCode ierr;
     unfemCtx     *user = (unfemCtx*)ctx;
-    const int    Q = 3; // number of quadrature points
-    // quadrature points and weights from bottom page 7 of Shaodeng notes
-    const double xi[3]  = {1.0/6.0, 2.0/3.0, 1.0/6.0},
-                 eta[3] = {1.0/6.0, 1.0/6.0, 2.0/3.0},
-                 w[3]   = {1.0/6.0, 1.0/6.0, 1.0/6.0};
-    double       *aF, unode[3],
+    double       *aF, unode[3], gradu[2], gradpsi[3][2],
                  uquad[Q], aquad[Q], fquad[Q],
-                 dx1, dx2, dy1, dy2, rho, xx, yy, sum;
+                 dx1, dx2, dy1, dy2, detJ, xx, yy, sum;
     int          n, k, l, q;
     const int    *abf, *ae, *en;
     const double *ax, *ay, *agD, *au;
@@ -201,24 +112,32 @@ PetscErrorCode FormFunction(SNES snes, Vec u, Vec F, void *ctx) {
         if (abf[n] == 2)
             aF[n] = au[n] - agD[n];
     }
-    ierr = VecRestoreArrayRead(u,&au); CHKERRQ(ierr);
-    ierr = VecRestoreArrayRead(user->gD,&agD); CHKERRQ(ierr);
     // add element contributions to residuals
     ierr = ISGetIndices(user->mesh.e,&ae); CHKERRQ(ierr);
     ierr = VecGetArrayRead(user->mesh.x,&ax); CHKERRQ(ierr);
     ierr = VecGetArrayRead(user->mesh.y,&ay); CHKERRQ(ierr);
     for (k = 0; k < user->mesh.K; k++) {
-        en = ae + 3*k;        // en[0], en[1], en[2] are nodes of element k
-        ierr = PetscPrintf(PETSC_COMM_WORLD,"element k=%3d:  en[0]=%d, en[1]=%d, en[2]=%d\n",
-                           k, en[0], en[1], en[2]); CHKERRQ(ierr);
-        // get geometry of element
+        en = ae + 3*k;  // en[0], en[1], en[2] are nodes of element k
+        // geometry of element
         dx1 = ax[en[1]] - ax[en[0]];
         dx2 = ax[en[2]] - ax[en[0]];
         dy1 = ay[en[1]] - ay[en[0]];
         dy2 = ay[en[2]] - ay[en[0]];
-        rho = fabs(dx1 * dy2 - dx2 * dy1);
-        // get all function values at quadrature nodes on element
-        ierr = GetUorG(u,k,unode,user); CHKERRQ(ierr);
+        detJ = dx1 * dy2 - dx2 * dy1;
+        // gradients of hat functions
+        for (l = 0; l < 3; l++) {
+            gradpsi[l][0] = ( dy2 * dchi[l][0] - dy1 * dchi[l][1]) / detJ;
+            gradpsi[l][1] = (-dx2 * dchi[l][0] + dx1 * dchi[l][1]) / detJ;
+        }
+        // u and grad u on element
+        gradu[0] = 0.0;
+        gradu[1] = 0.0;
+        for (l = 0; l < 3; l++) {
+            unode[l] = (abf[en[l]] == 2) ? agD[en[l]] : au[en[l]];
+            gradu[0] += unode[l] * gradpsi[l][0];
+            gradu[1] += unode[l] * gradpsi[l][1];
+        }
+        // function values at quadrature points on element
         for (q = 0; q < Q; q++) {
             uquad[q] = eval(unode,xi[q],eta[q]);
             xx = ax[en[0]] + dx1 * xi[q] + dx2 * eta[q];
@@ -231,15 +150,17 @@ PetscErrorCode FormFunction(SNES snes, Vec u, Vec F, void *ctx) {
             if (abf[en[l]] < 2) { // if NOT a Dirichlet node
                 sum = 0.0;
                 for (q = 0; q < Q; q++) {
-                    sum += w[q] * FunIntegrand(l,unode,aquad[q],fquad[q],
-                                               xi[q],eta[q]);
+                    sum += w[q] * ( aquad[q] * InnerProd(gradu,gradpsi[l])
+                                    - fquad[q] * chi(l,xi[q],eta[q]) );
                 }
-                aF[en[l]] += rho * sum;
+                aF[en[l]] += fabs(detJ) * sum;
             }
         }
     }
     ierr = ISRestoreIndices(user->mesh.e,&ae); CHKERRQ(ierr);
     ierr = ISRestoreIndices(user->mesh.bf,&abf); CHKERRQ(ierr);
+    ierr = VecRestoreArrayRead(u,&au); CHKERRQ(ierr);
+    ierr = VecRestoreArrayRead(user->gD,&agD); CHKERRQ(ierr);
     ierr = VecRestoreArrayRead(user->mesh.x,&ax); CHKERRQ(ierr);
     ierr = VecRestoreArrayRead(user->mesh.y,&ay); CHKERRQ(ierr);
     ierr = VecRestoreArray(F,&aF); CHKERRQ(ierr);
@@ -265,25 +186,20 @@ int main(int argc,char **argv) {
            "unfem.c",meshroot,meshroot,sizeof(meshroot),NULL); CHKERRQ(ierr);
     ierr = PetscOptionsEnd(); CHKERRQ(ierr);
 
-    // initialize and read mesh/context object of type UM
+    // read mesh/context object of type UM
     ierr = UMInitialize(&(user.mesh)); CHKERRQ(ierr);
     ierr = UMReadNodes(&(user.mesh),meshroot); CHKERRQ(ierr);
     ierr = UMReadElements(&(user.mesh),meshroot); CHKERRQ(ierr);
-
-    // fill fields
-    ierr = UMCreateGlobalVec(&(user.mesh),&uexact); CHKERRQ(ierr);
-    ierr = FillExact(uexact,&user); CHKERRQ(ierr);
-    ierr = VecDuplicate(uexact,&(user.gD)); CHKERRQ(ierr);
-    ierr = FillDirichletFromExact(uexact,user.gD,&user); CHKERRQ(ierr);
     if (view) {
         PetscViewer stdoutviewer;
         ierr = PetscViewerASCIIGetStdout(PETSC_COMM_WORLD,&stdoutviewer); CHKERRQ(ierr);
         ierr = UMView(&(user.mesh),stdoutviewer); CHKERRQ(ierr);
-        ierr = PetscObjectSetName((PetscObject)(user.gD),"gD");
-        ierr = VecView(user.gD,stdoutviewer); CHKERRQ(ierr);
-        ierr = PetscObjectSetName((PetscObject)uexact,"uexact");
-        ierr = VecView(uexact,stdoutviewer); CHKERRQ(ierr);
     }
+
+    // fill fields: boundary values and exact solution
+    ierr = UMCreateGlobalVec(&(user.mesh),&uexact); CHKERRQ(ierr);
+    ierr = VecDuplicate(uexact,&(user.gD)); CHKERRQ(ierr);
+    ierr = FillExactAndDirichlet(uexact,user.gD,&user); CHKERRQ(ierr);
 
     // configure SNES
     ierr = VecDuplicate(uexact,&r); CHKERRQ(ierr);
@@ -303,6 +219,7 @@ int main(int argc,char **argv) {
                        err); CHKERRQ(ierr);
 
     // clean-up
+    SNESDestroy(&snes);
     VecDestroy(&(user.gD));  VecDestroy(&uexact);
     VecDestroy(&u);  VecDestroy(&r);
     UMDestroy(&(user.mesh));
