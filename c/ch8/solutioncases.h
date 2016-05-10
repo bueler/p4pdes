ebed9ce9d6a8a272adf0a0e8adce49ae482764ab
Ed Bueler <elbueler@alaska.edu>
2016-05-09 23:47:20 -0800

    improve script to allow setting quadrature degree and solution case as arguments

--- c/ch8/solutioncases.h
+++ c/ch8/solutioncases.h
@@ -5,15 +5,17 @@ double a_lin(double u, double x, double y) {
     return 1.0;
 }
 
-double f_lin(double u, double x, double y) {
-    return 2.0 + 3.0 * y * y;
-}
-
 double uexact_lin(double x, double y) {
     const double y2 = y * y;
     return 1.0 - y2 - 0.25 * y2 * y2;
 }
 
+// manufactured from a_lin(), uexact_lin()
+double f_lin(double u, double x, double y) {
+    return 2.0 + 3.0 * y * y;
+}
+
+// just evaluate exact u on boundary point
 double gD_lin(double x, double y) {
     return uexact_lin(x,y);
 }
@@ -22,6 +24,7 @@ double a_nonlin(double u, double x, double y) {
     return 1.0 + u * u;
 }
 
+// manufactured from a_nonlin(), uexact_lin()
 double f_nonlin(double u, double x, double y) {
     const double dudy = - 2.0 * y - y * y * y;
     return - 2.0 * u * dudy * dudy + (1.0 + u * u) * (2.0 + 3.0 * y * y);
