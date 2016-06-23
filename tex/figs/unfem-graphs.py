a1179211bdcd24dc1d01c830cdfa110c81d55496
Ed Bueler <elbueler@alaska.edu>
2016-06-23 11:48:51 -0800

    Reasonable figures showing error&evals for snesfd runs, and errors for Jacobian runs.  Remains to generate figure with stage times for Jacobian runs.

--- tex/figs/unfem-graphs.py
+++ tex/figs/unfem-graphs.py
@@ -3,33 +3,53 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-names = ['ErrorsEvalsTimes','snes-fd-ErrorsEvalsTimes']
-for n in range(2):
-    eet = np.loadtxt('../timing/unfem/' + names[n])
+def getheet(name):
+    eet = np.loadtxt('../timing/unfem/' + name)
     N = len(eet)
     eet = np.reshape(eet,(N/4,4))
     h = eet[:,0]
     err = eet[:,1]
     evals = np.array(eet[:,2],dtype=int)
     time = eet[:,3]
-    plt.loglog(h,err,'ko',markersize=16.0,markerfacecolor='w')
-    plt.grid(True)
-    plt.xlabel(r'$h$',fontsize=20.0)
-    plt.ylabel('relative error',fontsize=18.0)
-    plt.show()
+    return h, err, evals, time
 
-#filename='plap-conv.pdf'
-#fig = plt.figure(figsize=(7,6))
-#plt.hold(True)
-#plt.text(0.012,4.0e-6,r'$O(h^{%.4f})$' % p[0],fontsize=20.0)
-#plt.axis((1.0e-3,0.35,1.0e-7,3.0e-2))
-#plt.xticks(np.array([0.001, 0.01, 0.1]),
-#                    (r'$0.001$',r'$0.01$',r'$0.1$'),
-#                    fontsize=16.0)
-#plt.yticks(np.array([1.0e-7, 1.0e-6, 1.0e-5, 1.0e-4, 1.0e-3, 1.0e-2]),
-#                    (r'$10^{-7}$',r'$10^{-6}$',r'$10^{-5}$',r'$10^{-4}$',r'$10^{-3}$',r'$10^{-2}$'),
-#                    fontsize=16.0)
-##plt.show()
-#print "writing %s ..." % filename
-#plt.savefig(filename,bbox_inches='tight')
+def saveit(outname):
+    print "writing %s ..." % outname
+    plt.savefig(outname,bbox_inches='tight')
+    plt.clf()
+
+h, err, evals, time = getheet('snes-fd-ErrorsEvalsTimes')
+
+fig, ax1 = plt.subplots()
+ax1.loglog(h,err,'ko',markersize=16.0,markerfacecolor='w')
+ax1.grid(True)
+ax1.set_xlabel(r'$h_{max}$',fontsize=20.0)
+ax1.set_ylabel(r'$\|u-u_{ex}\|_\infty$  (circles)',fontsize=18.0)
+ax2 = ax1.twinx()
+ax2.loglog(h,evals,'k*',markersize=14.0)
+ax2.set_ylabel('FormFunction() evaluations  (stars)',fontsize=16.0)
+ax1.set_xlim(3.0e-2,3.0)
+#plt.show()
+saveit('unfem-snesfd.pdf')
+
+snesfderr = err.copy()
+h, err, evals, time = getheet('ErrorsEvalsTimes')
+p = np.polyfit(np.log(h),np.log(err),1)
+print 'convergence at rate h^%.3f' % p[0]
+
+fig = plt.figure(1)
+plt.loglog(h[:5],snesfderr,'ko',markersize=16.0,markerfacecolor='w')
+plt.grid(True)
+plt.hold(True)
+plt.loglog(h,err,'ko',markersize=10.0,markerfacecolor='k')
+plt.loglog(h,np.exp(p[0]*np.log(h)+p[1]),'k--')
+plt.text(1.8*h[3],1.1*err[3],r'$O(h_{max}^{%.3f})$' % p[0],fontsize=18.0)
+plt.xlabel(r'$h_{max}$',fontsize=20.0)
+plt.ylabel(r'$\|u-u_{ex}\|_\infty$',fontsize=18.0)
+plt.xlim(2.0e-3,3.0)
+plt.hold(False)
+#plt.show()
+saveit('unfem-error.pdf')
+
+#FIXME: figure with stage times: read mesh, assemble, solve
 
