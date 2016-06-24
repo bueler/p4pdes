8ae409f41380d5d5131e2c5e6f76201b1e4d8e1b
Ed Bueler <elbueler@alaska.edu>
2016-06-23 21:26:38 -0800

    Figure showing stage times in unfem.c.

--- tex/figs/unfem-graphs.py
+++ tex/figs/unfem-graphs.py
@@ -22,8 +22,8 @@ def saveit(outname):
 
 h, err, evals, _, _, _ = getstats('snes-fd-ErrorsEvalsTimes')
 
-fig, ax1 = plt.subplots()
-ax1.loglog(h,err,'ko',markersize=16.0,markerfacecolor='w')
+_, ax1 = plt.subplots()
+ax1.loglog(h,err,'ko',markersize=16.0,markeredgewidth=1.5,markerfacecolor='w')
 ax1.grid(True)
 ax1.set_xlabel(r'$h_{max}$',fontsize=20.0)
 ax1.set_ylabel(r'$\|u-u_{ex}\|_\infty$  (circles)',fontsize=18.0)
@@ -39,11 +39,10 @@ h, err, _, rmst, sust, sost = getstats('ErrorsEvalsTimes')
 p = np.polyfit(np.log(h),np.log(err),1)
 print 'convergence at rate h^%.3f' % p[0]
 
-fig = plt.figure(1)
-plt.loglog(h[:5],snesfderr,'ko',markersize=16.0,markerfacecolor='w')
+plt.loglog(h[:5],snesfderr,'ko',markersize=16.0,markeredgewidth=1.5,markerfacecolor='w')
 plt.grid(True)
 plt.hold(True)
-plt.loglog(h,err,'ko',markersize=10.0,markerfacecolor='k')
+plt.loglog(h,err,'ko',markersize=9.0,markerfacecolor='k')
 plt.loglog(h,np.exp(p[0]*np.log(h)+p[1]),'k--')
 plt.text(1.8*h[3],1.1*err[3],r'$O(h_{max}^{%.3f})$' % p[0],fontsize=18.0)
 plt.xlabel(r'$h_{max}$',fontsize=20.0)
@@ -53,5 +52,16 @@ plt.hold(False)
 #plt.show()
 saveit('unfem-error.pdf')
 
-#FIXME: figure with stage times: read mesh, assemble, solve
+plt.loglog(h,rmst,'k^',markersize=12.0,markerfacecolor='w',markeredgewidth=2.0,label='read mesh')
+plt.hold(True)
+plt.loglog(h,sust,'kd',markersize=12.0,markerfacecolor='w',markeredgewidth=2.0,label='set-up')
+plt.loglog(h,sost,'ks',markersize=14.0,markerfacecolor='w',markeredgewidth=2.0,label='solver')
+plt.grid(True)
+plt.xlabel(r'$h_{max}$',fontsize=20.0)
+plt.ylabel('time for stage  (seconds)',fontsize=18.0)
+plt.xlim(2.0e-3,3.0)
+plt.hold(False)
+plt.legend()
+#plt.show()
+saveit('unfem-times.pdf')
 
