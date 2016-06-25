7b0f49f7d7ea7c4b8d7432094b44b743bc94b4c0
Ed Bueler <elbueler@alaska.edu>
2016-06-25 10:30:31 -0800

    update unfem timing to correctly generate meshes, and add case 1,2 runs; will need to rerun on -opt PETSC_ARCH and then commit new timing results files *ErrorsEvalsTimes

--- c/ch7/gentraps.sh
+++ c/ch7/gentraps.sh
@@ -18,10 +18,12 @@ area[6]=0.00005
 area[7]=0.00001
 area[8]=0.000002
 triangle -pqa${area[0]} meshes/$NAME
+./tri2petsc.py meshes/$NAME.1 meshes/$NAME.1
 for (( N=1; N<$LEV; N++ )); do
-    # generate .poly, .node, .ele for meshes
+    # generates $NAME.$((N+1)).poly|.node|.ele
     triangle -rpqa${area[$N]} meshes/$NAME.$N
-    # generate .vec, .is files
-    ./tri2petsc.py meshes/$NAME.$N meshes/$NAME.$N
+    # generates $NAME.$((N+1)).vec|.is
+    OUT=meshes/$NAME.$((N+1))
+    ./tri2petsc.py $OUT $OUT
 done
 
--- tex/timing/unfem/time.sh
+++ tex/timing/unfem/time.sh
@@ -7,28 +7,43 @@ LOC=$1
 function run() {
   rm -f tmp
   set -x
-  $LOC/unfem -log_view -un_mesh $LOC/meshes/trap.$1 $2 &> tmp
+  $LOC/unfem -log_view -un_mesh $LOC/meshes/$1.$3 $4 &> tmp
   set +x
-  grep "|u-u_ex|_inf" tmp | awk '{print $10}' >> $OUT      # h
-  grep "|u-u_ex|_inf" tmp | awk '{print $NF}' >> $OUT      # error
-  grep "SNESFunctionEval" tmp | awk '{print $2}' >> $OUT  # evals
-  grep "Read mesh      :" tmp | awk '{print $5}' >> $OUT  # stage time
-  grep "Set-up         :" tmp | awk '{print $4}' >> $OUT  # stage time
-  grep "Solver         :" tmp | awk '{print $4}' >> $OUT  # stage time
-  rm tmp
+  grep "|u-u_ex|_inf" tmp | awk '{print $10}' >> $2     # h
+  grep "|u-u_ex|_inf" tmp | awk '{print $NF}' >> $2     # error
+  grep "SNESFunctionEval" tmp | awk '{print $2}' >> $2  # evals
+  grep "Read mesh      :" tmp | awk '{print $5}' >> $2  # stage time
+  grep "Set-up         :" tmp | awk '{print $4}' >> $2  # stage time
+  grep "Solver         :" tmp | awk '{print $4}' >> $2  # stage time
 }
 
-(cd $LOC/ && ./gentraps.sh trap 9)
-
-export OUT=snes-fd-ErrorsEvalsTimes
+# case 0
+NAME=trap
+(cd $LOC/ && ./gentraps.sh $NAME 9)
+OUT=snes-fd-ErrorsEvalsTimes
 rm -f $OUT
 for N in 1 2 3 4 5; do
-    run $N "-snes_fd"
+    run $NAME $OUT $N "-snes_fd"
 done
-
-export OUT=ErrorsEvalsTimes
+OUT=ErrorsEvalsTimes
 rm -f $OUT
 for N in 1 2 3 4 5 6 7 8 9; do
-    run $N
+    run $NAME $OUT $N
+done
+
+#case 1
+export OUT=case1-ErrorsEvalsTimes
+rm -f $OUT
+for N in 1 2 3 4 5 6 7 8 9; do
+    run $NAME $OUT $N "-un_case 1"
+done
+
+#case 2
+NAME=trapneu
+(cd $LOC/ && ./gentraps.sh $NAME 9)
+export OUT=case2-ErrorsEvalsTimes
+rm -f $OUT
+for N in 1 2 3 4 5 6 7 8 9; do
+    run $NAME $OUT $N "-un_case 2"
 done
 
