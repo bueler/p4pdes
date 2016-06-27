f7ff4f5a3e3b2d94cb5eaffdcd6ac31b6e9104ee
Ed Bueler <elbueler@alaska.edu>
2016-06-27 14:07:52 -0800

    Better figures for -snes_fd|mf runs, and completion of text on initial testing

--- tex/figs/unfem-graphs.py
+++ tex/figs/unfem-graphs.py
@@ -3,54 +3,72 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# returns:
+#     N = number of runs
+#     JEpresent = True if "Jacobian eval" present
+#     h = array of N maximum triangle side lengths
+#     err = array of N numerical errors (inf norm)
+#     evals = integer array of N residual-fcn evaluations (SNESFunctionEval)
+#     solver = array of N "Solver" stage times
+#     ost = Nx5 array of OTHER stage times; each line is
+#         Read mesh, Set-up, Residual eval[, Jacobian eval]
 def getstats(name):
     eet = np.loadtxt('../timing/unfem/' + name)
-    N = len(eet)
-    eet = np.reshape(eet,(N/6,6))
+    N = np.shape(eet)[0]
+    JEpresent = (np.shape(eet)[1] == 8)
     h = eet[:,0]
     err = eet[:,1]
     evals = np.array(eet[:,2],dtype=int)
-    readmeshstagetime = eet[:,3]
-    setupstagetime = eet[:,4]
-    solverstagetime = eet[:,5]
-    return h, err, evals, readmeshstagetime, setupstagetime, solverstagetime
+    solver = eet[:,5]
+    if JEpresent:
+        ost = eet[:,(3,4,6,7)]
+    else:
+        ost = eet[:,(3,4,6)]
+    return N, JEpresent, h, err, evals, solver, ost
 
 def saveit(outname):
     print "writing %s ..." % outname
     plt.savefig(outname,bbox_inches='tight')
     plt.clf()
 
-h, err, evals, _, _, _ = getstats('snes-fd-ErrorsEvalsTimes')
+_, _, hfd, errfd, evalsfd, _, _ = getstats('snes-fd-ErrorsEvalsTimes')
+_, _, hmf, errmf, evalsmf, _, _ = getstats('snes-mf-ErrorsEvalsTimes')
 
-# figure for -snes_fd run with both errors and evals
+# figure for -snes_fd, -snes_mf runs with both errors and evals
 _, ax1 = plt.subplots()
-ax1.loglog(h,err,'ko',markersize=16.0,markeredgewidth=2.0,markerfacecolor='w')
+ax1.loglog(hmf,errmf,'ko',markersize=16.0,markeredgewidth=2.0,markerfacecolor='w')
+#on top: ax1.loglog(hfd,errfd,'ks',markersize=12.0,markeredgewidth=2.0,markerfacecolor='w')
 ax1.grid(True)
 ax1.set_xlabel(r'$h$',fontsize=20.0)
 ax1.set_ylabel(r'$\|u-u_{ex}\|_\infty$  (circles)',fontsize=18.0)
 ax2 = ax1.twinx()
-ax2.loglog(h,evals,'k*',markersize=14.0,markeredgewidth=2.0)
-ax2.set_ylabel('FormFunction() evaluations  (stars)',fontsize=16.0)
-ax1.set_xlim(3.0e-2,3.0)
-#plt.show()
-saveit('unfem-snesfd.pdf')
+ax2.loglog(hfd,evalsfd,'k*',markersize=16.0,markeredgewidth=2.0,label=r'-snes_fd')
+ax2.hold(True)
+ax2.loglog(hmf,evalsmf,'ks',markersize=12.0,markeredgewidth=2.0,label=r'-snes_mf')
+ax2.set_ylabel('FormFunction() evaluations  (solid)',fontsize=16.0)
+ax2.hold(False)
+ax2.legend(loc='lower center')
+ax1.set_xlim(1.0e-2,3.0)
+saveit('unfem-fdmf.pdf')
+
+_, _, h0, err0, _, sost, ost = getstats('ErrorsEvalsTimes')
+ost = ost.sum(1)
+_, _, h1, err1, _, _, _ = getstats('case1-ErrorsEvalsTimes')
+_, _, h2, err2, _, _, _ = getstats('case2-ErrorsEvalsTimes')
 
 # figure for case 0,1,2 errors
-h0, err0, _, rmst, sust, sost = getstats('ErrorsEvalsTimes')
 p = np.polyfit(np.log(h0[2:-1]),np.log(err0[2:-1]),1)
 print 'convergence for case 0 at rate h^%.3f' % p[0]
 plt.loglog(h0,err0,'ko',markersize=9.0,markerfacecolor='k',
            label=r'case 0  $O(h^{%.3f})$' % p[0])
 plt.hold(True)
-h, err, _, _, _, _ = getstats('case1-ErrorsEvalsTimes')
-p = np.polyfit(np.log(h[2:-1]),np.log(err[2:-1]),1)
+p = np.polyfit(np.log(h1[2:-1]),np.log(err1[2:-1]),1)
 print 'convergence for case 1 at rate h^%.3f' % p[0]
-plt.loglog(h,err,'ks',markersize=14.0,markerfacecolor='w',alpha=1.0,
+plt.loglog(h1,err1,'ks',markersize=14.0,markerfacecolor='w',alpha=1.0,
            label=r'case 1  $O(h^{%.3f})$' % p[0])
-h, err, _, _, _, _ = getstats('case2-ErrorsEvalsTimes')
-p = np.polyfit(np.log(h[2:-1]),np.log(err[2:-1]),1)
+p = np.polyfit(np.log(h2[2:-1]),np.log(err2[2:-1]),1)
 print 'convergence for case 2 at rate h^%.3f' % p[0]
-plt.loglog(h,err,'kd',markersize=14.0,markerfacecolor='w',alpha=1.0,
+plt.loglog(h2,err2,'kd',markersize=14.0,markerfacecolor='w',alpha=1.0,
            label=r'case 2  $O(h^{%.3f})$' % p[0])
 plt.loglog(h0,err0,'ko',markersize=9.0,markerfacecolor='k')
 plt.hold(False)
@@ -59,19 +77,16 @@ plt.legend(loc='upper left')
 plt.xlabel(r'$h$',fontsize=20.0)
 plt.ylabel(r'$\|u-u_{ex}\|_\infty$',fontsize=18.0)
 plt.xlim(2.0e-3,3.0)
-#plt.show()
 saveit('unfem-error.pdf')
 
-plt.loglog(h,rmst,'kx',markersize=12.0,markeredgewidth=2.0,label='read mesh')
+plt.loglog(h0,sost,'k*',markersize=14.0,markeredgewidth=2.0,label='solver')
 plt.hold(True)
-plt.loglog(h,sust,'k+',markersize=14.0,markeredgewidth=2.0,label='set-up')
-plt.loglog(h,sost,'k*',markersize=14.0,markeredgewidth=2.0,label='solver')
+plt.loglog(h0,ost,'kx',markersize=12.0,markeredgewidth=2.0,label='other stages')
 plt.grid(True)
 plt.xlabel(r'$h$',fontsize=20.0)
 plt.ylabel('time for stage  (seconds)',fontsize=18.0)
 plt.xlim(2.0e-3,3.0)
 plt.hold(False)
 plt.legend()
-#plt.show()
 saveit('unfem-times.pdf')
 
