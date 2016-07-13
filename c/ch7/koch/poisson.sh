3bd8c0f835a4ee7007c3ae16fb5d5f9c36cc4d82
Ed Bueler <elbueler@alaska.edu>
2016-07-13 14:09:30 -0800

    minor improvements

--- c/ch7/koch/poisson.sh
+++ c/ch7/koch/poisson.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-./mesh.py -l 5
-triangle -pqa0.00002 koch.poly
+LEVEL=5
+ELEMENTAREA=0.00002
+CONTOURS="1e-6 1e-5 3e-5 1e-4 3e-4 0.001 0.003 0.01 0.02 0.03 0.05 0.1"
+
+# triangulate the Koch snowflake
+./mesh.py -l $LEVEL
+triangle -pqa$ELEMENTAREA koch.poly
 #showme koch
+
+# solve the Poisson equation on it
 ../tri2petsc.py koch.1 koch.1
 ../unfem -un_mesh koch.1 -un_case 4 -un_view_solution \
     -snes_monitor -ksp_converged_reason
-../petsc2contour.py -i koch.1 -o snowflake.pdf \
-    --contours 0.00001 0.00003 0.0001 0.0003 0.001 0.003 0.01 0.02 0.03 0.05 0.1
+
+# generate contour plot
+../petsc2contour.py -i koch.1 -o snowflake.pdf --contours $CONTOURS
 
