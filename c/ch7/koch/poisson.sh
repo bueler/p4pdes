23f79c69db9656010bb537ffae6ab5e0212c63fb
Ed Bueler <elbueler@alaska.edu>
2016-07-14 10:42:04 -0800

    sensible rename

--- c/ch7/koch/poisson.sh
+++ c/ch7/koch/poisson.sh
@@ -5,7 +5,7 @@ ELEMENTAREA=0.00002
 CONTOURS="1e-6 1e-5 3e-5 1e-4 3e-4 0.001 0.003 0.01 0.02 0.03 0.05 0.1"
 
 # triangulate the Koch snowflake
-./mesh.py -l $LEVEL
+./polygon.py -l $LEVEL
 triangle -pqa$ELEMENTAREA koch.poly
 #showme koch
 
