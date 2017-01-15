9cc280fbc3ac4a81a13c332ab092001f96b5d2a1
Ed Bueler <elbueler@alaska.edu>
2017-01-15 10:18:48 -0900

    accomplished both goals mentioned in last commit message; works in parallel and generates symmetric matrix in laplace case

--- c/ch10/minimal.c
+++ c/ch10/minimal.c
@@ -9,26 +9,36 @@ static char help[] =
 "Allows re-use of Jacobian (Laplacian) from fish2 as preconditioner.\n"
 "Multigrid-capable.\n\n";
 
+/* evidence of parallel:
+
+timer mpiexec -n N ./minimal -snes_fd_color -snes_converged_reason -da_refine 6 -pc_type mg -snes_max_it 200
+Nonlinear solve converged due to CONVERGED_FNORM_RELATIVE iterations 34
+
+gives 4.0 sec on N=1 and 2.1 sec on N=2
+*/
+
 #include <petsc.h>
 #include "jacobians.c"
 #define COMM PETSC_COMM_WORLD
+
+
+// FIXME   add RHS f(x,y) and manufacture a solution
+
+// FIXME   write computeArea()
+
 
 typedef struct {
     double    H;       // height of tent along y=0 boundary
     PetscBool laplace; // solve Laplace equation instead of minimal surface
 } MinimalCtx;
 
+// Dirichlet boundary condition along y=0 boundary
 double GG(double H, double x) {
     return 2.0 * H * (x < 0.5 ? x : (1.0 - x));
 }
 
-// FIXME   make it work in parallel
-
-// FIXME   add RHS f(x,y) and manufacture a solution
-
-// FIXME   write computeArea()
-
-// the diffusivity as a function of  z = |nabla u|^2
+// the coefficient (diffusivity) of minimal surface equation, as a function
+//   of  z = |nabla u|^2
 double DD(double z) { 
     return 1.0 / sqrt(1.0 + z);
 }
@@ -37,11 +47,14 @@ PetscErrorCode FormFunctionLocal(DMDALocalInfo *info, double **au,
                                  double **FF, MinimalCtx *user) {
     PetscErrorCode ierr;
     int          i, j;
-    double       xymin[2], xymax[2], hx, hy, x,
-                 dux, duy, De, Dw, Dn, Ds, **av;
+    double       xymin[2], xymax[2], hx, hy, hxhy, hyhx, x,
+                 ue, uw, un, us, une, use, unw, usw,
+                 dux, duy, De, Dw, Dn, Ds;
     ierr = DMDAGetBoundingBox(info->da,xymin,xymax); CHKERRQ(ierr);
     hx = (xymax[0] - xymin[0]) / (info->mx - 1);
     hy = (xymax[1] - xymin[1]) / (info->my - 1);
+    hxhy = hx / hy;
+    hyhx = hy / hx;
     for (j = info->ys; j < info->ys + info->ym; j++) {
         for (i = info->xs; i < info->xs + info->xm; i++) {
             x = xymin[0] + i * hx;
@@ -50,40 +63,44 @@ PetscErrorCode FormFunctionLocal(DMDALocalInfo *info, double **au,
             } else if (i==0 || i==info->mx-1 || j==info->my-1) {
                 FF[j][i] = au[j][i];
             } else {
-                if (i+1 == info->mx-1) {
-                    ue 
-                }
-FIXME  too sleepy to decide how to do this
-                ue = (i+1 == info->mx-1) ? 0.0 : au[j][i+1];
-                uw = (i-1 == 0)          ? 0.0 : au[j][i-1];
-                un = (j+1 == info->my-1) ? 0.0 : au[j+1][i];
-                us = (j-1 == 0)          ? GG(user->H,xs) : au[j-1][i];
+                // assign either boundary condition or current value at
+                //     neighboring points (esp. for -snes_fd symmetric matrix)
+                ue  = (i+1 == info->mx-1) ? 0.0 : au[j][i+1];
+                uw  = (i-1 == 0)          ? 0.0 : au[j][i-1];
+                un  = (j+1 == info->my-1) ? 0.0 : au[j+1][i];
+                us  = (j-1 == 0)          ? GG(user->H,x) : au[j-1][i];
                 if (user->laplace) {
                     De = 1.0;  Dw = 1.0;
                     Dn = 1.0;  Ds = 1.0;
                 } else {
+                    une = ((i+1 == info->mx-1) || (j+1 == info->my-1))
+                                              ? 0.0 : au[j+1][i+1];
+                    use = ((i+1 == info->mx-1) || (j-1 == 0))
+                                              ? 0.0 : au[j-1][i+1];
+                    unw = ((i-1 == 0) || (j+1 == info->my-1))
+                                              ? 0.0 : au[j+1][i-1];
+                    usw = ((i-1 == 0) || (j-1 == 0))
+                                              ? 0.0 : au[j-1][i-1];
                     // gradient of u squared at east point  (i+1/2,j):
-                    dux = (av[j][i+1] - av[j][i]) / hx;
-                    duy = (av[j+1][i] + av[j+1][i+1] - av[j-1][i] - av[j-1][i+1]) / (4.0 * hy);
+                    dux = (ue - au[j][i]) / hx;
+                    duy = (un + une - us - use) / (4.0 * hy);
                     De = DD(dux * dux + duy * duy);
                     // ...                   at west point  (i-1/2,j):
-                    dux = (av[j][i] - av[j][i-1]) / hx;
-                    duy = (av[j+1][i-1] + av[j+1][i] - av[j-1][i-1] - av[j-1][i]) / (4.0 * hy);
+                    dux = (au[j][i] - uw) / hx;
+                    duy = (unw + un - usw - us) / (4.0 * hy);
                     Dw = DD(dux * dux + duy * duy);
                     // ...                  at north point  (i,j+1/2):
-                    dux = (av[j][i+1] + av[j+1][i+1] - av[j][i-1] - av[j+1][i-1]) / (4.0 * hx);
-                    duy = (av[j+1][i] - av[j][i]) / hy;
+                    dux = (ue + une - uw - unw) / (4.0 * hx);
+                    duy = (un - au[j][i]) / hy;
                     Dn = DD(dux * dux + duy * duy);
                     // ...                  at south point  (i,j-1/2):
-                    dux = (av[j][i+1] + av[j-1][i+1] - av[j][i-1] - av[j-1][i-1]) / (4.0 * hx);
-                    duy = (av[j][i] - av[j-1][i]) / hy;
+                    dux = (ue + use - uw - use) / (4.0 * hx);
+                    duy = (au[j][i] - us) / hy;
                     Ds = DD(dux * dux + duy * duy);
                 }
                 // evaluate residual
-                FF[j][i] = - hy/hx * (   De * (av[j][i+1] - av[j][i])
-                                       - Dw * (av[j][i] - av[j][i-1]) )
-                           - hx/hy * (   Dn * (av[j+1][i] - av[j][i])
-                                       - Ds * (av[j][i] - av[j-1][i]) );
+                FF[j][i] = - hyhx * (De * (ue - au[j][i]) - Dw * (au[j][i] - uw))
+                           - hxhy * (Dn * (un - au[j][i]) - Ds * (au[j][i] - us));
             }
         }
     }
@@ -109,9 +126,6 @@ int main(int argc,char **argv) {
                             "minimal.c",user.laplace,&(user.laplace),NULL);CHKERRQ(ierr);
     ierr = PetscOptionsEnd(); CHKERRQ(ierr);
 
-    //FIXME add option between pure laplacian and minimal surface equations, with
-    //      comparison of surface areas for same boundary conditions, via objective
-
     ierr = DMDACreate2d(COMM,
                         DM_BOUNDARY_NONE, DM_BOUNDARY_NONE,
                         DMDA_STENCIL_BOX,  // contrast with fish2
@@ -121,7 +135,6 @@ int main(int argc,char **argv) {
     ierr = DMSetUp(da); CHKERRQ(ierr);  // this must be called BEFORE SetUniformCoordinates
     ierr = DMSetApplicationContext(da,&user);CHKERRQ(ierr);
     ierr = DMDASetUniformCoordinates(da,0.0,1.0,0.0,1.0,0.0,1.0);CHKERRQ(ierr);
-
     ierr = DMCreateGlobalVector(da,&u);CHKERRQ(ierr);
     ierr = PetscObjectSetName((PetscObject)u,"u");CHKERRQ(ierr);
 
@@ -129,8 +142,8 @@ int main(int argc,char **argv) {
     ierr = SNESSetDM(snes,da); CHKERRQ(ierr);
     ierr = DMDASNESSetFunctionLocal(da,INSERT_VALUES,
                (DMDASNESFunction)FormFunctionLocal,&user); CHKERRQ(ierr);
-    // this is the Jacobian of the Poisson equation, thus only approximate;
-    // consider using -snes_mf_operator
+    // this is the Jacobian of the Poisson equation, thus only approximate
+    //     (consider using -snes_mf_operator)
     ierr = DMDASNESSetJacobianLocal(da,
                (DMDASNESJacobian)Form2DJacobianLocal,&user); CHKERRQ(ierr);
     ierr = SNESSetFromOptions(snes); CHKERRQ(ierr);
