4f2be2f34c4e391471729d50578cb90c81bb9c32
Ed Bueler <elbueler@alaska.edu>
2017-03-01 11:23:48 -0500

    most of script for dt performance slide

---
+++ c/ch11/study/fiftyka/figure.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# see fiftyka.slurm for run script
+
+# the base run:  defaults: -ts_type arkimex -ts_arkimex_type 3 -snes_type vinewtonrsls -ksp_type gmres
+basic = "./ice -da_refine $LEV -pc_type mg -pc_mg_levels $(( $LEV - 2 )) -ice_dtlimits -ice_tf 50000 -ice_dtinit 1.0 -ts_max_snes_failures -1 -ts_adapt_scale_solve_failed 0.9 -ice_dump -snes_converged_reason"
+
+lev = [6,7,8,9]
+L = 1800.0e3
+dur = 50000.0
+
+# mx, steps, dtD, dtCFL
+raw = [[192,  1496, 2.667,   46.88],
+       [384,  1924, 0.6866,  23.44],
+       [768,  2167, 0.1718,  11.72],
+       [1536, 2517, 0.04297, 5.859]]
+data = np.array(raw)
+
+mx = data[:,0]
+m = mx * mx
+h = L / mx
+dtactual = dur / data[:,1]
+dtFE = data[:,2]
+dtCFL = data[:,3]
+
+def writeit(fname):
+    print 'saving figure %s' % fname
+    plt.savefig(fname,bbox_inches='tight')
+
+plt.figure(1)
+plt.semilogy(lev,dtactual,'o',ms=12,label=r'average $\Delta t$ from ARKIMEX(3)')
+plt.semilogy(lev,dtFE,'*',ms=12,label=r'$\Delta t_{FE}$')
+plt.semilogy(lev,dtCFL,'s',ms=12,label=r'$\Delta t_{CFL}$')
+plt.grid('on')
+xticlabel = []
+for k in range(4):
+    xticlabel.append('%.1f' % (h[k] / 1000.0))
+plt.xticks(lev,xticlabel)
+plt.xlabel('h  (km)')
+plt.ylabel('time step (a)')
+#plt.axis([8.8,12.2,10.0,70.0])  FIXME
+#plt.yticks([20, 40, 60],['20','40','60'])
+plt.legend(fontsize=12,loc='upper right')
+#writeit('timepernewtonweak.pdf')
+
+plt.show()
+
