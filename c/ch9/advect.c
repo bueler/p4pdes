efaa6fb9c5ad93b72fdd8d167274c0f437d92d23
Ed Bueler <elbueler@alaska.edu>
2017-03-28 13:39:05 -0800

    initial working implementation with first-order upwind and no Jacobian

--- c/ch9/advect.c
+++ c/ch9/advect.c
@@ -7,20 +7,79 @@ static char help[] =
 
 #include <petsc.h>
 
+// try:
+//   ./advect -da_refine 5 -ts_monitor_solution draw -ts_monitor -ts_rk_type 5dp
+
+// exercise: evaluate error by reusing FormInitial()
+
 typedef struct {
-    PetscBool  circlewind;  // if true, wind is equivalent to rigid rotation
-    double     angle;       // angle of wind relative to right-ward, in degrees
+    PetscBool  circlewind;   // if true, wind is equivalent to rigid rotation
+    double     windx, windy, // x,y components of wind (if not circular)
+               conex, coney, coner, coneh; // parameters for cone initial cond.
 } AdvectCtx;
 
-/* method-of-lines discretization gives ODE system  u' = G(t,u) */
-PetscErrorCode FormRHSFunctionLocal(DMDALocalInfo *info, double t, double **au,
-                                    double **aG, AdvectCtx *user) {
-    int      i, j;
-    for (j = info->ys; j < info->ys + info->ym; j++) {
-        for (i = info->xs; i < info->xs + info->xm; i++) {
-            aG[j][i] = FIXME;
+PetscErrorCode FormInitial(DMDALocalInfo *info, Vec u, AdvectCtx* user) {
+    PetscErrorCode ierr;
+    DMDACoor2d   **coords;
+    int          i, j;
+    double       x, y, r, **au;
+
+    ierr = VecSet(u,0.0); CHKERRQ(ierr);  // clear it first
+    ierr = DMDAGetCoordinateArray(info->da, &coords); CHKERRQ(ierr);
+    ierr = DMDAVecGetArray(info->da, u, &au); CHKERRQ(ierr);
+    for (j=info->ys; j<info->ys+info->ym; j++) {
+        for (i=info->xs; i<info->xs+info->xm; i++) {
+            x = coords[j][i].x - user->conex;
+            y = coords[j][i].y - user->coney;
+            r = PetscSqrtReal(x * x + y * y);
+            if (r < user->coner) {
+                au[j][i] = user->coneh * (1.0 - r / user->coner);
+            }
         }
     }
+    ierr = DMDAVecRestoreArray(info->da, u, &au); CHKERRQ(ierr);
+    ierr = DMDARestoreCoordinateArray(info->da, &coords); CHKERRQ(ierr);
+    return 0;
+}
+
+static double a_wind(double x, double y, int dir, AdvectCtx* user) {
+    if (user->circlewind) {
+        return (dir == 0) ? -y : x;
+    } else {
+        return (dir == 0) ? user->windx : user->windy;
+    }
+}
+
+static double g_source(double x, double y, double u, AdvectCtx* user) {
+    return 0.0;
+}
+
+/* method-of-lines discretization gives ODE system  u' = G(t,u)
+FIXME only first-order upwind for now */
+PetscErrorCode FormRHSFunctionLocal(DMDALocalInfo *info, double t, double **au,
+                                    double **aG, AdvectCtx *user) {
+    PetscErrorCode ierr;
+    DMDACoor2d   **coords;
+    int          i, j;
+    double       hx, hy, x, y, a, fluxL, fluxR, fluxN, fluxS;
+    ierr = DMDAGetCoordinateArray(info->da, &coords); CHKERRQ(ierr);
+    for (j = info->ys; j < info->ys + info->ym; j++) {
+        for (i = info->xs; i < info->xs + info->xm; i++) {
+            hx = 1.0 / info->mx;  hy = 1.0 / info->my;
+            x = coords[j][i].x;  y = coords[j][i].y;
+            a = a_wind(x + hx/2,y,0,user);
+            fluxR = a * ( (a >= 0) ? au[j][i] : au[j][i+1] );
+            a = a_wind(x - hx/2,y,0,user);
+            fluxL = a * ( (a >= 0) ? au[j][i-1] : au[j][i] );
+            a = a_wind(x,y + hy/2,0,user);
+            fluxN = a * ( (a >= 0) ? au[j][i] : au[j+1][i] );
+            a = a_wind(x,y - hy/2,0,user);
+            fluxS = a * ( (a >= 0) ? au[j-1][i] : au[j][i] );
+            aG[j][i] = - (fluxR - fluxL) / hx - (fluxN - fluxS) / hy
+                       + g_source(x,y,au[j][i],user);
+        }
+    }
+    ierr = DMDARestoreCoordinateArray(info->da, &coords); CHKERRQ(ierr);
     return 0;
 }
 
@@ -35,11 +94,26 @@ int main(int argc,char **argv) {
 
     PetscInitialize(&argc,&argv,(char*)0,help);
 
+    user.circlewind = PETSC_FALSE;
+    user.windx = 1.0;
+    user.windy = 1.0;
+
+    // the cone initial condition is for reproducing results from
+    // Hundsdorfer & Verwer, "Numerical Solution of Time-Dependent Advection-
+    // Diffusion-Reaction Equations", Springer 2003, page 303
+    // ... these could be set from options
+    user.conex = 0.2;
+    user.coney = 0.2;
+    user.coner = 0.1;
+    user.coneh = 1.0;
+
     ierr = PetscOptionsBegin(PETSC_COMM_WORLD, "adv_", "options for advect.c", ""); CHKERRQ(ierr);
-    ierr = PetscOptionsReal("-angle","angle of wind relative to right-ward, in degrees",
-           "advect.c",user.angle,&user.angle,NULL);CHKERRQ(ierr);
     ierr = PetscOptionsBool("-circlewind","if true, wind is equivalent to rigid rotation",
            "advect.c",user.circlewind,&user.circlewind,NULL);CHKERRQ(ierr);
+    ierr = PetscOptionsReal("-windx","x component of wind (if not circular)",
+           "advect.c",user.windx,&user.windx,NULL);CHKERRQ(ierr);
+    ierr = PetscOptionsReal("-windy","y component of wind (if not circular)",
+           "advect.c",user.windy,&user.windy,NULL);CHKERRQ(ierr);
     ierr = PetscOptionsEnd(); CHKERRQ(ierr);
 
     ierr = DMDACreate2d(PETSC_COMM_WORLD,
@@ -62,10 +136,11 @@ int main(int argc,char **argv) {
     ierr = TSSetDM(ts,da); CHKERRQ(ierr);
     ierr = DMDATSSetRHSFunctionLocal(da,INSERT_VALUES,
            (DMDATSRHSFunctionLocal)FormRHSFunctionLocal,&user); CHKERRQ(ierr);
-    ierr = TSSetType(ts,TSRK2A); CHKERRQ(ierr);
+    ierr = TSSetType(ts,TSRK); CHKERRQ(ierr);
+    ierr = TSRKSetType(ts,TSRK2A); CHKERRQ(ierr);
     ierr = TSSetExactFinalTime(ts,TS_EXACTFINALTIME_MATCHSTEP); CHKERRQ(ierr);
     ierr = TSSetInitialTimeStep(ts,0.0,0.1); CHKERRQ(ierr);
-    ierr = TSSetDuration(ts,1000000,1.0); CHKERRQ(ierr);
+    ierr = TSSetDuration(ts,1000000,0.6); CHKERRQ(ierr);
     ierr = TSSetFromOptions(ts);CHKERRQ(ierr);
 
     ierr = TSGetTime(ts,&t0); CHKERRQ(ierr);
@@ -76,7 +151,7 @@ int main(int argc,char **argv) {
            info.mx,info.my,hx,hy,t0,dt); CHKERRQ(ierr);
 
     ierr = DMCreateGlobalVector(da,&u); CHKERRQ(ierr);
-    ierr = VecSet(u,0.0); CHKERRQ(ierr);   // initial condition  FIXME cones
+    ierr = FormInitial(&info,u,&user); CHKERRQ(ierr);
     ierr = TSSolve(ts,u); CHKERRQ(ierr);
 
     VecDestroy(&u);  TSDestroy(&ts);  DMDestroy(&da);
