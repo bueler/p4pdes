f94f6526f89f02bf48dfa43e8994f7b2d8bf148e
Ed Bueler <elbueler@alaska.edu>
2017-04-07 17:44:28 -0800

    change to square (-1,1)x(-1,1) and use enum for problem type ... now reproduces Fig 6.2 in Hundsdorfer&Verwer and Fig 20.5 in LeVeque

--- c/ch9/advect.c
+++ c/ch9/advect.c
@@ -1,31 +1,28 @@
 static char help[] =
 "Time-dependent pure-advection equation, in flux-conservative form, in 2D\n"
-"using TS.  Option prefix -adv_.\n"
-"Domain is (0,1) x (0,1).  Equation is\n"
+"using TS.  Option prefix -adv_.  Domain is (-1,1) x (-1,1).  Equation is\n"
 "  u_t + div(a(x,y) u) = g(x,y,u).\n"
 "Boundary conditions are periodic in x and y.  Cells are grid-point centered.\n"
 "Uses flux-limited (non-oscillatory) method-of-lines discretization\n"
 "(Hundsdorfer & Verwer 2003). Limiters are van Leer (1974) [default],\n"
-"Koren (1993), centered, or NONE (= first-order upwind).\n\n";
+"Koren (1993), centered, or none (= first-order upwind).\n\n";
 
 #include <petsc.h>
 
 // try:
-//   ./advect -da_refine 5 -ts_monitor_solution draw -ts_monitor -ts_rk_type 5dp -adv_limiter KOREN
+//   ./advect -da_refine 5 -ts_monitor_solution draw -ts_monitor -ts_rk_type 5dp -adv_limiter koren
 
-// one lap of circular motion, computed in parallel:
-//   mpiexec -n 4 ./advect -da_refine 5 -adv_circlewind -adv_conex 0.3 -adv_coney 0.3 -ts_final_time 3.1416 -ts_monitor -ts_monitor_solution draw
+// one lap of circular motion, computed in parallel, reproducing LeVeque Figure 20.5:
+//   mpiexec -n 4 ./advect -da_grid_x 80 -da_grid_y 80 -ts_monitor_solution draw -ts_monitor -ts_rk_type 2a -adv_problem rotation -ts_final_time 3.1415926
 
-// implicit and evidence that smoother limiter is better: succeeds with XX=NONE,VANLEER
-// but not with XX=KOREN; note removing -snes_fd_color helps sort of
-// mpiexec -n 4 ./advect -ts_monitor_solution draw -ts_monitor -adv_circlewind -ts_final_time 0.2 -adv_conex 0.3 -adv_coney 0.3 -ts_type cn -da_refine 6 -snes_monitor -ts_dt 0.02 -snes_fd_color -adv_limiter XX
+// above is really fast with -ts_rk_type 5f|5dp|3bs
 
-// with -adv_limiter NONE, -snes_type test suggests Jacobian is correct
+// implicit and evidence that smoother limiter is better: succeeds with XX=none,centered,vanleer; big iteration counts and then fails for koren; note "-snes_mf_operator" works with vanleer but hangs with koren
+// mpiexec -n 4 ./advect -ts_monitor_solution draw -ts_monitor -adv_problem rotation -ts_final_time 0.2 -ts_type cn -da_refine 6 -snes_monitor -ts_dt 0.02 -snes_fd_color -adv_limiter XX
 
-// testing Jacobian option -snes_mf_operator
-// (FAILS with "No support for this operation for this object type ...
-// Not written for this matrix type"  see petsc-users):
-// ./advect -da_refine 1 -ts_monitor -adv_circlewind -adv_conex 0.3 -adv_coney 0.3 -ts_type beuler -snes_monitor_short -ts_final_time 0.01 -ts_dt 0.01 -snes_mf_operator
+// with -adv_limiter none, -snes_type test suggests Jacobian is correct
+
+// see petsc pull request https://bitbucket.org/petsc/petsc/pull-requests/662/barry-fix-even-huger-flaw-in-ts/diff regarding -snes_mf_operator
 
 //STARTLIMITER
 /* the centered-space method is a trivial (and poor) limiter */
@@ -46,46 +43,70 @@ static double koren(double th) {
 }
 
 typedef enum {NONE, CENTERED, VANLEER, KOREN} LimiterType;
-static const char *LimiterTypes[] = {"NONE","CENTERED","VANLEER","KOREN",
+static const char *LimiterTypes[] = {"none","centered","vanleer","koren",
                                      "LimiterType", "", NULL};
 static void* limiterptr[] = {NULL, &centered, &vanleer, &koren};
 //ENDLIMITER
 
+//STARTCTX
+typedef enum {ROTATION, STRAIGHT} ProblemType;
+static const char *ProblemTypes[] = {"rotation","straight",
+                                     "ProblemType", "", NULL};
+
 typedef struct {
-    PetscBool  circlewind;   // if true, wind is equivalent to rigid rotation
-    double     windx, windy, // x,y components of wind (if not circular)
-               conex, coney, coner, coneh; // parameters for cone initial cond.
-    double     (*limiter)(double);
+    ProblemType problem;
+    double      windx, windy; // x,y components of wind (if problem = STRAIGHT)
+    double      (*limiter)(double);
 } AdvectCtx;
+//ENDCTX
 
 PetscErrorCode FormInitial(DMDALocalInfo *info, Vec u, AdvectCtx* user) {
     PetscErrorCode ierr;
-    DMDACoor2d   **coords;
     int          i, j;
-    double       x, y, r, **au;
+    double       hx, hy, x, y, r, **au;
 
     ierr = VecSet(u,0.0); CHKERRQ(ierr);  // clear it first
-    ierr = DMDAGetCoordinateArray(info->da, &coords); CHKERRQ(ierr);
     ierr = DMDAVecGetArray(info->da, u, &au); CHKERRQ(ierr);
+    hx = 2.0 / info->mx;  hy = 2.0 / info->my;
     for (j=info->ys; j<info->ys+info->ym; j++) {
+        y = -1.0 + (j+0.5) * hy;
         for (i=info->xs; i<info->xs+info->xm; i++) {
-            x = coords[j][i].x - user->conex;
-            y = coords[j][i].y - user->coney;
-            r = PetscSqrtReal(x * x + y * y);
-            if (r < user->coner) {
-                au[j][i] = user->coneh * (1.0 - r / user->coner);
+            x = -1.0 + (i+0.5) * hx;
+            switch (user->problem) {
+                case STRAIGHT:
+                    // goal: reproduce Figure 6.2, page 303, in
+                    // Hundsdorfer & Verwer (2003). "Numerical Solution of
+                    // Time-Dependent Advection-Diffusion-Reaction Equations",
+                    // Springer; but scaled by factor of 2
+                    r = PetscSqrtReal((x+0.6)*(x+0.6) + (y+0.6)*(y+0.6));
+                    if (r < 0.2) {
+                        au[j][i] = 1.0;
+                    }
+                    break;
+                case ROTATION:
+                    // goal: reproduce Figure 20.5, page 461, in
+                    // LeVeque (2002). "Finite Volume Methods for Hyperbolic
+                    // Problems", Cambridge
+                    r = PetscSqrtReal((x+0.45)*(x+0.45) + y*y);
+                    if ((0.1 < x) && (x < 0.6) && (-0.25 < y) && (y < 0.25)) {
+                        au[j][i] = 1.0;
+                    } else if (r < 0.35) {
+                        au[j][i] = 1.0 - r / 0.35;
+                    }
+                    break;
+                default:
+                    SETERRQ(PETSC_COMM_WORLD,1,"invalid user->problem\n");
             }
         }
     }
     ierr = DMDAVecRestoreArray(info->da, u, &au); CHKERRQ(ierr);
-    ierr = DMDARestoreCoordinateArray(info->da, &coords); CHKERRQ(ierr);
     return 0;
 }
 
 // velocity  a(x,y) = ( a^x(x,y), a^y(x,y) )
 static double a_wind(double x, double y, int dir, AdvectCtx* user) {
-    if (user->circlewind) {
-        return (dir == 0) ? - 2.0 * (y - 0.5) : 2.0 * (x - 0.5);
+    if (user->problem == ROTATION) {
+        return (dir == 0) ? 2.0 * y : - 2.0 * x;
     } else {
         return (dir == 0) ? user->windx : user->windy;
     }
@@ -109,8 +130,9 @@ but only east (E) and north (N) fluxes are computed
 //STARTFUNCTION
 PetscErrorCode FormRHSFunctionLocal(DMDALocalInfo *info, double t,
         double **au, double **aG, AdvectCtx *user) {
-    int     i, j, q;
-    double  hx, hy, halfx, halfy, x, y, a, u_up, u_dn, u_far, theta, flux;
+    int         i, j, q;
+    double      hx, hy, halfx, halfy, x, y, a,
+                u_up, u_dn, u_far, theta, flux;
 
     // clear G first
     for (j = info->ys; j < info->ys + info->ym; j++)
@@ -119,12 +141,12 @@ PetscErrorCode FormRHSFunctionLocal(DMDALocalInfo *info, double t,
     // fluxes on cell boundaries are traversed in this order:  ,-1-,
     // cell center at * has coordinates (x,y):                 | * 0
     // q = 0,1 is cell boundary index                          '---'
-    hx = 1.0 / info->mx;  hy = 1.0 / info->my;
+    hx = 2.0 / info->mx;  hy = 2.0 / info->my;
     halfx = hx / 2.0;     halfy = hy / 2.0;
     for (j = info->ys-1; j < info->ys + info->ym; j++) { // note -1
-        y = (j + 0.5) * hy;
+        y = -1.0 + (j+0.5) * hy;
         for (i = info->xs-1; i < info->xs + info->xm; i++) {
-            x = (i + 0.5) * hx;
+            x = -1.0 + (i+0.5) * hx;
             if ((i >= info->xs) && (j >= info->ys)) {
                 aG[j][i] += g_source(x,y,au[j][i],user);
             }
@@ -175,13 +197,13 @@ PetscErrorCode FormRHSJacobianLocal(DMDALocalInfo *info, double t,
     MatStencil  col[5],row;
 
     ierr = MatZeroEntries(P); CHKERRQ(ierr);
-    hx = 1.0 / info->mx;  hy = 1.0 / info->my;
+    hx = 2.0 / info->mx;  hy = 2.0 / info->my;
     halfx = hx / 2.0;     halfy = hy / 2.0;
     for (j = info->ys; j < info->ys+info->ym; j++) {
-        y = (j + 0.5) * hy;
+        y = -1.0 + (j+0.5) * hy;
         row.j = j;
         for (i = info->xs; i < info->xs+info->xm; i++) {
-            x = (i + 0.5) * hx;
+            x = -1.0 + (i+0.5) * hx;
             row.i = i;
             col[0].j = j;  col[0].i = i;
             v[0] = dg_source(x,y,au[j][i],user);
@@ -210,7 +232,6 @@ PetscErrorCode FormRHSJacobianLocal(DMDALocalInfo *info, double t,
             ierr = MatSetValuesStencil(P,1,&row,nc,col,v,ADD_VALUES); CHKERRQ(ierr);
         }
     }
-
     ierr = MatAssemblyBegin(P,MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
     ierr = MatAssemblyEnd(P,MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
     if (J != P) {
@@ -249,37 +270,23 @@ int main(int argc,char **argv) {
 
     PetscInitialize(&argc,&argv,(char*)0,help);
 
-    // the wind, and the cone initial condition, are from
-    // Hundsdorfer & Verwer, "Numerical Solution of Time-Dependent Advection-
-    // Diffusion-Reaction Equations", Springer 2003, page 303
-    user.windx = 1.0;
-    user.windy = 1.0;
-    user.conex = 0.2;
-    user.coney = 0.2;
-    user.coner = 0.1;
-    user.coneh = 1.0;
-    user.circlewind = PETSC_FALSE;
+    user.problem = STRAIGHT;
+    user.windx = 2.0;
+    user.windy = 2.0;
     ierr = PetscOptionsBegin(PETSC_COMM_WORLD,
            "adv_", "options for advect.c", ""); CHKERRQ(ierr);
-    ierr = PetscOptionsBool("-circlewind","if true, wind is rigid rotation",
-           "advect.c",user.circlewind,&user.circlewind,NULL);CHKERRQ(ierr);
-    ierr = PetscOptionsReal("-coneh","cone height",
-           "advect.c",user.coneh,&user.coneh,NULL);CHKERRQ(ierr);
-    ierr = PetscOptionsReal("-coner","cone radius",
-           "advect.c",user.coner,&user.coner,NULL);CHKERRQ(ierr);
-    ierr = PetscOptionsReal("-conex","x component of cone center",
-           "advect.c",user.conex,&user.conex,NULL);CHKERRQ(ierr);
-    ierr = PetscOptionsReal("-coney","y component of cone center",
-           "advect.c",user.coney,&user.coney,NULL);CHKERRQ(ierr);
     ierr = PetscOptionsString("-dumpto","filename root for initial/final state",
            "advect.c",fileroot,fileroot,PETSC_MAX_PATH_LEN,NULL);CHKERRQ(ierr);
     ierr = PetscOptionsEnum("-limiter","flux-limiter type",
            "advect.c",LimiterTypes,
            (PetscEnum)limiterchoice,(PetscEnum*)&limiterchoice,NULL); CHKERRQ(ierr);
     user.limiter = limiterptr[limiterchoice];
-    ierr = PetscOptionsReal("-windx","x component of wind (if not circular)",
+    ierr = PetscOptionsEnum("-problem","problem type",
+           "advect.c",ProblemTypes,
+           (PetscEnum)user.problem,(PetscEnum*)&user.problem,NULL); CHKERRQ(ierr);
+    ierr = PetscOptionsReal("-windx","x component of wind (if problem==straight)",
            "advect.c",user.windx,&user.windx,NULL);CHKERRQ(ierr);
-    ierr = PetscOptionsReal("-windy","y component of wind (if not circular)",
+    ierr = PetscOptionsReal("-windy","y component of wind (if problem==straight)",
            "advect.c",user.windy,&user.windy,NULL);CHKERRQ(ierr);
     ierr = PetscOptionsEnd(); CHKERRQ(ierr);
 
@@ -296,9 +303,9 @@ int main(int argc,char **argv) {
 
     // grid is cell-centered
     ierr = DMDAGetLocalInfo(da,&info); CHKERRQ(ierr);
-    hx = 1.0 / info.mx;  hy = 1.0 / info.my;
+    hx = 2.0 / info.mx;  hy = 2.0 / info.my;
     ierr = DMDASetUniformCoordinates(da,
-        0.0+hx/2.0,1.0-hx/2.0,0.0+hy/2.0,1.0-hy/2.0,0.0,1.0);CHKERRQ(ierr);
+        -1.0+hx/2.0,1.0-hx/2.0,-1.0+hy/2.0,1.0-hy/2.0,0.0,1.0);CHKERRQ(ierr);
 
     ierr = TSCreate(PETSC_COMM_WORLD,&ts); CHKERRQ(ierr);
     ierr = TSSetProblemType(ts,TS_NONLINEAR); CHKERRQ(ierr);
@@ -317,9 +324,10 @@ int main(int argc,char **argv) {
     ierr = TSGetTime(ts,&t0); CHKERRQ(ierr);
     ierr = TSGetTimeStep(ts,&dt); CHKERRQ(ierr);
     ierr = PetscPrintf(PETSC_COMM_WORLD,
-           "solving on %d x %d grid with dx=%g x dy=%g cells, t0=%g,\n"
-           "initial step dt=%g, and %s limiter ...\n",
-           info.mx,info.my,hx,hy,t0,dt,LimiterTypes[limiterchoice]); CHKERRQ(ierr);
+           "solving problem '%s' on %d x %d grid with dx=%g x dy=%g cells,\n"
+           "  t0=%g, initial dt=%g, and '%s' limiter ...\n",
+           ProblemTypes[user.problem],info.mx,info.my,hx,hy,
+           t0,dt,LimiterTypes[limiterchoice]); CHKERRQ(ierr);
 
     ierr = DMCreateGlobalVector(da,&u); CHKERRQ(ierr);
     ierr = FormInitial(&info,u,&user); CHKERRQ(ierr);
