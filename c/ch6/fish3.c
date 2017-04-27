0e7e88687e07e1f98041d74a24b244c50002f420
Ed Bueler <elbueler@alaska.edu>
2017-04-26 22:34:07 -0800

    passes all tests

--- c/ch6/fish3.c
+++ c/ch6/fish3.c
@@ -105,7 +105,7 @@ int main(int argc,char **argv) {
   Vec            u, uexact;
   FishCtx        user;
   DMDALocalInfo  info;
-  double         errnorm;
+  double         errinf,err2h;
 
   PetscInitialize(&argc,&argv,NULL,help);
 
@@ -140,9 +140,12 @@ int main(int argc,char **argv) {
   ierr = VecSet(u,0.0); CHKERRQ(ierr);
   ierr = SNESSolve(snes,NULL,u); CHKERRQ(ierr);
   ierr = VecAXPY(u,-1.0,uexact); CHKERRQ(ierr);    // u <- u + (-1.0) uexact
-  ierr = VecNorm(u,NORM_INFINITY,&errnorm); CHKERRQ(ierr);
-  ierr = PetscPrintf(COMM,"on %d x %d x %d grid:  error |u-uexact|_inf = %g\n",
-           info.mx,info.my,info.mz,errnorm); CHKERRQ(ierr);
+  ierr = VecNorm(u,NORM_INFINITY,&errinf); CHKERRQ(ierr);
+  ierr = VecNorm(u,NORM_2,&err2h); CHKERRQ(ierr);
+  err2h /= PetscSqrtReal((double)(info.mx-1)*(info.my-1)*(info.mz-1));
+  ierr = PetscPrintf(COMM,
+           "on %d x %d x %d grid:  error |u-uexact|_inf = %g, |...|_h = %.2e\n",
+           info.mx,info.my,info.mz,errinf,err2h); CHKERRQ(ierr);
 
   VecDestroy(&u);  VecDestroy(&uexact);  VecDestroy(&(user.f));
   SNESDestroy(&snes);  DMDestroy(&da);
