f8b2c3c79e57ba06957a0088c37d38337c8f95b6
Ed Bueler <elbueler@alaska.edu>
2017-04-30 14:52:58 -0800

    minor: timing

--- c/ch9/study/verif.sh
+++ c/ch9/study/verif.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+set +x
 
 NPROCS=4
 EXEC=../advect
@@ -7,11 +8,12 @@ LAPS=1
 
 # run with --with-debugging=0 build
 
-for LEV in 2 3 4 5; do
+for LEV in 1 3 5 7; do
+#for LEV in 1 3 5 7 8; do
     for SHAPE in box stump cone; do
         for LIMITER in centered none vanleer koren; do
             echo
-            mpiexec -n $NPROCS $EXEC -ts_final_time $LAPS -da_refine $LEV \
+            /usr/bin/time -f "%e" mpiexec -n $NPROCS $EXEC -ts_final_time $LAPS -da_refine $LEV \
                 -adv_limiter $LIMITER -adv_initial $SHAPE
         done
     done
