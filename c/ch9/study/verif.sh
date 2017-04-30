8d0a2481c8b2c99b4a23181b1926f51d4dba3ef3
Ed Bueler <elbueler@alaska.edu>
2017-04-30 15:50:54 -0800

    save results from run at this point

--- c/ch9/study/verif.sh
+++ c/ch9/study/verif.sh
@@ -2,18 +2,19 @@
 set -e
 set +x
 
-NPROCS=4
+NPROCS=8
 EXEC=../advect
-LAPS=1
+LAPS=1            # could run additional experiment with e.g. 5 laps
+TSRKTYPE=3bs      # seems to be fastest
 
 # run with --with-debugging=0 build
 
-for LEV in 1 3 5 7; do
-#for LEV in 1 3 5 7 8; do
-    for SHAPE in box stump cone; do
+for LEV in 1 3 5 7 8; do
+    for SHAPE in stump cone; do    # "box" is other possibility, but doesnt add much
         for LIMITER in centered none vanleer koren; do
             echo
-            /usr/bin/time -f "%e" mpiexec -n $NPROCS $EXEC -ts_final_time $LAPS -da_refine $LEV \
+            /usr/bin/time -f "%e" mpiexec -n $NPROCS $EXEC -ts_final_time $LAPS \
+                -ts_type rk -ts_rk_type $TSRKTYPE -da_refine $LEV \
                 -adv_limiter $LIMITER -adv_initial $SHAPE
         done
     done
