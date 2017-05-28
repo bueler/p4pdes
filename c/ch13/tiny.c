2138f445cef70001176998981731ee1454415657
Ed Bueler <elbueler@alaska.edu>
2017-05-27 17:38:08 -0800

    basic stuff a bit clearer because of DMSetDimension() and DMViewFromOptions() (why not default for -dm_view?)

--- c/ch13/tiny.c
+++ c/ch13/tiny.c
@@ -1,6 +1,12 @@
 static const char help[] =
-"Try out DMPlex by building the tiny triangular mesh in the DMPlex part\n"
-"of the PETSc User Manual\n\n";
+"Build a tiny triangular mesh using DMPlex.  Option prefix tny_.\n\n";
+// from two-triangle mesh in PETSc User's Manual
+
+/* compare these views:
+$ ./tiny -dm_view
+$ ./tiny -tny_print_points
+$ ./tiny -tny_print_points -tny_use_height
+*/
 
 #include <petsc.h>
 
@@ -10,12 +16,21 @@ int main(int argc,char **argv) {
   int         points[] = {0,1,6,7,8,9,10},
               sizes[]  = {3,3,2,2,2,2, 2},
               cone[]   = {6,7,8,  7,9,10,  2,3,  3,4,  4,2,  4,5,  5,3},
-              j, m, start, end;
-  const char* names[] = {"nodes",  // or "vertices"
-                         "edges",
-                         "cells"}; // or "elements"
+              j, m;
+  PetscBool   print = PETSC_FALSE,
+              height = PETSC_FALSE;
 
   PetscInitialize(&argc,&argv,NULL,help);
+
+  ierr = PetscOptionsBegin(PETSC_COMM_WORLD, "tny_", "options for tiny", "");CHKERRQ(ierr);
+  ierr = PetscOptionsBool("-print_points", "print indices for cells,edges,nodes",
+                          "tiny.c", print, &print, NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsBool("-use_height", "use Height instead of Depth when printing",
+                          "tiny.c", height, &height, NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsEnd();
+
+  // create the DMPlex "by hand" following user manual
+  // ... probably equivalent to DMPlexCreateFromDAG() or DMPlexCreateFromCellList()
   ierr = DMPlexCreate(PETSC_COMM_WORLD,&dmplex); CHKERRQ(ierr);
   ierr = DMPlexSetChart(dmplex, 0, 11); CHKERRQ(ierr);
   for (j = 0; j < (int)(sizeof(points) / sizeof(int)); j++) {
@@ -27,18 +42,38 @@ int main(int argc,char **argv) {
      ierr = DMPlexSetCone(dmplex, points[j], &(cone[m])); CHKERRQ(ierr);
      m += sizes[j];
   }
-  ierr = DMPlexSymmetrize(dmplex); CHKERRQ(ierr);  // Symmetrize must be before Stratify!
+  ierr = DMPlexSymmetrize(dmplex); CHKERRQ(ierr);  // both are required and Symmetrize must precede Stratify
   ierr = DMPlexStratify(dmplex); CHKERRQ(ierr);
+  ierr = DMSetDimension(dmplex, 2); CHKERRQ(ierr);
   ierr = DMSetFromOptions(dmplex); CHKERRQ(ierr);
-  //ierr = DMView(dmplex,PETSC_VIEWER_STDOUT_WORLD); CHKERRQ(ierr);
-  for (m = 0; m < 3; m++) {
-      ierr = DMPlexGetDepthStratum(dmplex,m,&start,&end); CHKERRQ(ierr);
-      ierr = PetscPrintf(PETSC_COMM_WORLD,"%s are %d,...,%d\n",names[m],start,end-1); CHKERRQ(ierr);
-      //ierr = DMPlexGetHeightStratum(dmplex,m,&start,&end); CHKERRQ(ierr);
-      //ierr = PetscPrintf(PETSC_COMM_WORLD,"%s are %d,...,%d\n",names[2-m],start,end-1); CHKERRQ(ierr);
+  ierr = PetscObjectSetName((PetscObject) dmplex, "Mesh");CHKERRQ(ierr);
+  ierr = DMViewFromOptions(dmplex, NULL, "-dm_view");CHKERRQ(ierr);
+
+  // show point indices by stratum
+  if (print) {
+      int         start, end;
+      const char* names[] = {"nodes",  // or "vertices"
+                             "edges",
+                             "cells"}; // or "elements"
+      ierr = DMPlexGetChart(dmplex,&start,&end); CHKERRQ(ierr);
+      ierr = PetscPrintf(PETSC_COMM_WORLD,"chart has point indices %d,...,%d\n",
+                         start,end-1); CHKERRQ(ierr);
+      if (height) {
+          for (m = 0; m < 3; m++) {
+              ierr = DMPlexGetHeightStratum(dmplex,m,&start,&end); CHKERRQ(ierr);
+              ierr = PetscPrintf(PETSC_COMM_WORLD,"    height %d: %s are %d,...,%d\n",
+                                 m,names[2-m],start,end-1); CHKERRQ(ierr);
+          }
+      } else {
+          for (m = 0; m < 3; m++) {
+              ierr = DMPlexGetDepthStratum(dmplex,m,&start,&end); CHKERRQ(ierr);
+              ierr = PetscPrintf(PETSC_COMM_WORLD,"    depth %d: %s are %d,...,%d\n",
+                                 m,names[m],start,end-1); CHKERRQ(ierr);
+          }
+      }
   }
+
   DMDestroy(&dmplex);
-  PetscFinalize();
-  return 0;
+  return PetscFinalize();
 }
 
