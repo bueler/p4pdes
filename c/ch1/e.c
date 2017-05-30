c8659e9cc4a003c53eb28c0353319c08b57d1762
Ed Bueler <elbueler@alaska.edu>
2017-05-30 14:48:21 -0800

    no functional change; tighten appearance w whitespace & comments; should last line of all main() be "return PetscFinalize()?"

--- c/ch1/e.c
+++ c/ch1/e.c
@@ -5,25 +5,21 @@ int main(int argc, char **argv) {
   int            rank, i;
   double         localval, globalsum;
 
-  PetscInitialize(&argc,&argv,NULL,"Compute e in parallel with PETSc.\n\n");
-
+  PetscInitialize(&argc,&argv,NULL,
+      "Compute e in parallel with PETSc.\n\n");
   ierr = MPI_Comm_rank(PETSC_COMM_WORLD,&rank); CHKERRQ(ierr);
-
-  // compute  1 / n!  where n = (rank of process) + 1
+  // compute  1/n!  where n = (rank of process) + 1
   localval = 1.0;
   for (i = 2; i < rank+1; i++)
       localval /= i;
-
   // sum the contributions over all processes
   ierr = MPI_Allreduce(&localval, &globalsum, 1, MPI_DOUBLE, MPI_SUM,
-                       PETSC_COMM_WORLD); CHKERRQ(ierr);
-
+      PETSC_COMM_WORLD); CHKERRQ(ierr);
   // output one estimate of e, but report on work from each process
   ierr = PetscPrintf(PETSC_COMM_WORLD,
       "e is about %17.15f\n",globalsum); CHKERRQ(ierr);
   ierr = PetscPrintf(PETSC_COMM_SELF,
-      "rank %d did %d flops\n",rank,(rank > 0) ? rank-1 : 0); CHKERRQ(ierr);
-
-  PetscFinalize();
-  return 0;
+      "rank %d did %d flops\n",rank,(rank > 0) ? rank-1 : 0);
+      CHKERRQ(ierr);
+  return PetscFinalize();
 }
