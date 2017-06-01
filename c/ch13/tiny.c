0d5a02182bf77b3c41ef5f2833aecad0f0fa6819
Ed Bueler <elbueler@alaska.edu>
2017-06-01 10:39:43 -0800

    fix up PlexViewXXX() routines to work in parallel, move them into tiny.c, and use "vertex" and "node" correctly

--- c/ch13/tiny.c
+++ c/ch13/tiny.c
@@ -1,5 +1,5 @@
 static const char help[] =
-"Build and explore a tiny three-triangle mesh using DMPlex.  Option prefix tny_.\n\n";
+"Build and view a tiny three-triangle mesh using DMPlex.  Option prefix tny_.\n\n";
 
 /* 
 either build DMPlex via call to DMPlexCreateFromCellList() with interpolate=TRUE:
@@ -7,17 +7,16 @@ either build DMPlex via call to DMPlexCreateFromCellList() with interpolate=TRUE
 or by directly setting cones "by hand":
 ./tiny -tny_by_hand
 
-compare these views:  FIXME:  abstract out my "view" routines
+compare these views:
 ./tiny -dm_view
 ./tiny -tny_ranges
 ./tiny -tny_ranges -tny_use_height
-./tiny -tny_cones
-./tiny -tny_supports
-./tiny -tny_vec_view
+./tiny -tny_cell_cones
+./tiny -tny_vertex_supports
+./tiny -tny_vec_view  FIXME broken
 */
 
 #include <petsc.h>
-#include "plexview.h"
 
 // describe mesh: triangle style with separate numbering for cells and vertices
 static const int dim = 2,
@@ -48,16 +47,18 @@ static const int npoint = 15,
                                 {5,7}};
 
 extern PetscErrorCode CreateMeshByHand(DM*);
+extern PetscErrorCode PlexViewRanges(DM, PetscBool);
+extern PetscErrorCode PlexViewFans(DM, int, int, int);
 
 int main(int argc,char **argv) {
     PetscErrorCode ierr;
     DM            dmplex;
     PetscSection  section;
     PetscBool     by_hand = PETSC_FALSE,
-                  cones = PETSC_FALSE,
+                  cell_cones = PETSC_FALSE,
                   use_height = PETSC_FALSE,
                   ranges = PETSC_FALSE,
-                  supports = PETSC_FALSE,
+                  vertex_supports = PETSC_FALSE,
                   vec_view = PETSC_FALSE;
 
     PetscInitialize(&argc,&argv,NULL,help);
@@ -65,14 +66,14 @@ int main(int argc,char **argv) {
     ierr = PetscOptionsBegin(PETSC_COMM_WORLD, "tny_", "options for tiny", "");CHKERRQ(ierr);
     ierr = PetscOptionsBool("-by_hand", "use by-hand construction",
                             "tiny.c", by_hand, &by_hand, NULL);CHKERRQ(ierr);
-    ierr = PetscOptionsBool("-cones", "print cones of each cell",
-                            "tiny.c", cones, &cones, NULL);CHKERRQ(ierr);
+    ierr = PetscOptionsBool("-cell_cones", "print cones of each cell",
+                            "tiny.c", cell_cones, &cell_cones, NULL);CHKERRQ(ierr);
     ierr = PetscOptionsBool("-use_height", "use Height instead of Depth when printing points",
                             "tiny.c", use_height, &use_height, NULL);CHKERRQ(ierr);
-    ierr = PetscOptionsBool("-ranges", "print point index ranges for cells,edges,nodes",
+    ierr = PetscOptionsBool("-ranges", "print point index ranges for vertices,edges,cells",
                             "tiny.c", ranges, &ranges, NULL);CHKERRQ(ierr);
-    ierr = PetscOptionsBool("-supports", "print supports of each vertex",
-                            "tiny.c", supports, &supports, NULL);CHKERRQ(ierr);
+    ierr = PetscOptionsBool("-vertex_supports", "print supports of each vertex",
+                            "tiny.c", vertex_supports, &vertex_supports, NULL);CHKERRQ(ierr);
     ierr = PetscOptionsBool("-vec_view", "print entries of a global vec for P2 elements",
                             "tiny.c", vec_view, &vec_view, NULL);CHKERRQ(ierr);
     ierr = PetscOptionsEnd();
@@ -112,27 +113,25 @@ int main(int argc,char **argv) {
     // viewing of dmplex
     ierr = DMViewFromOptions(dmplex, NULL, "-dm_view"); CHKERRQ(ierr);  // why not enabled by default?
     if (ranges) {
-        ierr = PlexViewRanges(dmplex,PETSC_VIEWER_STDOUT_WORLD,use_height); CHKERRQ(ierr);
+        ierr = PlexViewRanges(dmplex,use_height); CHKERRQ(ierr);
     }
-    if (cones) {
-        // FIXME: start=0, end=ncell is only correct if serial
-        ierr = PlexViewFans(dmplex,PETSC_VIEWER_STDOUT_WORLD,PETSC_TRUE,"cell","edge",0,ncell); CHKERRQ(ierr);
+    if (cell_cones) {
+        ierr = PlexViewFans(dmplex,2,2,1); CHKERRQ(ierr);
     }
-    if (supports) {
-        // FIXME: ditto ... only correct if serial
-        ierr = PlexViewFans(dmplex,PETSC_VIEWER_STDOUT_WORLD,PETSC_FALSE,"vertex","edge",ncell,ncell+nvert); CHKERRQ(ierr);
+    if (vertex_supports) {
+        ierr = PlexViewFans(dmplex,2,0,1); CHKERRQ(ierr);
     }
 
     // create dofs like P2 elements using PetscSection
-    // with 1 dof on each node (depth==0) and 1 dof on each edge (depth==1)
+    // with 1 dof on each vertex (depth==0) and 1 dof on each edge (depth==1)
     // [DMPlexCreateSection() seems to do something like the following]
-    int  j, pstart, pend, nstart, nend, estart, eend;
+    int  j, pstart, pend, vstart, vend, estart, eend;
     ierr = DMPlexGetChart(dmplex,&pstart,&pend); CHKERRQ(ierr);
-    ierr = DMPlexGetDepthStratum(dmplex, 0, &nstart, &nend); CHKERRQ(ierr);
+    ierr = DMPlexGetDepthStratum(dmplex, 0, &vstart, &vend); CHKERRQ(ierr);
     ierr = DMPlexGetDepthStratum(dmplex, 1, &estart, &eend); CHKERRQ(ierr);
     ierr = PetscSectionCreate(PETSC_COMM_WORLD,&section); CHKERRQ(ierr);
     ierr = PetscSectionSetChart(section, pstart, pend); CHKERRQ(ierr);
-    for (j = nstart; j < nend; ++j) {
+    for (j = vstart; j < vend; ++j) {
         ierr = PetscSectionSetDof(section, j, 1); CHKERRQ(ierr);
     }
     for (j = estart; j < eend; ++j) {
@@ -200,7 +199,7 @@ PetscErrorCode CreateMeshByHand(DM *dmplex) {
     ierr = DMPlexCreate(PETSC_COMM_WORLD,dmplex); CHKERRQ(ierr);
     // set the total number of points (ncell+nvert+nedges)
     ierr = DMPlexSetChart(*dmplex, 0, npoint); CHKERRQ(ierr);
-    // the points are cells, nodes, edges in that order, and we only set cones for cells and edges
+    // the points are cells, vertices, edges in that order, and we only set cones for cells and edges
     for (j = 0; j < ncell; j++) {
         ierr = DMPlexSetConeSize(*dmplex, j, dim+1); CHKERRQ(ierr);
     }
@@ -246,6 +245,87 @@ PetscErrorCode CreateMeshByHand(DM *dmplex) {
     // finally we tell the DM that it has coordinates
     ierr = DMSetCoordinatesLocal(*dmplex, coordinates); CHKERRQ(ierr);
     VecDestroy(&coordinates);
+    return 0;
+}
+
+static const char* stratanames[4][10] =
+                       {{"vertex","",    "",    ""},       // dim=0 names
+                        {"vertex","cell","",    ""},       // dim=1 names
+                        {"vertex","edge","cell",""},       // dim=2 names
+                        {"vertex","edge","face","cell"}};  // dim=3 names
+
+PetscErrorCode PlexViewRanges(DM plex, PetscBool use_height) {
+    PetscErrorCode ierr;
+    int         dim, m, start, end;
+    MPI_Comm    comm;
+    PetscMPIInt rank,size;
+    ierr = PetscObjectGetComm((PetscObject)plex,&comm); CHKERRQ(ierr);
+    ierr = MPI_Comm_size(comm,&size);CHKERRQ(ierr);
+    ierr = MPI_Comm_rank(comm,&rank); CHKERRQ(ierr);
+    ierr = DMGetDimension(plex,&dim); CHKERRQ(ierr);
+    if (size > 1) {
+        ierr = PetscSynchronizedPrintf(comm,"[rank %d] ",rank); CHKERRQ(ierr);
+    }
+    ierr = DMPlexGetChart(plex,&start,&end); CHKERRQ(ierr);
+    ierr = PetscSynchronizedPrintf(comm,
+        "chart for %d-dimensional DMPlex has points %d,...,%d\n",
+        dim,start,end-1); CHKERRQ(ierr);
+    for (m = 0; m < dim + 1; m++) {
+        if (use_height) {
+            ierr = DMPlexGetHeightStratum(plex,m,&start,&end); CHKERRQ(ierr);
+            ierr = PetscSynchronizedPrintf(comm,
+                "    height %d of size %d: %d,...,%d (%s)\n",
+                m,end-start,start,end-1,dim < 4 ? stratanames[dim][2-m] : ""); CHKERRQ(ierr);
+        } else {
+            ierr = DMPlexGetDepthStratum(plex,m,&start,&end); CHKERRQ(ierr);
+            ierr = PetscSynchronizedPrintf(comm,
+                "    depth=dim %d of size %d: %d,...,%d (%s)\n",
+                m,end-start,start,end-1,dim < 4 ? stratanames[dim][m] : ""); CHKERRQ(ierr);
+        }
+    }
+    ierr = PetscSynchronizedFlush(comm,PETSC_STDOUT); CHKERRQ(ierr);
+    return 0;
+}
+
+/* viewing cell cones in 2D:
+     PlexViewFans(dmplex,2,1)
+viewing vertex supports:
+     PlexViewFans(dmplex,0,1)   */
+PetscErrorCode PlexViewFans(DM plex, int dim, int basestrata, int targetstrata) {
+    PetscErrorCode ierr;
+    const int   *targets;
+    int         j, m, start, end, cssize;
+    MPI_Comm    comm;
+    PetscMPIInt rank,size;
+    ierr = PetscObjectGetComm((PetscObject)plex,&comm); CHKERRQ(ierr);
+    ierr = MPI_Comm_size(comm,&size);CHKERRQ(ierr);
+    ierr = MPI_Comm_rank(comm,&rank); CHKERRQ(ierr);
+    if (size > 1) {
+        ierr = PetscSynchronizedPrintf(comm,"[rank %d] ",rank); CHKERRQ(ierr);
+    }
+    ierr = PetscSynchronizedPrintf(comm,
+        "%s (= %s indices) of each %s:\n",
+        (basestrata > targetstrata) ? "cones" : "supports",
+        stratanames[dim][targetstrata],stratanames[dim][basestrata]); CHKERRQ(ierr);
+    ierr = DMPlexGetDepthStratum(plex, basestrata, &start, &end); CHKERRQ(ierr);
+    for (m = start; m < end; m++) {
+        if (basestrata > targetstrata) {
+            ierr = DMPlexGetConeSize(plex,m,&cssize); CHKERRQ(ierr);
+            ierr = DMPlexGetCone(plex,m,&targets); CHKERRQ(ierr);
+        } else {
+            ierr = DMPlexGetSupportSize(plex,m,&cssize); CHKERRQ(ierr);
+            ierr = DMPlexGetSupport(plex,m,&targets); CHKERRQ(ierr);
+        }
+        ierr = PetscSynchronizedPrintf(comm,
+            "    %s %d: ",stratanames[dim][basestrata],m); CHKERRQ(ierr);
+        for (j = 0; j < cssize-1; j++) {
+            ierr = PetscSynchronizedPrintf(comm,
+                "%d,",targets[j]); CHKERRQ(ierr);
+        }
+        ierr = PetscSynchronizedPrintf(comm,
+            "%d\n",targets[cssize-1]); CHKERRQ(ierr);
+    }
+    ierr = PetscSynchronizedFlush(comm,PETSC_STDOUT); CHKERRQ(ierr);
     return 0;
 }
 
