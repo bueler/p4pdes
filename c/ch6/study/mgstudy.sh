d6cac6847cc5773ef0d52ecb622aad668e4f3c88
Ed Bueler <elbueler@alaska.edu>
2017-06-17 13:56:49 -0800

    remove -mg_levels_pc_type jacobi (i.e. jacobi smoother) runs as always too slow; clean up

--- c/ch6/study/mgstudy.sh
+++ c/ch6/study/mgstudy.sh
@@ -9,23 +9,26 @@ REFINE=$2
 #     -pc_mg_cycle_type         <v|w>
 #     -mg_levels_ksp_type       <chebyshev|richardson>   WHICH SMOOTHER?
 #     -mg_levels_ksp_max_it     <1|2|3>                  HOW MANY SMOOTHINGS (UP AND DOWN)
+
+# thus does (REFINE/2) * 2 * 2 * 3 = 6*REFINE runs
+
+# note: if we compare these, jacobi is systematically about 10 times slower;
+# sor is default:
 #     -mg_levels_pc_type        <jacobi|sor>
 
 # use PETSC_ARCH with --with-debugging=0
 # run as in these examples:
-#    $ ./mgstudy.sh ../fish2 9 &> fish2refine9.txt
-#    $ ./mgstudy.sh ../fish3 5 &> fish3refine5.txt
-#    $ ./mgstudy.sh "mpiexec -n 2 ../fish2" 5 &> fish2refine5mpi2.txt
+#    $ ./mgstudy.sh "../fish -fsh_dim 2" 9 &> fish2refine9.txt
+#    $ ./mgstudy.sh "../fish -fsh_dim 3" 5 &> fish3refine5.txt
+#    $ ./mgstudy.sh "mpiexec -n 2 ../fish -fsh_dim 2" 5 &> fish2refine5mpi2.txt
 
 for (( MGLEV=$(( REFINE + 1 )); MGLEV>2; MGLEV-=2 )); do
     for MGVW in v w; do
         for MGKSPTYPE in chebyshev richardson; do
-            for MGPCTYPE in jacobi sor; do
-                for MGSMOOTHIT in 1 2 3; do
-                    CMD="$EXEC -pc_type mg -ksp_rtol 1.0e-12 -ksp_converged_reason -da_refine $REFINE -pc_mg_levels $MGLEV -pc_mg_cycle_type $MGVW -mg_levels_ksp_type $MGKSPTYPE -mg_levels_pc_type $MGPCTYPE -mg_levels_ksp_max_it $MGSMOOTHIT"
-                    echo "COMMAND:  $CMD"
-                    /usr/bin/time --portability -f "real %e" $CMD
-                done
+            for MGSMOOTHIT in 1 2 3; do
+                CMD="$EXEC -pc_type mg -ksp_rtol 1.0e-12 -ksp_converged_reason -da_refine $REFINE -pc_mg_levels $MGLEV -pc_mg_cycle_type $MGVW -mg_levels_ksp_type $MGKSPTYPE -mg_levels_ksp_max_it $MGSMOOTHIT"
+                echo "COMMAND:  $CMD"
+                /usr/bin/time --portability -f "real %e" $CMD
             done
         done
     done
