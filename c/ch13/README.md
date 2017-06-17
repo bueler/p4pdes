b8257705b6938044d47c96b21ef58198e1bc9161
Ed Bueler <elbueler@alaska.edu>
2017-06-17 13:51:47 -0800

    set up a usable chinook (CLUSTER) test script for Chapter 13

--- c/ch13/README.md
+++ c/ch13/README.md
@@ -1,8 +1,8 @@
 Running jobs on CLUSTER
 =======================
 
-There are no `.c` codes or `makefile`s in this subdirectory, only a sample
-batch script for running codes from previous Chapters.
+The only thin in this subdirectory is a sample batch script for running codes
+from previous Chapters.
 
 CLUSTER is a modest-sized Linux cluster at the University of Alaska Fairbanks,
 namely
@@ -13,6 +13,3 @@ Access is generally limited to UAF students and faculty.  Thus this batch
 script is _only_ an example which might be useful as a suggestion for how
 PETSc codes are run on many-node machines.
 
-### FIXME
-
-possible specifics
--- c/ch13/cluster.sh
+++ c/ch13/cluster.sh
@@ -1,43 +1,58 @@
 #!/bin/bash
 
-# This is a sample batch file for running jobs on a linux cluster.
+# This is a sample batch file for running jobs on a linux cluster.  A goal is that the
+# total run time is less than an hour for > 8 processes.  If you have a bigger machine
+# (e.g. >100 processes) consider refining runs below.
 
-# In parent directory ../c/ run "make test" first so executables ../ch*/CODE
-# get built.  This script runs these executables:
-#   * ../ch5/pattern
-#   * ../ch6/fish
-#   * ../ch9/advect
-#   * ../ch12/obstacle
-# For each of these a -log_view is generated, and analyzed for performance data.
+# In parent directory ../c/, set PETSC_ARCH to a --with-debugging=0 PETSc configuration.
+# Then run "make distclean" to clean out old executables.  Then run "make test" to make sure
+# executables ../ch*/CODE get built.
 
-# You will need to edit this file to match your batch system.
+# For each run a -log_view is generated, and analyzed for performance data.  Do
+#   grep "(sec):" cluster.xxxx
+# to see total times of runs.
 
+# You will need to edit this file to match your batch system, and for each run!!
+# Many details below are particular to the Slurm batch system (see https://slurm.schedmd.com/).
 
 #SBATCH --partition=debug
-#SBATCH --ntasks=4
+#SBATCH --ntasks=8
 #SBATCH --tasks-per-node=4
-#SBATCH --mail-user=elbueler@alaska.edu
+#SBATCH --mail-user=elbueler@alaska.edu   # FIXME don't release this way; I'll get spammed by slurm!
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
-#SBATCH --output=study.%j
+#SBATCH --output=cluster.o.%j
 
+# Submit from c/ch13/ for this to work.
+cd $SLURM_SUBMIT_DIR
+
+# This cluster needs these for some reason.
 ulimit -s unlimited
 ulimit -l unlimited
 
-cd $SLURM_SUBMIT_DIR
-
-# Generate a list of allocated nodes; will serve as a machinefile for mpirun
+# Generate a list of allocated nodes; will serve as a machinefile for mpirun.
 srun -l /bin/hostname | sort -n | awk '{print $2}' > ./nodes.$SLURM_JOB_ID
 
-# Launch the MPI application
-#mpirun -np $SLURM_NTASKS -machinefile ./nodes.$SLURM_JOB_ID ./<APPLICATION>
+# Launch the MPI application.  Tested with mpich2.
+GO="mpiexec -n $SLURM_NTASKS -machinefile ./nodes.$SLURM_JOB_ID"
 
-#../ch5/pattern
-../ch6/fish -fsh_dim 3 -da_refine 4
-#../ch9/advect
-#../ch12/obstacle
+#---------------------------------------------------------------------------------
+# -da_refine 6 is 256x256 and does ~230 time steps
+$GO ../ch5/pattern -da_refine 6 -ptn_phi 0.05 -ptn_kappa 0.063 -ts_final_time 5000 -ptn_noisy_init 0.15 -ts_monitor -log_view
 
-# Clean up the machinefile
+# -da_refine 8 is 513x513x513; ~6Gb memory
+$GO ../ch6/fish -fsh_dim 3 -da_refine 8 -pc_type mg -snes_type ksponly -ksp_converged_reason -log_view
+
+# -da_refine 8 is 1280x1280 and does ~2400 time steps
+$GO ../ch9/advect -da_refine 8 -ts_final_time 1.0 -ts_rk_type 3bs -ts_monitor -log_view
+
+# -da_refine 10 is 2049x2049; -pc_mg_levels to avoid "too fine" error?
+$GO ../ch12/obstacle -da_refine 10 -snes_monitor -ksp_converged_reason -pc_type mg -pc_mg_levels 8 -log_view
+
+#FIXME add an unstructured from Chapter 10?
+#---------------------------------------------------------------------------------
+
+# Clean up the machinefile.
 rm ./nodes.$SLURM_JOB_ID
 
