6d9adcfa689ac402c3d47ec3ed417df9c76bf925
Ed Bueler <elbueler@alaska.edu>
2017-07-10 18:00:37 -0800

    add script for counting asm iters with 2,3 dimensions, lu|ilu solves, various overlaps, various resolutions

---
+++ c/ch6/study/asmaddmult.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e
+
+# attempt to reproduce results on first table on page 27 of Smith et al 1996 "Domain Decomposition ..."
+
+# note that numbers will be different because instead of "10^-2 reduction
+# in error", probably in inf-norm (page 15), as in the table, here our criterion
+# is 10^-2 reduction in preconditioned 2-norm (see man page for KSPSetNormType())
+
+# do
+#   ./asmiters.sh 4
+# for N=33 (33x33 grid) and
+#   ./asmiters.sh 5
+# for N=65 (65x65 grid)
+
+LEV=$1
+
+COMMON="-fsh_dim 2 -ksp_type gmres -ksp_gmres_restart 10 -fsh_problem manuexp -snes_type ksponly -ksp_converged_reason -ksp_rtol 1.0e-2"
+
+ASM="-pc_type asm -sub_pc_type lu -pc_asm_type BASIC"
+
+#  -pc_asm_dm_subdomains: <FALSE> Use DMCreateDomainDecomposition() to define subdomains (PCASMSetDMSubdomains)
+#  -pc_asm_blocks <-1>: Number of subdomains (PCASMSetTotalSubdomains)
+#  -pc_asm_overlap <1>: Number of grid points overlap (PCASMSetOverlap)
+#  -pc_asm_type <RESTRICT> Type of restriction/extension (choose one of) NONE RESTRICT INTERPOLATE BASIC (PCASMSetType)
+#  -pc_asm_local_type <ADDITIVE> Type of local solver composition (choose one of) ADDITIVE MULTIPLICATIVE SYMMETRIC_MULTIPLICATIVE SPECIAL SCHUR (PCASMSetLocalType)
+
+function runcase() {
+  CMD="../fish $COMMON $1"
+  #echo $CMD   # uncomment to show command, and add -snes_view to command to show solver
+  rm -f tmp.txt
+  $CMD &> tmp.txt
+  grep iterations tmp.txt
+  rm -f tmp.txt
+}
+
+for BL in 2 4 8; do
+    for TYPE in multiplicative additive; do
+        for OVER in 1 2 4; do
+            echo "asm: $BL blocks, $TYPE, overlap $OVER"
+            runcase "$ASM -da_refine $LEV -pc_asm_blocks $BL -pc_asm_local_type $TYPE -pc_asm_overlap $OVER"
+        done
+    done
+done
+echo "ilu:"
+runcase "-da_refine $LEV -pc_type ilu"
+echo "ssor:"
+runcase "-da_refine $LEV -pc_type sor -pc_sor_symmetric"
+
--- c/ch6/study/asmiters.sh
+++ c/ch6/study/asmiters.sh
@@ -1,49 +1,45 @@
 #!/bin/bash
 set -e
 
-# attempt to reproduce results on first table on page 27 of Smith et al 1996 "Domain Decomposition ..."
+# test ASM iterations with 2,3 dimensions and lu,ilu solution on blocks, with
+#   NN   = 2,4,8    subdomains/processes
+#   LEV  = 4,6,8    refinement (m=33,129,513) in 2D;  only m=33,129 in 3D
+#   OVER = 0,1,2,4  overlap
 
-# note that numbers will be different because instead of "10^-2 reduction
-# in error", probably in inf-norm (page 15), as in the table, here our criterion
-# is 10^-2 reduction in preconditioned 2-norm (see man page for KSPSetNormType())
+COMMON="-snes_type ksponly -ksp_converged_reason -pc_type asm"
 
-# do
-#   ./asmiters.sh 4
-# for N=33 (33x33 grid) and
-#   ./asmiters.sh 5
-# for N=65 (65x65 grid)
+# notes:
+#   * defaults to -ksp_type gmres -ksp_gmres_restart 30 -ksp_rtol 1.0e-5
+#   * run with --with-debugging=0 configuration
 
-LEV=$1
+#FIXME exercise: set GMRES300 and see reduced iters
 
-COMMON="-fsh_dim 2 -ksp_type gmres -ksp_gmres_restart 10 -fsh_problem manuexp -snes_type ksponly -ksp_converged_reason -ksp_rtol 1.0e-2"
-
-ASM="-pc_type asm -sub_pc_type lu -pc_asm_type BASIC"
-
-#  -pc_asm_dm_subdomains: <FALSE> Use DMCreateDomainDecomposition() to define subdomains (PCASMSetDMSubdomains)
-#  -pc_asm_blocks <-1>: Number of subdomains (PCASMSetTotalSubdomains)
-#  -pc_asm_overlap <1>: Number of grid points overlap (PCASMSetOverlap)
-#  -pc_asm_type <RESTRICT> Type of restriction/extension (choose one of) NONE RESTRICT INTERPOLATE BASIC (PCASMSetType)
-#  -pc_asm_local_type <ADDITIVE> Type of local solver composition (choose one of) ADDITIVE MULTIPLICATIVE SYMMETRIC_MULTIPLICATIVE SPECIAL SCHUR (PCASMSetLocalType)
+#FIXME exercise: set CG+cholesky|icc and -ksp_max_it 1000 and see some DIVERGED_ITS
 
 function runcase() {
-  CMD="../fish $COMMON $1"
-  #echo $CMD   # uncomment to show command, and add -snes_view to command to show solver
+  CMD="mpiexec -n $1 ../fish -fsh_dim $2 $COMMON -da_refine $3 -pc_asm_overlap $4 -sub_pc_type $5"
+  echo $CMD   # uncomment to show command, and add -snes_view to command to show solver
   rm -f tmp.txt
   $CMD &> tmp.txt
   grep iterations tmp.txt
   rm -f tmp.txt
 }
 
-for BL in 2 4 8; do
-    for TYPE in multiplicative additive; do
-        for OVER in 1 2 4; do
-            echo "asm: $BL blocks, $TYPE, overlap $OVER"
-            runcase "$ASM -da_refine $LEV -pc_asm_blocks $BL -pc_asm_local_type $TYPE -pc_asm_overlap $OVER"
+for NN in 2 4 8; do
+    for OVER in 0 1 2 4; do
+        for LEV in 4 6 8; do
+            runcase $NN 2 $LEV $OVER lu
+            runcase $NN 2 $LEV $OVER ilu
         done
     done
 done
-echo "ilu:"
-runcase "-da_refine $LEV -pc_type ilu"
-echo "ssor:"
-runcase "-da_refine $LEV -pc_type sor -pc_sor_symmetric"
+
+for NN in 2 4 8; do
+    for OVER in 0 1 2 4; do
+        for LEV in 4 6; do
+            runcase $NN 3 $LEV $OVER ilu
+        done
+    done
+done
+
 
