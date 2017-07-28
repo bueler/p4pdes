bceb698af99f28b2c812a204ab0839841089bdc8
Ed Bueler <elbueler@alaska.edu>
2017-07-28 11:36:06 -0800

    correct number of subdomains in 3D; rerun on workstation

--- c/ch6/study/asmoverlaps.sh
+++ c/ch6/study/asmoverlaps.sh
@@ -4,7 +4,8 @@ set -e
 # test ASM iterations with 2,3 dimensions and
 #   LEV  = 4,5,6,7      refinement (m=33,65,129,257) in 2D;  only m=33,65 in 3D
 #   OVER = 0,1,4,16     overlap
-# for all runs uses 4 blocks and lu on subdomains
+# all runs lu on subdomains
+# 2D runs uses 4 blocks and 3D use 8
 
 COMMON="-snes_type ksponly -ksp_converged_reason -pc_type asm -sub_pc_type lu"
 
@@ -17,7 +18,7 @@ COMMON="-snes_type ksponly -ksp_converged_reason -pc_type asm -sub_pc_type lu"
 #   * run with --with-debugging=0 configuration
 
 function runcase() {
-  CMD="mpiexec -n 4 ../fish $COMMON -fsh_dim $1 -da_refine $2 -pc_asm_overlap $3"
+  CMD="mpiexec -n $1 ../fish $COMMON -fsh_dim $2 -da_refine $3 -pc_asm_overlap $4"
   echo $CMD   # uncomment to show command, and add -snes_view to command to show solver
   rm -f tmp.txt
   /usr/bin/time -f "real %e" $CMD &> tmp.txt
@@ -28,10 +29,10 @@ function runcase() {
 
 for OVER in 0 1 2 4; do
     for LEV in 4 5 6 7; do
-        runcase 2 $LEV $OVER
+        runcase 4 2 $LEV $OVER
     done
     for LEV in 4 5; do
-        runcase 3 $LEV $OVER
+        runcase 8 3 $LEV $OVER
     done
 done
 
