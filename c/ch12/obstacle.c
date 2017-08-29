847686d486d6db189ce686e7e6f1486238a0e6a5
Ed Bueler <elbueler@alaska.edu>
2017-08-29 10:51:08 -0800

    make solution more accurate by correctly-computing exact free-boundary solution; and better documentation

--- c/ch12/obstacle.c
+++ c/ch12/obstacle.c
@@ -7,35 +7,31 @@ the problem is nonlinear.\n";
 parallel runs, spatial refinement, robust PC:
 for M in 0 1 2 3 4 5 6; do mpiexec -n 4 ./obstacle -da_refine $M -snes_converged_reason -pc_type asm -sub_pc_type lu; done
 
-grid sequencing really worthwhile:
-    timer mpiexec -n 4 ./obstacle -snes_monitor -snes_converged_reason -snes_grid_sequence 8
-    real 10.82
-versus 180 seconds
-
-check it out:
+grid sequencing really worthwhile; check out these serial runs:
 $ timer ./obstacle -da_refine 8 -pc_type ilu
-errors on 513 x 513 grid: av |u-uexact| = 2.141e-06, |u-uexact|_inf = 1.950e-05
-real 178.07
+errors on 513 x 513 grid: av |u-uexact| = 2.055e-06, |u-uexact|_inf = 1.918e-05
+real 143.77
 $ timer ./obstacle -da_refine 8 -pc_type mg
-errors on 513 x 513 grid: av |u-uexact| = 2.137e-06, |u-uexact|_inf = 1.950e-05
-real 38.58
+errors on 513 x 513 grid: av |u-uexact| = 2.051e-06, |u-uexact|_inf = 1.918e-05
+real 27.44
 $ timer ./obstacle -snes_grid_sequence 8 -pc_type ilu
-errors on 513 x 513 grid: av |u-uexact| = 2.137e-06, |u-uexact|_inf = 1.950e-05
-real 9.81
+errors on 513 x 513 grid: av |u-uexact| = 2.051e-06, |u-uexact|_inf = 1.918e-05
+real 7.90
 $ timer ./obstacle -snes_grid_sequence 8 -pc_type mg
-errors on 513 x 513 grid: av |u-uexact| = 2.137e-06, |u-uexact|_inf = 1.950e-05
-real 2.65
+errors on 513 x 513 grid: av |u-uexact| = 2.051e-06, |u-uexact|_inf = 1.918e-05
+real 1.91
 
-and then one can increase the dimension by 64 times!!!!:
+and then one can increase the dimension by 64 times!!!! (still serial; level 12
+would exceed memory on WORKSTATION):
 $ timer ./obstacle -snes_grid_sequence 9 -pc_type mg
-errors on 1025 x 1025 grid: av |u-uexact| = 7.130e-07, |u-uexact|_inf = 6.908e-06
-real 11.38
+errors on 1025 x 1025 grid: av |u-uexact| = 6.266e-07, |u-uexact|_inf = 6.592e-06
+real 8.18
 $ timer ./obstacle -snes_grid_sequence 10 -pc_type mg
-errors on 2049 x 2049 grid: av |u-uexact| = 2.243e-07, |u-uexact|_inf = 1.828e-06
-real 38.63
+errors on 2049 x 2049 grid: av |u-uexact| = 1.379e-07, |u-uexact|_inf = 1.511e-06
+real 29.15
 $ timer ./obstacle -snes_grid_sequence 11 -pc_type mg
-errors on 4097 x 4097 grid: av |u-uexact| = 1.231e-07, |u-uexact|_inf = 7.511e-07
-real 147.49
+errors on 4097 x 4097 grid: av |u-uexact| = 3.657e-08, |u-uexact|_inf = 4.332e-07
+real 111.59
 
 one can adjust the smallest grid, which is needed in parallel to correctly cut-up
 the coarsest grid, but starting small seems good:
@@ -46,16 +42,21 @@ $ timer ./obstacle -snes_converged_reason -pc_type mg -snes_grid_sequence 9
     Nonlinear solve converged due to CONVERGED_FNORM_RELATIVE iterations 3
   Nonlinear solve converged due to CONVERGED_FNORM_RELATIVE iterations 2
 Nonlinear solve converged due to CONVERGED_FNORM_RELATIVE iterations 3
-errors on 1025 x 1025 grid: av |u-uexact| = 7.130e-07, |u-uexact|_inf = 6.908e-06
-real 11.39
+errors on 1025 x 1025 grid: av |u-uexact| = 6.266e-07, |u-uexact|_inf = 6.592e-06
+real 8.33
 $ timer ./obstacle -da_grid_x 33 -da_grid_y 33 -snes_converged_reason -pc_type mg -snes_grid_sequence 5
           Nonlinear solve converged due to CONVERGED_FNORM_RELATIVE iterations 5
-...
+        Nonlinear solve converged due to CONVERGED_FNORM_RELATIVE iterations 2
+      Nonlinear solve converged due to CONVERGED_FNORM_RELATIVE iterations 2
     Nonlinear solve converged due to CONVERGED_FNORM_RELATIVE iterations 3
   Nonlinear solve converged due to CONVERGED_FNORM_RELATIVE iterations 2
 Nonlinear solve converged due to CONVERGED_FNORM_RELATIVE iterations 3
-errors on 1025 x 1025 grid: av |u-uexact| = 7.130e-07, |u-uexact|_inf = 6.908e-06
-real 12.16
+errors on 1025 x 1025 grid: av |u-uexact| = 6.266e-07, |u-uexact|_inf = 6.592e-06
+real 8.31
+
+parallel versions of above two runs:
+STALLS: $ timer mpiexec -n 4 ./obstacle -snes_converged_reason -pc_type mg -snes_grid_sequence 9
+SUCCEEDS IN 6.18 secs: $ timer mpiexec -n 4 ./obstacle -da_grid_x 33 -da_grid_y 33 -snes_converged_reason -pc_type mg -snes_grid_sequence 5
 */
 
 #include <petsc.h>
@@ -63,14 +64,49 @@ real 12.16
 
 // z = psi(x,y) is the obstacle
 double psi(double x, double y) {
-    double  r = PetscSqrtReal(x * x + y * y);
-    return (r <= 1.0) ? PetscSqrtReal(1.0 - r * r) : -1.0;
+    double  rr = x * x + y * y;
+    return (rr <= 1.0) ? PetscSqrtReal(1.0 - rr) : -1.0;
 }
 
-// exact solution
+/* To get exact solution see
+    https://github.com/bueler/fem-code-challenge/blob/master/obstacleDOC.pdf
+
+In summary, solve a 1D radial free-boundary problem on interval 0 < r < 2 with
+obstacle
+
+    psi(r) =  / sqrt(1 - r^2),  r < 1
+              \ -1,             otherwise
+
+and Laplace equation where u(r) > psi(r),
+
+    u''(r) + r^-1 u'(r) = 0
+
+with (free) boundary conditions
+
+    u(a) = psi(a),  u'(a) = psi'(a),  u(2) = 0
+
+The solution is  u(r) = - A log(r) + B   on the interval  a < r < 2.  The
+boundary conditions can then be reduced to a root-finding problem for a:
+
+    a^2 (log(2) - log(a)) = 1 - a^2
+
+Solved via Matlab/Octave:
+>> f = @(a) a.^2 .* (log(2)-log(a)) - 1 + a.^2;
+>> a = 0.2:0.001:1.0; plot(a,f(a)), grid on
+>> a = fzero(f,0.7)
+a =    0.697965148223374
+>> f(a)
+ans = 1.49880108324396e-15
+>> A = a^2*(1-a^2)^(-0.5),  B = A*log(2)
+A =    0.680259411891719
+B =    0.471519893402112
+
+These constants appear in the C function below.                    */
 double u_exact(double x, double y) {
-    // FIXME: precision limited because these are given only to 10^-5
-    double  r, afree = 0.69797, A = 0.68026, B = 0.47152;
+    const double afree = 0.697965148223374,
+                 A     = 0.680259411891719,
+                 B     = 0.471519893402112;
+    double  r;
     r = PetscSqrtReal(x * x + y * y);
     return (r <= afree) ? psi(x,y)  // on the obstacle
                         : - A * PetscLogReal(r) + B; // solves laplace eqn
