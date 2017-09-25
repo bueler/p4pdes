6fa7aabb3943bc6c3aef7ab7b9af5cf82f038de9
Ed Bueler <elbueler@alaska.edu>
2017-09-25 14:23:07 -0800

    draft; see FIXME

---
+++ c/ch6/study/cyclereductions.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -e
+
+# test one application of various multigrid cycles by looking at residual norm
+# reduction in 3D case
+
+# FIXME need to only test with initial conditions that satisfy the boundary
+# conditions, as otherwise the initial residual norm is mostly the mismatch
+# along the boundary
+
+# notes:
+#   * timings do not matter, but run with --with-debugging=0 configuration just
+#     to get it done (timings given below)
+#   * python one-liner to extract norm-reduction ratios
+#         import numpy; v = numpy.loadtxt('residuals.txt'); v[1::2] / v[::2]
+
+LEV=6    # 129x129x129 grid; each run takes less than 5 seconds?
+#LEV=7    # 257x257x257 grid; each run takes 30 seconds?
+
+COMMON="-fsh_dim 3 -fsh_problem manupoly -da_refine $LEV -snes_type ksponly -ksp_type richardson -ksp_max_it 1 -ksp_norm_type unpreconditioned -ksp_monitor -pc_type mg -mg_levels_${LEV}_ksp_converged_reason"
+
+function runcase() {
+  CMD="../fish $COMMON -pc_mg_type $1 -mg_levels_ksp_max_it $2 $3 $4"
+  echo $CMD >> transcript.txt
+  /usr/bin/time -f "real %e" $CMD &> tmp.txt
+  cat tmp.txt >> transcript.txt
+  grep "KSP Residual norm" tmp.txt | awk 'NF>1{print $NF}' >> residuals.txt  # extract number only
+  rm -f tmp.txt
+}
+
+rm -f transcript.txt residuals.txt
+for extra in "" "-fsh_init_random" "-fsh_cx 0.01 -fsh_cz 100.0" ; do
+    runcase multiplicative 2 "-pc_mg_cycle_type v" "$extra"
+    runcase multiplicative 2 "-pc_mg_cycle_type w" "$extra"
+    runcase multiplicative 1 "-pc_mg_cycle_type v -pc_mg_multiplicative_cycles 2" "$extra"
+    runcase multiplicative 1 "-pc_mg_cycle_type w -pc_mg_multiplicative_cycles 2" "$extra"
+    runcase full 2 "" "$extra"
+    runcase kaskade 4 "" "$extra"
+done
+
+
