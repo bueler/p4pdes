218a5219fb44c8ecade22069af2137c1d496a986
Ed Bueler <elbueler@alaska.edu>
2017-10-13 19:01:20 -0800

    this is the same set of computations, possibly clearer and closer to the generated Table in Chapter 6

--- c/ch6/study/cyclereductions.sh
+++ c/ch6/study/cyclereductions.sh
@@ -18,7 +18,7 @@ TRANSCRIPT=transcript.txt
 COMMON="-fsh_dim 3 -da_refine $LEV -snes_type ksponly -ksp_type richardson -ksp_max_it 1 -ksp_norm_type unpreconditioned -ksp_monitor -pc_type mg -mg_levels_${LEV}_ksp_converged_reason"
 
 function runcase() {
-  CMD="../fish $COMMON -pc_mg_type $1 -mg_levels_ksp_max_it $2 $3 $4"
+  CMD="../fish $COMMON -pc_mg_type $1 -mg_levels_ksp_max_it $2 -fsh_initial_type $3 $4"
   echo $CMD >> $TRANSCRIPT
   rm -f tmp.txt
   /usr/bin/time -f "real %e" $CMD &> tmp.txt
@@ -28,14 +28,15 @@ function runcase() {
 }
 
 rm -f $TRANSCRIPT
-#for extra in "" "-fsh_initial_type random" "-fsh_problem manupoly -fsh_cx 0.01 -fsh_cz 100.0" ; do
-for extra in "" "-fsh_initial_type random" ; do
-    runcase multiplicative 2 "-pc_mg_cycle_type v" "$extra"
-    runcase multiplicative 2 "-pc_mg_cycle_type w" "$extra"
-    runcase multiplicative 1 "-pc_mg_cycle_type v -pc_mg_multiplicative_cycles 2" "$extra"
-    runcase multiplicative 1 "-pc_mg_cycle_type w -pc_mg_multiplicative_cycles 2" "$extra"
-    runcase full 2 "" "$extra"
-    runcase kaskade 4 "" "$extra"
+for INIT in "zeros" "random" ; do
+    runcase multiplicative 2 $INIT "-pc_mg_cycle_type v"
+    runcase multiplicative 2 $INIT "-pc_mg_cycle_type w"
+    runcase multiplicative 1 $INIT "-pc_mg_cycle_type v -pc_mg_multiplicative_cycles 2"
+    runcase multiplicative 1 $INIT "-pc_mg_cycle_type w -pc_mg_multiplicative_cycles 2"
+    runcase full 2 $INIT ""
+    runcase kaskade 4 $INIT ""
 done
 
+#FIXME: test anisotropic at some point, with
+#   -fsh_problem manupoly -fsh_cx 0.01 -fsh_cz 100.0
 
