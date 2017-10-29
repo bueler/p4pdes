f000d88c98e03e91875b9e30f2493f3b55a5ded2
Ed Bueler <elbueler@alaska.edu>
2017-10-28 21:33:19 -0800

    add a coarser grid ... but FIXME

--- c/ch6/study/scaling3.sh
+++ c/ch6/study/scaling3.sh
@@ -13,12 +13,13 @@ set -e
 
 # shows number of CG iterations constant (7 at each level) and error is O(h^2):
 #   (level)    (mx)      (time)      (flops)
+#   3          17        2.8710e-02  1.31e+07
 #   4          33        1.5960e-01  9.65e+07
 #   5          65        9.2471e-01  7.41e+08
 #   6          129       6.1326e+00  5.81e+09
 #   7          257       4.6442e+01  4.60e+10
 
-# copy block of data above into data3.txt; then:
+# copy block of data above into data3.txt; then do in Matlab (FIXME: python):
 # >> load('data3.txt')
 # >> mx = data3(:,2);  time = data3(:,3);  flops = data3(:,4);
 # >> pt = polyfit(log(mx.^3),log(time),1)
@@ -29,7 +30,8 @@ set -e
 # pf =
 #       1.0015      7.8809                     % perfectly linear
 
-for LEV in 4 5 6 7; do
+for LEV in 3 4 5 6; do
+#for LEV in 3 4 5 6 7; do
     CMD="../fish -fsh_dim 3 -snes_type ksponly -ksp_type cg -pc_type mg -ksp_rtol 1.0e-10 -ksp_converged_reason -da_refine $LEV -log_view"
     echo "COMMAND:  $CMD"
     rm -rf tmp.txt
@@ -37,3 +39,4 @@ for LEV in 4 5 6 7; do
     grep -C 1 "problem manuexp" tmp.txt
     grep SNESSolve tmp.txt
 done
+
