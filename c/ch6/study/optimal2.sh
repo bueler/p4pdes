78cd39effdcdd3ae8e71301e0d688743adea089d
Ed Bueler <elbueler@alaska.edu>
2017-10-29 13:55:12 -0800

    simpler naming scheme matching book; and clean up

--- c/ch6/study/optimal2.sh
+++ c/ch6/study/optimal2.sh
@@ -1,26 +1,18 @@
 #!/bin/bash
 set -e
 
+# demonstrates optimality of a CG+MG solver for Poisson equation in 2D
+
 # run as:
-#   ./scaling2.sh &> scaling2.txt
+#   ./optimal2.sh &> optimal2.txt
+
 # use PETSC_ARCH with --with-debugging=0
+
 # limitation: going to -da_refine 12 gives out of memory on WORKSTATION with 16Gb
-# compare scaling3.sh
 
-# re measuring timing:  the time in SNESSolve includes assembly of system on
-# each coarser grid, through calls to FormJacobianLocal(); note
-# FormFunctionLocal() only called once, on finest grid
+# results:  see p4pdes-book/figs/optimal2.txt
 
-# shows number of CG iterations constant (7 at each level) and error is O(h^2):
-#   (level)    (mx)      (time)      (flops)
-#   6          129       9.2926e-02  4.04e+07
-#   7          257       2.4581e-01  1.60e+08
-#   8          513       9.0780e-01  6.39e+08
-#   9          1025      3.1085e+00  2.55e+09
-#   10         2049      1.1834e+01  1.02e+10
-#   11         4097      4.5728e+01  4.08e+10
-
-# copy block of data above into data2.txt; then:
+# copy block of data above into data2.txt; then:   # FIXME: replace w python
 # >> load('data2.txt')
 # >> mx = data2(:,2);  time = data2(:,3);  flops = data2(:,4);
 # >> pt = polyfit(log(mx.^2),log(time),1)
@@ -31,7 +23,7 @@ set -e
 # pf =
 #       1.0003      7.7907                     % perfectly linear
 
-for LEV in 6 7 8 9 10 11; do
+for LEV in 5 6 7 8 9 10 11; do
     CMD="../fish -fsh_dim 2 -snes_type ksponly -ksp_type cg -pc_type mg -ksp_rtol 1.0e-10 -ksp_converged_reason -da_refine $LEV -log_view"
     echo "COMMAND:  $CMD"
     rm -rf tmp.txt
--- c/ch6/study/optimal3.sh
+++ c/ch6/study/optimal3.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 set -e
 
+# demonstrates optimality of a CG+MG solver for Poisson equation in 3D
+
 # run as:
-#   ./scaling3.sh &> scaling3.txt
+#   ./optimal3.sh &> optimal3.txt
 # use PETSC_ARCH with --with-debugging=0
+
 # limitations: going to -da_refine 8 gives out of memory on WORKSTATION with 16Gb
-# compare scaling2.sh
 
-# re measuring timing:  the time in SNESSolve includes assembly of system on
-# each coarser grid, through calls to FormJacobianLocal(); note
-# FormFunctionLocal() only called once, on finest grid
+# re measured timing:  the time in SNESSolve includes assembly of system on
+# each coarser grid (= calls to FormJacobianLocal()), but note
+# FormFunctionLocal() is only called once (on finest grid)
 
-# shows number of CG iterations constant (7 at each level) and error is O(h^2):
-#   (level)    (mx)      (time)      (flops)
-#   3          17        2.8710e-02  1.31e+07
-#   4          33        1.5960e-01  9.65e+07
-#   5          65        9.2471e-01  7.41e+08
-#   6          129       6.1326e+00  5.81e+09
-#   7          257       4.6442e+01  4.60e+10
+# results:  see p4pdes-book/figs/optimal3.txt
 
-# copy block of data above into data3.txt; then do in Matlab (FIXME: python):
+# copy block of data above into data3.txt; then do in Matlab (FIXME: replace with python):
 # >> load('data3.txt')
 # >> mx = data3(:,2);  time = data3(:,3);  flops = data3(:,4);
 # >> pt = polyfit(log(mx.^3),log(time),1)
@@ -30,8 +26,7 @@ set -e
 # pf =
 #       1.0015      7.8809                     % perfectly linear
 
-for LEV in 3 4 5 6; do
-#for LEV in 3 4 5 6 7; do
+for LEV in 3 4 5 6 7; do
     CMD="../fish -fsh_dim 3 -snes_type ksponly -ksp_type cg -pc_type mg -ksp_rtol 1.0e-10 -ksp_converged_reason -da_refine $LEV -log_view"
     echo "COMMAND:  $CMD"
     rm -rf tmp.txt
