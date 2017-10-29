fcc1e701ea431004c6f2b7f3c68f7aa93f6c5046
Ed Bueler <elbueler@alaska.edu>
2017-10-29 14:06:00 -0800

    post-processing all moved to p4pdes-book now

--- c/ch6/study/optimal2.sh
+++ c/ch6/study/optimal2.sh
@@ -5,23 +5,12 @@ set -e
 
 # run as:
 #   ./optimal2.sh &> optimal2.txt
-
 # use PETSC_ARCH with --with-debugging=0
 
 # limitation: going to -da_refine 12 gives out of memory on WORKSTATION with 16Gb
 
 # results:  see p4pdes-book/figs/optimal2.txt
-
-# copy block of data above into data2.txt; then:   # FIXME: replace w python
-# >> load('data2.txt')
-# >> mx = data2(:,2);  time = data2(:,3);  flops = data2(:,4);
-# >> pt = polyfit(log(mx.^2),log(time),1)
-# pt =
-#      0.90554     -11.338                     % slightly sublinear
-# >> loglog(mx,time,'k-o',mx,exp(pt(1)*log(mx.^2)+pt(2)),'k--')
-# >> pf = polyfit(log(mx.^2),log(flops),1)
-# pf =
-#       1.0003      7.7907                     % perfectly linear
+# figure-generation:  see p4pdes-book/figs/optimal.py
 
 for LEV in 5 6 7 8 9 10 11; do
     CMD="../fish -fsh_dim 2 -snes_type ksponly -ksp_type cg -pc_type mg -ksp_rtol 1.0e-10 -ksp_converged_reason -da_refine $LEV -log_view"
--- c/ch6/study/optimal3.sh
+++ c/ch6/study/optimal3.sh
@@ -9,22 +9,12 @@ set -e
 
 # limitations: going to -da_refine 8 gives out of memory on WORKSTATION with 16Gb
 
-# re measured timing:  the time in SNESSolve includes assembly of system on
+# FIXME re measured timing:  the time in SNESSolve includes assembly of system on
 # each coarser grid (= calls to FormJacobianLocal()), but note
 # FormFunctionLocal() is only called once (on finest grid)
 
 # results:  see p4pdes-book/figs/optimal3.txt
-
-# copy block of data above into data3.txt; then do in Matlab (FIXME: replace with python):
-# >> load('data3.txt')
-# >> mx = data3(:,2);  time = data3(:,3);  flops = data3(:,4);
-# >> pt = polyfit(log(mx.^3),log(time),1)
-# pt =
-#       0.9213     -11.558                     % slightly sublinear
-# >> loglog(mx,time,'k-o',mx,exp(pt(1)*log(mx.^3)+pt(2)),'k--')
-# >> pf = polyfit(log(mx.^3),log(flops),1)
-# pf =
-#       1.0015      7.8809                     % perfectly linear
+# figure-generation:  see p4pdes-book/figs/optimal.py
 
 for LEV in 3 4 5 6 7; do
     CMD="../fish -fsh_dim 3 -snes_type ksponly -ksp_type cg -pc_type mg -ksp_rtol 1.0e-10 -ksp_converged_reason -da_refine $LEV -log_view"
