a39d522cc3e8644a2c07af35523d3ae75eb9b720
Ed Bueler <elbueler@alaska.edu>
2017-11-02 16:12:32 -0800

    the 2D fish runs worked as stated here; the 3D is being re-run because it crashed on coarse grid partitioning

--- c/ch13/cluster.sh
+++ c/ch13/cluster.sh
@@ -43,13 +43,20 @@ GO="mpiexec -n $SLURM_NTASKS -machinefile ./nodes.$SLURM_JOB_ID"
 # -da_refine 6 is 256x256; this does ~230 time steps
 $GO ../ch5/pattern -da_refine 6 -ptn_phi 0.05 -ptn_kappa 0.063 -ts_final_time 5000 -ptn_noisy_init 0.15 -ts_monitor -log_view
 
-# FISH:  solve 3D Poisson equation
-# -da_refine 8 is 513x513x513; uses ~6Gb memory
-$GO ../ch6/fish -fsh_dim 3 -da_refine 8 -pc_type mg -snes_type ksponly -ksp_converged_reason -log_view
-
-# FISH with Dryja&Widlund solver:  solve 2D Poisson equation
+# FISH with ASM+LU (single-level DD) solver:  solve 2D Poisson equation
+# -da_refine 9 is 1025 x 1025; this does hundreds of KSP iterations
 # note: this needs 512 tasks to be a data point in a figure in Chapter 6
-#$GO ../ch6/fish -fsh_dim 2 -snes_type ksponly -ksp_type gmres -ksp_rtol 1.0e-10 -ksp_converged_reason -log_view -pc_type mg -pc_mg_levels 2 -pc_mg_type additive -mg_levels_ksp_type preonly -mg_levels_pc_type asm -mg_levels_sub_pc_type lu -mg_coarse_ksp_type preonly -mg_coarse_pc_type redundant -mg_coarse_redundant_pc_type lu -da_refine 9
+$GO ../ch6/fish -fsh_dim 2 -snes_type ksponly -ksp_type gmres -ksp_rtol 1.0e-10 -ksp_converged_reason -log_view -pc_type asm -sub_pc_type lu -da_refine 9
+
+# FISH with Dryja&Widlund (two-level DD) solver:  solve 2D Poisson equation
+# -da_refine 9 is 1025 x 1025; this should do fewer than than 20 KSP iterations
+# note: this needs 512 tasks to be a data point in a figure in Chapter 6
+$GO ../ch6/fish -fsh_dim 2 -snes_type ksponly -ksp_type gmres -ksp_rtol 1.0e-10 -ksp_converged_reason -log_view -pc_type mg -pc_mg_levels 2 -pc_mg_type additive -mg_levels_ksp_type preonly -mg_levels_pc_type asm -mg_levels_sub_pc_type lu -mg_coarse_ksp_type preonly -mg_coarse_pc_type redundant -mg_coarse_redundant_pc_type lu -da_refine 9
+
+# FISH:  solve 3D Poisson equation
+# coarse grid is 17x17x17 so should work on O(10^3) processors (or fewer)
+# -da_refine 8 is 513x513x513; uses ~6Gb memory
+$GO ../ch6/fish -fsh_dim 3 -da_refine 8 -pc_mg_levels 6 -pc_type mg -snes_type ksponly -ksp_converged_reason -log_view
 
 # ADVECT:  solve 2D advection equation
 # -da_refine 8 is 1280x1280; this does ~2400 time steps
