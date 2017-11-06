24ca00320299e9f2a911a37f834e42d2a79a7b8a
Ed Bueler <elbueler@alaska.edu>
2017-11-06 11:43:24 -0900

    this script generates a figure in Chapter 6

---
+++ c/ch6/study/pccondition.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+# study condition numbers of preconditioned Poisson operators
+
+# run as:
+#   ./pccondition.sh &> pccondition.txt
+# use PETSC_ARCH with --with-debugging=0
+
+# results:  see p4pdes-book/figs/pccondition.txt
+# figure-generation:  see p4pdes-book/figs/pccondition.py
+
+for LEV in 3 4 5 6 7 8 9; do
+    for PC in none icc mg; do
+        CMD="../fish -snes_type ksponly -ksp_type cg -ksp_monitor_singular_value -da_refine $LEV -pc_type $PC"
+        #echo "COMMAND:  $CMD"
+        rm -rf tmp.txt
+        $CMD &> tmp.txt
+        COND=`tail -n 3 tmp.txt | head -n 1 | awk '{print $12}'`
+        echo "$LEV $COND # $PC"
+    done
+done
+
