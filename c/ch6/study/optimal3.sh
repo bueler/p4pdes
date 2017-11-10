45f112cd2d9535236c6ecc906dbd7113c3c41ee6
Ed Bueler <elbueler@alaska.edu>
2017-11-10 09:06:31 -0900

    3D fish is indeed optimal for -pc_mg_galerkin or -snes_fd_color or -snes_mf_operator (but demo for the latter two makes more sense w minimal)

--- c/ch6/study/optimal3.sh
+++ c/ch6/study/optimal3.sh
@@ -12,8 +12,14 @@ set -e
 # results:  see p4pdes-book/figs/optimal3.txt
 # figure-generation:  see p4pdes-book/figs/optimal.py
 
+# choose (uncomment) *one* of the following:
+ADD=
+#ADD=-pc_mg_galerkin
+#ADD=-snes_fd_color      # FIXME makes more sense to show for minimal
+#ADD=-snes_mf_operator   # ditto
+
 for LEV in 3 4 5 6 7; do
-    CMD="../fish -fsh_dim 3 -snes_type ksponly -ksp_type cg -pc_type mg -ksp_rtol 1.0e-10 -ksp_converged_reason -da_refine $LEV -log_view"
+    CMD="../fish -fsh_dim 3 -snes_type ksponly -ksp_type cg -pc_type mg $ADD -ksp_rtol 1.0e-10 -ksp_converged_reason -da_refine $LEV -log_view"
     echo "COMMAND:  $CMD"
     rm -rf tmp.txt
     $CMD &> tmp.txt
