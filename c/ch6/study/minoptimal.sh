79b617802ceccc4c90f3dd2690a40d3e7fc99e3d
Ed Bueler <elbueler@alaska.edu>
2017-11-29 12:15:13 -0900

    script as used to actually generate figure in book

--- c/ch6/study/minoptimal.sh
+++ c/ch6/study/minoptimal.sh
@@ -2,28 +2,23 @@
 set -e
 
 # demonstrates optimality of minimal surface equation Newton-Krylov solver
-# with grid sequencing and GMG preconditioning
+# with grid sequencing and CG and GMG preconditioning
 
 # run as:
 #   ./minoptimal.sh &> minoptimal.txt
 # use PETSC_ARCH with --with-debugging=0
 
-# limitation: FIXME gives out of memory on WORKSTATION with 16Gb
-
-# results:  see p4pdes-book/figs/FIXME
-# figure-generation:  see p4pdes-book/figs/FIXME
+# results & figure-generation:  see p4pdes-book/figs/minoptimal.txt|py
 
 # choose (uncomment) *one* of the following:
-PROB=tent               # note low regularity at boundary,
-                        # so linear problems get worse as grid is refined
-#PROB=catenoid
+PROB=catenoid
+#PROB=tent     # low regularity at boundary so linear problems get worse as grid is refined
 
 # choose (uncomment) *one* of the following:
-JAC=-snes_fd_color
-#JAC=-snes_mf_operator
+JAC=-snes_mf_operator
+#JAC=-snes_fd_color   # faster, but on finest levels FD jacobian loses quality
 
-for LEV in 5 6 7 8 9; do
-#for LEV in 5 6 7 8 9 10; do    # on finest level, FD jacobian seems to lose quality
+for LEV in 5 6 7 8 9 10; do
     CMD="../minimal -mse_problem $PROB $JAC -ksp_type cg -pc_type mg -snes_converged_reason -ksp_converged_reason -snes_grid_sequence $LEV -log_view"
     echo "COMMAND:  $CMD"
     rm -rf tmp.txt
