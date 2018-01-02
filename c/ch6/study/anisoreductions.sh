28f678af33fae67ed7add4a1dd15694a87b2f647
Ed Bueler <elbueler@alaska.edu>
2018-01-01 19:36:52 -0700

    add cheb+icc smoother case, and simplify

--- c/ch6/study/anisoreductions.sh
+++ c/ch6/study/anisoreductions.sh
@@ -8,13 +8,13 @@ set -e
 #   * timings do not matter (but using --with-debugging=0 is convenient)
 #   * run as:
 #         $ ./anisoreductions.sh > anisoresiduals.txt
-#   * generates additional file transcript.txt
 #   * python script to post process in p4pdes-book/figs/anisoreductions.py
+#   * generates additional files trans_*.txt, post-processed by
+#     p4pdes-book/figs/anisostagnate.py
 
-LEV=6    # 129x129 grid
+LEV=7    # 257x257 grid
 
 TRANSCRIPT=transcript.txt
-
 
 COMMON="-fsh_problem manupoly -fsh_initial_type random -da_refine $LEV -ksp_type richardson -ksp_norm_type unpreconditioned -ksp_monitor -ksp_rtol 0 -ksp_atol 0 -pc_type mg"
 
@@ -26,6 +26,8 @@ function runcasetranscripttmp() {
   cat tmp.txt >> $2
 }
 
+KSPITS=5
+
 function runcase() {
   runcasetranscripttmp "${1}" transcript.txt
   grep "0 KSP Residual norm" tmp.txt | awk 'NF>1{print $NF}'  # extract number only
@@ -35,9 +37,9 @@ function runcase() {
 
 rm -f $TRANSCRIPT
 
-# compare Chebyshev, SSOR, GS smoothing with anisotropy
-KSPITS=5
-for SMOOTH in "" "-mg_levels_ksp_type richardson -mg_levels_pc_type sor" "-mg_levels_ksp_type richardson -mg_levels_pc_type sor -mg_levels_pc_sor_forward"; do
+# compare Chebyshev+SSOR, Chebyshev+ICC, Richardson+SSOR, Richardson+GS smoothing
+# (and different levels of anisotropy)
+for SMOOTH in "" "-mg_levels_pc_type icc" "-mg_levels_ksp_type richardson -mg_levels_pc_type sor" "-mg_levels_ksp_type richardson -mg_levels_pc_type sor -mg_levels_pc_sor_forward"; do
     echo "#SMOOTH = $SMOOTH"       # note "#" is comment character for numpy.loadtxt()
     for CY in 1.0 1.0e1 1.0e2 1.0e3; do
         runcase "-ksp_max_it ${KSPITS} -fsh_cy ${CY} ${SMOOTH}"
@@ -45,18 +47,17 @@ for SMOOTH in "" "-mg_levels_ksp_type richardson -mg_levels_pc_type sor" "-mg_le
     done
 done
 
-echo
-echo
 
 # put longer runs into individual transcripts for stagnation figure
 # (which is created via c/sneskspplot.py)
 KSPITS=12
-for CY in 1.0 1.0e2; do
-    runcasetranscripttmp "-ksp_max_it ${KSPITS} -fsh_cy ${CY}" trans_${CY}_cheb.txt
-    rm -f tmp.txt
-    runcasetranscripttmp "-ksp_max_it ${KSPITS} -fsh_cy ${CY} -mg_levels_ksp_type richardson -mg_levels_pc_type sor" trans_${CY}_ssor.txt
-    rm -f tmp.txt
-    runcasetranscripttmp "-ksp_max_it ${KSPITS} -fsh_cy ${CY} -mg_levels_ksp_type richardson -mg_levels_pc_type sor -mg_levels_pc_sor_forward" trans_${CY}_gs.txt
-    rm -f tmp.txt
-done
+CY=1.0e3
+runcasetranscripttmp "-ksp_max_it ${KSPITS} -fsh_cy ${CY}" trans_chebssor.txt
+rm -f tmp.txt
+runcasetranscripttmp "-ksp_max_it ${KSPITS} -fsh_cy ${CY} -mg_levels_pc_type icc" trans_chebicc.txt
+rm -f tmp.txt
+runcasetranscripttmp "-ksp_max_it ${KSPITS} -fsh_cy ${CY} -mg_levels_ksp_type richardson -mg_levels_pc_type sor" trans_richssor.txt
+rm -f tmp.txt
+runcasetranscripttmp "-ksp_max_it ${KSPITS} -fsh_cy ${CY} -mg_levels_ksp_type richardson -mg_levels_pc_type sor -mg_levels_pc_sor_forward" trans_richgs.txt
+rm -f tmp.txt
 
