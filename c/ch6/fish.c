bueler/p4pdes

commit 8c0192e527df616aa770e370af57f4414dee4e58

clean up best practices

c/ch6/fish.c
@@ -81,12 +81,6 @@ static double f_rhs_3Dmanuexp(double x, double y, double z, void *ctx) {
     return 2.0 * x * exp(y + z);  // note  f = - laplacian u = - 2 u
 }
 
-// functions simply to put u_exact()=g_bdry() into a grid
-// these are irritatingly-dimension-dependent inside ...
-extern PetscErrorCode Form1DUExact(DMDALocalInfo*, Vec, PoissonCtx*);
-extern PetscErrorCode Form2DUExact(DMDALocalInfo*, Vec, PoissonCtx*);
-extern PetscErrorCode Form3DUExact(DMDALocalInfo*, Vec, PoissonCtx*);
-
 //STARTPTRARRAYS
 // arrays of pointers to functions:   ..._ptr[DIMS]
 static void* residual_ptr[3]
@@ -117,6 +111,11 @@ static void* f_rhs_ptr[3][3]
 static const char* InitialTypes[] = {"zeros","random",
                                      "InitialType", "", NULL};
 
+// functions simply to put u_exact()=g_bdry() into a grid
+// these are irritatingly-dimension-dependent inside ...
+extern PetscErrorCode Form1DUExact(DMDALocalInfo*, Vec, PoissonCtx*);
+extern PetscErrorCode Form2DUExact(DMDALocalInfo*, Vec, PoissonCtx*);
+extern PetscErrorCode Form3DUExact(DMDALocalInfo*, Vec, PoissonCtx*);
 
 int main(int argc,char **argv) {
     PetscErrorCode ierr;

c/ch7/minimal.c
@@ -45,14 +45,14 @@ static double DD(double w, double q) {
     return pow(1.0 + w,q);
 }
 
+typedef enum {TENT, CATENOID} ProblemType;
+static const char* ProblemTypes[] = {"tent","catenoid",
+                                     "ProblemType", "", NULL};
+
 extern PetscErrorCode FormExactFromG(DMDALocalInfo*, Vec, PoissonCtx*);
 extern PetscErrorCode FormFunctionLocal(DMDALocalInfo*, double**,
                                         double **FF, PoissonCtx*);
 extern PetscErrorCode MSEMonitor(SNES, int, double, void*);
-
-typedef enum {TENT, CATENOID} ProblemType;
-static const char* ProblemTypes[] = {"tent","catenoid",
-                                     "ProblemType", "", NULL};
 
 int main(int argc,char **argv) {
     PetscErrorCode ierr;

commit 5dd1fbbd28ef9a7afded591bb420679058ba0257

fix compile fail caused by 8c0192e5

c/ch6/fish.c
@@ -81,6 +81,12 @@ static double f_rhs_3Dmanuexp(double x, double y, double z, void *ctx) {
     return 2.0 * x * exp(y + z);  // note  f = - laplacian u = - 2 u
 }
 
+// functions simply to put u_exact()=g_bdry() into a grid
+// these are irritatingly-dimension-dependent inside ...
+extern PetscErrorCode Form1DUExact(DMDALocalInfo*, Vec, PoissonCtx*);
+extern PetscErrorCode Form2DUExact(DMDALocalInfo*, Vec, PoissonCtx*);
+extern PetscErrorCode Form3DUExact(DMDALocalInfo*, Vec, PoissonCtx*);
+
 //STARTPTRARRAYS
 // arrays of pointers to functions:   ..._ptr[DIMS]
 static void* residual_ptr[3]
@@ -110,12 +116,6 @@ static void* f_rhs_ptr[3][3]
 
 static const char* InitialTypes[] = {"zeros","random",
                                      "InitialType", "", NULL};
-
-// functions simply to put u_exact()=g_bdry() into a grid
-// these are irritatingly-dimension-dependent inside ...
-extern PetscErrorCode Form1DUExact(DMDALocalInfo*, Vec, PoissonCtx*);
-extern PetscErrorCode Form2DUExact(DMDALocalInfo*, Vec, PoissonCtx*);
-extern PetscErrorCode Form3DUExact(DMDALocalInfo*, Vec, PoissonCtx*);
 
 int main(int argc,char **argv) {
     PetscErrorCode ierr;