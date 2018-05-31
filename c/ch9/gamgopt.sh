df8cce77b632f5f27206f088c3d2ff3fde65fdcc
Ed Bueler <elbueler@alaska.edu>
2018-05-30 19:15:02 -0800

    tidy up and test ... works great with gamgopt.sh and meshes/trap*

--- c/ch9/gamgopt.sh
+++ c/ch9/gamgopt.sh
@@ -4,13 +4,12 @@
 # meshes; uses CG for Krylov
 
 # main example uses PETSC_ARCH with --with-debugging=0:
-#   ./gentraps.sh trap 10
-#   ./gamgopt.sh meshes/trap 0 10 &> gamgopt.txt
+#   ./refinetraps.sh meshes/trap LEV
+#   ./gamgopt.sh meshes/trap 0 LEV &> gamgopt.txt
+# LEV=12 with N=10492929 nodes and 100 sec run achievable on ed-galago
 
-# other examples; run gensquares.sh or koch/genkochs.sh first:
-#   ./gamgopt.sh meshes/sq 3 4      # -un_case 3 and 4 levels
+# other examples: FIXME redo with genstructured.py and koch/ stuff
 #   ./gamgopt.sh meshes/sq 3 6      # ... level 6 is large: 3200x3200 grid w. N=10^7
-#   ./gamgopt.sh koch/koch 4 7      # -un_case 4 and 7 levels
 #   ./gamgopt.sh koch/koch 4 9      # ... level 9 is large: N = 4 x 10^6
 
 # results & figure-generation:  see p4pdes-book/figs/gamgopt.txt|py
@@ -21,9 +20,10 @@ MAXLEV=$3
 
 for (( Z=1; Z<=$MAXLEV; Z++ )); do
     echo "running level ${Z}"
-    IN=$NAME.$Z
-    cmd="./unfem -un_case $CASE -snes_type ksponly -ksp_rtol 1.0e-8 -ksp_type cg -pc_type gamg -ksp_converged_reason -un_mesh $IN"
+    IN=${NAME}$Z
+    cmd="./unfem -un_case $CASE -snes_type ksponly -ksp_rtol 1.0e-9 -ksp_type cg -pc_type gamg -ksp_converged_reason -un_mesh $IN"
     rm -f foo.txt
+    echo $cmd
     $cmd -log_view &> foo.txt
     'grep' "Linear solve converged due to" foo.txt
     'grep' "result for N" foo.txt
--- c/ch9/refinetraps.sh
+++ c/ch9/refinetraps.sh
@@ -6,15 +6,15 @@
 # run "make test" first to get links to PETSc python scripts for binary files
 
 # example:
-#   ./gentraps.sh trap 5
+#   ./refinetraps.sh meshes/trap 5
 
 NAME=$1
 MAXLEV=$2
 
-gmsh -2 meshes/${NAME}.geo -o meshes/${NAME}1.msh
-./msh2petsc.py meshes/${NAME}1.msh
+gmsh -2 ${NAME}.geo -o ${NAME}1.msh
+./msh2petsc.py ${NAME}1.msh
 for (( Z=1; Z<$MAXLEV; Z++ )); do
-    gmsh -refine meshes/${NAME}$Z.msh -o meshes/${NAME}$((Z+1)).msh
-    ./msh2petsc.py meshes/$NAME$((Z+1)).msh
+    gmsh -refine ${NAME}$Z.msh -o ${NAME}$((Z+1)).msh
+    ./msh2petsc.py $NAME$((Z+1)).msh
 done
 
