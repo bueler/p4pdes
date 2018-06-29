6b364889ea2710937e52faa7a185150d4f0d3795
Ed Bueler <elbueler@alaska.edu>
2018-06-29 16:12:38 -0700

    allow easy setting of -ad3_eps

--- c/ch10/study/ad3opt.sh
+++ c/ch10/study/ad3opt.sh
@@ -5,10 +5,11 @@
 # small coarse grid for efficiency.
 
 MAXLEV=6
+EPS=0.1
 
 for (( Z=2; Z<=$MAXLEV; Z++ )); do
     echo "running level ${Z}"
-    cmd="../ad3 -ad3_eps 0.1 -ad3_limiter none -da_grid_x 4 -da_grid_y 3 -da_grid_z 4 -da_refine ${Z} -ksp_type bcgs -ksp_rtol 1.0e-9 -ksp_monitor -ksp_converged_reason -pc_type mg -mg_levels_ksp_type richardson -mg_levels_pc_type ilu"
+    cmd="../ad3 -ad3_eps ${EPS} -ad3_limiter none -da_grid_x 4 -da_grid_y 3 -da_grid_z 4 -da_refine ${Z} -ksp_type bcgs -ksp_rtol 1.0e-9 -ksp_monitor -ksp_converged_reason -pc_type mg -mg_levels_ksp_type richardson -mg_levels_pc_type ilu"
     rm -f foo.txt
     echo $cmd
     $cmd -log_view &> foo.txt
