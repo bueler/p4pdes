103e703465710181fc7cdc3bee877fcd22cef706
Ed Bueler <elbueler@alaska.edu>
2018-06-29 15:59:11 -0700

    use decent coarse grid for optimality study

--- c/ch10/study/ad3opt.sh
+++ c/ch10/study/ad3opt.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# generate data to measure flop/N for ad3 using GMG with ILU smoothing on
-# advection-diffusion problem
+# Generate data to measure flop/N for ad3 using GMG with ILU smoothing on
+# advection-diffusion problem.  Uses no limiter (= first-order upwinding) and
+# small coarse grid for efficiency.
 
-MAXLEV=5   # can go to level 6 on ed-galago ... uses ~ 36 Gb memory
+MAXLEV=6
 
 for (( Z=2; Z<=$MAXLEV; Z++ )); do
     echo "running level ${Z}"
-    cmd="../ad3 -ad3_limiter none -ksp_converged_reason -da_refine ${Z} -ksp_rtol 1.0e-9 -pc_type mg -mg_levels_ksp_type richardson -mg_levels_pc_type ilu -ksp_monitor -ad3_eps 0.1 -ksp_type bcgs"
+    cmd="../ad3 -ad3_eps 0.1 -ad3_limiter none -da_grid_x 4 -da_grid_y 3 -da_grid_z 4 -da_refine ${Z} -ksp_type bcgs -ksp_rtol 1.0e-9 -ksp_monitor -ksp_converged_reason -pc_type mg -mg_levels_ksp_type richardson -mg_levels_pc_type ilu"
     rm -f foo.txt
     echo $cmd
     $cmd -log_view &> foo.txt
