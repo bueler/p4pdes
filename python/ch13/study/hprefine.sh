d06bcd8d467f43b1115f38cc7cbe0e09dee758d1
Ed Bueler <elbueler@alaska.edu>
2018-07-23 11:46:31 -0800

    does h and p .. need to make figures

--- python/ch13/study/hprefine.sh
+++ python/ch13/study/hprefine.sh
@@ -2,18 +2,14 @@
 set -e
 set +x
 
-# FIXME this version only has p-refinement
-
-# FIXME make figure with p-versus-error
-# FIXME make dual-axis figure with p-versus-matrix-dimension and p-versus-sparsity-ratio
-
-# FIXME figures as above include h-refine and hp-refine too?
+# FIXME make figure with h/p-versus-error
+# FIXME make dual-axis figure with h/p-versus-matrix-dimension and h/p-versus-sparsity-ratio
 
 # run as
 #    ./hprefine.sh &> hprefine.txt
 
 function runcase() {
-    CMD="../firefish.py -mx $1 -my $1 -order $2 $3 -s_ksp_type preonly -s_pc_type lu -s_ksp_view"
+    CMD="../firefish.py -mx $1 -my $1 -order $2 $3 -s_ksp_view"
     echo "COMMAND:  $CMD"
     rm -rf tmp.txt
     $CMD &> tmp.txt
@@ -23,8 +19,15 @@ function runcase() {
     grep -A 3 "Mat Object: (s_)" tmp.txt | grep "total:"
 }
 
-for P in 1 2 3 4 5 6 7; do
-     runcase 5 $P ""
+# h refine
+for M in 5 9 17 33 65 129 257 513 1025; do
+     runcase $M 1 "-s_ksp_type cg -s_pc_type gamg -s_ksp_rtol 1.0e-14"
 done
+
+# p refine
+for P in 1 2 3 4 5 6 7; do
+     runcase 5 $P "-s_ksp_type preonly -s_pc_type lu"
+done
+
 rm -rf tmp.txt
 
