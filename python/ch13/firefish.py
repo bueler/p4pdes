c06e644ed10df006bda9fd1b4051c5c14541339a
Ed Bueler <elbueler@alaska.edu>
2018-07-25 23:16:21 -0800

    code is slightly longer but now it does GMG; sweet; I should read Firedrake docs more

--- python/ch13/firefish.py
+++ python/ch13/firefish.py
@@ -19,14 +19,21 @@ parser.add_argument('-order', type=int, default=1, metavar='X',
                     help='polynomial degree for elements')
 parser.add_argument('-quad', action='store_true', default=False,
                     help='use quadrilateral finite elements')
+parser.add_argument('-refine', type=int, default=-1, metavar='X',
+                    help='number of refinement levels (e.g. for GMG)')
 args, unknown = parser.parse_known_args()
 
-# Create mesh and define function space
-mesh = UnitSquareMesh(args.mx-1, args.my-1, quadrilateral=args.quad)
+# Create mesh, enabling geometric multigrid using heirarchy
+mx, my = args.mx, args.my
+mesh = UnitSquareMesh(mx-1, my-1, quadrilateral=args.quad)
+if args.refine > 0:
+    hierarchy = MeshHierarchy(mesh, args.refine)
+    mesh = hierarchy[-1]     # i.e. the fine mesh
+    mx, my = (mx-1) * 2**args.refine + 1, (my-1) * 2**args.refine + 1
 x,y = SpatialCoordinate(mesh)
-W = FunctionSpace(mesh, 'Lagrange', degree=args.order)
 
-# Define right-hand side and weak form.
+# Define function space, right-hand side, and weak form.
+W = FunctionSpace(mesh, 'Lagrange', degree=args.order)
 f_rhs = Function(W).interpolate(x * exp(y))  # manufactured
 u = Function(W)  # initialized to zero
 v = TestFunction(W)
@@ -45,8 +52,7 @@ elementstr = '%s^%d' % (['P','Q'][args.quad],args.order)
 u_exact = g_bdry
 error_Linf = max(abs(u.vector().array() - u_exact.vector().array()))
 error_L2 = sqrt(assemble(dot(u - u_exact, u - u_exact) * dx))
-print('done on %d x %d point grid with %s elements:' \
-      % (args.mx,args.my,elementstr))
+print('done on %d x %d grid with %s elements:' % (mx,my,elementstr))
 print('  error |u-uexact|_inf = %.3e, |u-uexact|_h = %.3e' \
       % (error_Linf,error_L2))
 
