942d79240d228b3cb4b213ddcc26be26d2ca5d39
Ed Bueler <elbueler@alaska.edu>
2018-07-28 14:24:31 -0800

    generate better hp-refine results for chapter 13

--- python/ch13/study/hprefine.sh
+++ python/ch13/study/hprefine.sh
@@ -2,14 +2,11 @@
 set -e
 set +x
 
-# FIXME make figure with h/p-versus-error
-# FIXME make dual-axis figure with h/p-versus-matrix-dimension and h/p-versus-sparsity-ratio
-
 # run as
 #    ./hprefine.sh &> hprefine.txt
 
 function runcase() {
-    CMD="../firefish.py -mx $1 -my $1 -order $2 $3 -s_ksp_view -log_view"
+    CMD="../firefish.py -refine $1 -order $2 $3 -s_ksp_view -log_view"
     echo "COMMAND:  $CMD"
     rm -rf tmp.txt
     $CMD &> tmp.txt
@@ -21,13 +18,13 @@ function runcase() {
 }
 
 # h refine
-for M in 5 9 17 33 65 129 257 513 1025; do
-     runcase $M 1 "-s_ksp_type cg -s_pc_type gamg -s_ksp_rtol 1.0e-14"
+for L in 1 2 3 4 5 6 7 8 9; do
+     runcase $L 1 "-s_ksp_type cg -s_pc_type mg -s_ksp_rtol 1.0e-14"
 done
 
 # p refine
 for P in 1 2 3 4 5 6 7 8; do
-     runcase 5 $P "-s_ksp_type preonly -s_pc_type lu"
+     runcase 1 $P "-s_ksp_type preonly -s_pc_type cholesky"
 done
 
 rm -rf tmp.txt
