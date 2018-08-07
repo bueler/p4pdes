68b308b7d07b9e43f7834e2e66c2f61ad9fc3b29
Ed Bueler <elbueler@alaska.edu>
2018-08-06 16:33:06 -0800

    update script with minor fiddles and corrections

--- c/cluster/cluster.sh
+++ c/cluster/cluster.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-# This is a sample batch file for running jobs on a linux cluster.  A goal is that the
-# total run time is less than an hour for > 8 processes.  If you have a bigger machine
-# (e.g. >100 processes) consider refining the grids more in the runs below.
+# This is a sample batch file for running jobs on a linux cluster.  A goal is
+# that the total run time is less than an hour for P > 8 processes.  With a
+# bigger machine (e.g. >100 processes) consider refining the grids more in
+# the runs below.
 
-# In parent directory ../c/, set PETSC_ARCH to a --with-debugging=0 PETSc configuration.
-# Then run "make distclean" to clean out old executables.  Then run "make test" to make sure
-# executables in ../ch*/ get built.
+# In parent directory ../c/, set PETSC_ARCH to a --with-debugging=0 PETSc
+# configuration.  Then run "make distclean" to clean out old executables.
+# Then run "make test" to make sure executables in ../ch*/ get built.
 
 # For each run a -log_view is generated.  These can be analyzed for performance
 # data.  For example do
 #   grep "(sec):" cluster.xxxx
 # to see total times of runs.
 
-# You will need to edit this file to match your batch system, and for each run!!
-# Many details below are particular to the Slurm batch system (see https://slurm.schedmd.com/).
+# You will need to edit this file to match your batch system!!
+
+# Many details below are particular to the Slurm batch system;
+#     https://slurm.schedmd.com/
 
 #SBATCH --partition=debug
 #SBATCH --ntasks=12
@@ -25,7 +28,7 @@
 #SBATCH --mail-type=FAIL
 #SBATCH --output=cluster.o.%j
 
-# Submit from c/ch13/ for this to work.
+# Submit from c/cluster/ for this to work.
 cd $SLURM_SUBMIT_DIR
 
 # This cluster needs these for some reason.
@@ -38,7 +41,8 @@ srun -l /bin/hostname | sort -n | awk '{print $2}' > ./nodes.$SLURM_JOB_ID
 # Launch the MPI application.  Tested with mpich2.
 GO="mpiexec -n $SLURM_NTASKS -machinefile ./nodes.$SLURM_JOB_ID"
 
-#---------------------------------------------------------------------------------
+#-------------------------- EXAMPLES ------------------------------------
+
 # PATTERN:  solve 2D, time-dependent, coupled diffusion-reaction equations
 # -da_refine 6 is 256x256; this does ~230 time steps
 $GO ../ch5/pattern -da_refine 6 -ptn_phi 0.05 -ptn_kappa 0.063 -ts_final_time 5000 -ptn_noisy_init 0.15 -ts_monitor -log_view
@@ -56,21 +60,22 @@ $GO ../ch6/fish -fsh_dim 2 -snes_type ksponly -ksp_type gmres -ksp_rtol 1.0e-10 
 # FISH:  solve 3D Poisson equation
 # coarse grid is 17x17x17 so should work on O(10^3) processors (or fewer)
 # -da_refine 8 is 513x513x513; uses ~6Gb memory
+# FIXME consider adding telescope
 $GO ../ch6/fish -fsh_dim 3 -da_refine 8 -pc_mg_levels 6 -pc_type mg -snes_type ksponly -ksp_converged_reason -log_view
+
+# FIXME consider a minimal example using NEWTON+CG+GMG
 
 # ADVECT:  solve 2D advection equation
 # -da_refine 8 is 1280x1280; this does ~2400 time steps
-$GO ../ch9/advect -da_refine 8 -ts_final_time 1.0 -ts_rk_type 3bs -ts_monitor -log_view
+$GO ../ch11/advect -da_refine 8 -ts_final_time 1.0 -ts_rk_type 3bs -ts_monitor -log_view
 
 # OBSTACLE:  solve 2D inequality-constrained free-boundary problem
 # 33x33 base grid with -snes_grid_sequence 6 is 2049x2049 finest grid
-$GO ../ch10/obstacle -da_grid_x 33 -da_grid_y 33 -snes_grid_sequence 6 -snes_converged_reason -snes_monitor -ksp_converged_reason -pc_type mg -log_view
+$GO ../ch12/obstacle -da_grid_x 33 -da_grid_y 33 -snes_grid_sequence 6 -snes_converged_reason -snes_monitor -ksp_converged_reason -pc_type mg -log_view
 
-#FIXME add an unstructured Poisson problem from Chapter 8 using GAMG?
+#FIXME add an unstructured Stokes problem from Chapter 13 using Firedrake
 
-#FIXME add an unstructured Stokes problem from Chapter 11?
-
-#---------------------------------------------------------------------------------
+#-----------------------------------------------------------------------
 
 # Clean up the machinefile.
 rm ./nodes.$SLURM_JOB_ID
