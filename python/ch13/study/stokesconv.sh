37fd22895c99609ab56d29343d1c5f3af87e44b8
Ed Bueler <elbueler@alaska.edu>
2018-08-13 22:30:54 -0800

    update convergence script and results

--- python/ch13/study/stokesconv.sh
+++ python/ch13/study/stokesconv.sh
@@ -5,11 +5,12 @@ set +x
 # run as
 #    ./stokesconv.sh &> stokesconv.txt
 
-# methods: P2xP1, Q2xQ1, P3xP2, CD(P2xP0)
+# FE methods: P2xP1, Q2xQ1, P3xP2, CD(P2xP0)
 
-for METH in "" "-quad" "-uorder 3 -porder 2" "-porder 0 -dpressure"; do
-    for L in 2 3 4 5 6 7 8; do  # WARNING: level 8 expensive
-        ../stokes.py -analytical $METH -refine $L -s_ksp_rtol 1.0e-11
+for FE in "" "-quad" "-udegree 3 -pdegree 2" "-pdegree 0 -dpressure"; do
+    for LEV in 2 3 4 5 6 7 8; do  # WARNING: level 8 (=513x513) is expensive
+        ../stokes.py -analytical -package schur2 -s_ksp_converged_reason \
+            -s_ksp_rtol 1.0e-11 $FE -refine $LEV
     done
 done
 
