5f6658ba16ccd011f3a4a2e6ede5f0b36eaedcec
Ed Bueler <elbueler@alaska.edu>
2018-08-27 22:48:14 -0800

    simplify and clean fish.py

--- python/ch13/fish.py
+++ python/ch13/fish.py
@@ -16,7 +16,7 @@ parser.add_argument('-my', type=int, default=3, metavar='MY',
                     help='number of grid points in y-direction')
 parser.add_argument('-o', metavar='NAME', type=str, default='',
                     help='output file name ending with .pvd')
-parser.add_argument('-order', type=int, default=1, metavar='X',
+parser.add_argument('-k', type=int, default=1, metavar='K',
                     help='polynomial degree for elements')
 parser.add_argument('-quad', action='store_true', default=False,
                     help='use quadrilateral finite elements')
@@ -24,7 +24,7 @@ parser.add_argument('-refine', type=int, default=-1, metavar='X',
                     help='number of refinement levels (e.g. for GMG)')
 args, unknown = parser.parse_known_args()
 
-# Create mesh, enabling GMG using hierarchy
+# Create mesh, enabling GMG via refinement using hierarchy
 mx, my = args.mx, args.my
 mesh = UnitSquareMesh(mx-1, my-1, quadrilateral=args.quad)
 if args.refine > 0:
@@ -35,22 +35,24 @@ x,y = SpatialCoordinate(mesh)
 mesh._plex.viewFromOptions('-dm_view')
 
 # Define function space, right-hand side, and weak form.
-W = FunctionSpace(mesh, 'Lagrange', degree=args.order)
+W = FunctionSpace(mesh, 'Lagrange', degree=args.k)
 f_rhs = Function(W).interpolate(x * exp(y))  # manufactured
-u = Function(W)  # initialized to zero
+u = Function(W)  # initialized to zero here
 v = TestFunction(W)
 F = (dot(grad(u), grad(v)) - f_rhs * v) * dx
 
-# Call solver, including boundary conditions
+# Dirichlet boundary conditions
 g_bdry = Function(W).interpolate(- x * exp(y))  # = exact solution
 bdry_ids = (1, 2, 3, 4)   # all four sides of boundary
 bc = DirichletBC(W, g_bdry, bdry_ids)
+
+# Solve
 solve(F == 0, u, bcs = [bc], options_prefix = 's',
       solver_parameters = {'snes_type': 'ksponly',
                            'ksp_type': 'cg'})
 
-# Compute error in L_infty and L_2 norm
-elementstr = '%s^%d' % (['P','Q'][args.quad],args.order)
+# Numerical error in L_infty and L_2 norm
+elementstr = '%s^%d' % (['P','Q'][args.quad],args.k)
 udiff = Function(W).interpolate(u - g_bdry)
 with udiff.dat.vec_ro as vudiff:
     error_Linf = abs(vudiff).max()[1]
