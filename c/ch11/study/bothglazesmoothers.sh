3231d579c36a7201721e29a7ddf787e251ee250d
Ed Bueler <elbueler@alaska.edu>
2019-05-29 15:58:28 -0800

    a script to test smoothers ... but now I realize that the freakishness of KSPRICHARDSON means I have to go back through all my results and NOT use -ksp_type richardson because it cannot be rationally inspected without changing its behavior

---
+++ c/ch11/study/bothglazesmoothers.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+# run with --with-debugging=0 configuration
+
+# generate table comparing GMG smoothers for problem GLAZE using eps=1/200
+# and first-order upwinding at all levels
+
+# edited result is put directly in p4pdes-book/chaps/advdif.tex
+
+for SMOOTH in "-mg_levels_pc_type sor" \
+              "-mg_levels_pc_type ilu" \
+              "-mg_levels_pc_type ilu -mg_levels_pc_factor_levels 1"; do
+    for LEV in 7 8 9 10; do
+        CMD="../both -bth_problem glaze -snes_type ksponly -ksp_type bcgs -ksp_converged_reason -pc_type mg -mg_levels_ksp_type richardson -da_refine $LEV $SMOOTH"
+        echo $CMD
+        $CMD
+    done
+done
+
