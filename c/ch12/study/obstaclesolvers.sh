fc87c57b43dc5ee50a115095ae800264e26e70ea
Ed Bueler <elbueler@alaska.edu>
2019-05-31 15:22:58 -0800

    redo da_refine/snes_grid_sequence comparison, going to one less level; regarding changing number of smoother iterations, it was necessary so all runs would converge, but see "Previously the KSP Chebyshev implementation always did one more iteration than requested ..." at 3.11 changelog https://www.mcs.anl.gov/petsc/documentation/changes/311.html

--- c/ch12/study/obstaclesolvers.sh
+++ c/ch12/study/obstaclesolvers.sh
@@ -4,16 +4,16 @@ set +x
 
 # run with --with-debugging=0 build, as
 #   ./obstaclesolvers.sh &> obstaclesolvers.txt
-# generates table in chapter 11
+# generates tables 12.1 and 12.2
 
-MAXLEV=8
+MAXLEV=7
 
 for GRD in -da_refine -snes_grid_sequence; do
     for TYPE in vinewtonrsls vinewtonssls; do
         echo
         echo "grid by ${GRD}, type ${TYPE}:"
         for (( LEV=3; LEV<=$MAXLEV; LEV++ )); do
-            ../obstacle -snes_type $TYPE $GRD $LEV -ksp_type cg -pc_type mg -snes_max_it 200;
+            ../obstacle -snes_type $TYPE $GRD $LEV -ksp_type cg -pc_type mg -snes_max_it 200 -mg_levels_ksp_max_it 3;
         done;
     done
 done
