f0a507fbe0244e089cef5957cdd959ff730b45f0
Ed Bueler <elbueler@alaska.edu>
2019-06-12 14:20:30 -0800

    simplify script ... data from obstaclemgruns.txt is enough to settle on RS+GMG

--- c/ch12/study/obstacleoptimal.sh
+++ c/ch12/study/obstacleoptimal.sh
@@ -7,19 +7,15 @@ set +x
 
 # p4pdes-book/figs/obstacle.py uses result to generate N vs flops/N figure in Chapter 12
 
-MAXLEV=10   # 10 is 2049^2 grid;  SS solver takes more than 20 min on this grid, so was stopped
+MAXLEV=11   # 11 is 4097^2 grid; requires 16Gb memory
 
-for SOLVE in "-snes_type vinewtonrsls -pc_type mg -mg_levels_ksp_type richardson" \
-             "-snes_type vinewtonrsls -pc_type gamg -pc_gamg_type classical" \
-             "-snes_type vinewtonssls -pc_type gamg -pc_gamg_type classical"; do
-    echo "METHOD:  $SOLVE"
-    for (( LEV=3; LEV<=$MAXLEV; LEV++ )); do
-        rm -rf tmp.txt
-        ../obstacle $SOLVE -snes_grid_sequence $LEV -log_view > tmp.txt
-        grep "errors:" tmp.txt
-        grep "last KSP iters" tmp.txt
-        grep "Flop:  " tmp.txt
-    done
-    echo
+SOLVE="-snes_type vinewtonrsls -pc_type mg -mg_levels_ksp_type richardson"
+echo "METHOD:  $SOLVE"
+for (( LEV=3; LEV<=$MAXLEV; LEV++ )); do
+    rm -rf tmp.txt
+    ../obstacle $SOLVE -snes_grid_sequence $LEV -log_view > tmp.txt
+    grep "errors:" tmp.txt
+    grep "last KSP iters" tmp.txt
+    grep "Flop:  " tmp.txt
 done
 
