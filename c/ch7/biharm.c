c2950ef11b200c52e554dcaa74531a9b50122021
Ed Bueler <elbueler@alaska.edu>
2019-06-17 19:23:00 -0800

    a viable solver for the biharmonic (plate) problem in the case of simply-supported boundary conditions

--- c/ch7/biharm.c
+++ c/ch7/biharm.c
@@ -2,24 +2,39 @@ static char help[] =
 "Solve the linear biharmonic equation in 2D.  Option prefix bh_.\n"
 "Equation is\n"
 "  - grad^4 u = f\n"
-"equivalently\n"
-"  - u_xxxx - 2 u_xxyy - u_yyyy = f(x,y),\n"
-"on the unit square S=(0,1)^2 subject to simply-supported boundary conditions:\n"
+"equivalently  - u_xxxx - 2 u_xxyy - u_yyyy = f(x,y),  on the unit square\n"
+"S=(0,1)^2 subject to homogeneous simply-supported boundary conditions:\n"
 "  u = 0,  Lap u = 0\n"
 "where Lap u = u_xx + u_yy.  The equation is rewritten as a 2x2 block system\n"
 "with SPD Laplacian blocks on the diagonal,\n"
-"   / -Lap |   0   \\  / v \\   / 0 \\ \n"
-"   |------|-------|  |---| = |---| \n"
-"   \\ - I  | - Lap /  \\ u /   \\-f / \n"
+"   / -Lap |   0  \\  / v \\   /  0 \\ \n"
+"   |------|------|  |---| = |----| \n"
+"   \\  -I  | -Lap /  \\ u /   \\ -f / \n"
 "Includes manufactured, polynomial exact solution.  The discretization is\n"
 "structured-grid (DMDA) finite differences.  Option -snes_fd_color (essentially)\n"
 "required to form the Jacobian.  Recommended preconditioning combines\n"
 "Gauss-Seidel-type (i.e. multiplicative) fieldsplit with GMG as the\n"
 "preconditioner for the diagonal blocks:\n"
-"   -pc_type fieldsplit -pc_fieldsplit_type multiplicative\n"
-"   -fieldsplit_0_ksp_type preonly -fieldsplit_0_pc_type mg\n"
-"   -fieldsplit_1_ksp_type preonly -fieldsplit_1_pc_type mg\n\n";
+"   -pc_type fieldsplit -pc_fieldsplit_type multiplicative \\\n"
+"   -fieldsplit_v_pc_type mg -fieldsplit_u_pc_type mg\n\n";
 
+/*
+1. view of solver; actually seems to be multiplicative fieldsplit with 2-level multigrid as preconditioner in each block:
+    ./biharm -ksp_monitor_short -pc_type fieldsplit -pc_fieldsplit_type multiplicative -da_refine 1 -fieldsplit_v_pc_type mg -fieldsplit_v_pc_mg_galerkin -fieldsplit_v_pc_mg_levels 2 -fieldsplit_u_pc_type mg -fieldsplit_u_pc_mg_galerkin -fieldsplit_u_pc_mg_levels 2 -snes_view |less
+  NOTE:  -fieldsplit_?_ksp_type preonly  is default
+  BUG: removing "galerkin"s causes memory corruption errors
+
+2. optimal; 2 KSP iterations on all grids up to 2049^2:
+    for LEV in 5 6 7 8 9 10; do ./biharm -ksp_converged_reason -pc_type fieldsplit -da_refine $LEV -fieldsplit_v_pc_type mg -fieldsplit_v_pc_mg_galerkin -fieldsplit_v_pc_mg_levels $LEV -fieldsplit_u_pc_type mg -fieldsplit_u_pc_mg_galerkin -fieldsplit_u_pc_mg_levels $LEV -fieldsplit_v_mg_levels_ksp_type richardson -fieldsplit_u_mg_levels_ksp_type richardson; done
+QUESTIONS:
+  a) why need to set mg_levels (-fieldsplit_?_pc_mg_levels $LEV)?
+  b) BUG: fails without galerkin
+
+3. one can do "monolithic" GMG too; 3 KSP iterations on all grids up to 2049^2:
+    for LEV in 5 6 7 8 9 10; do ./biharm -ksp_converged_reason -pc_type mg -da_refine $LEV -mg_levels_ksp_type richardson; done
+
+4. GAMG works fine too
+*/
 #include <petsc.h>
 
 typedef struct {
@@ -65,7 +80,7 @@ int main(int argc,char **argv) {
     Vec            w, w_initial, w_exact;
     BiharmCtx      user;
     Field          **aW;
-    double         erru, errv;
+    double         normv, normu, errv, erru;
     DMDALocalInfo  info;
 
     PetscInitialize(&argc,&argv,NULL,help);
@@ -79,11 +94,14 @@ int main(int argc,char **argv) {
     ierr = DMSetFromOptions(da); CHKERRQ(ierr);
     ierr = DMSetUp(da); CHKERRQ(ierr);  // this must be called BEFORE SetUniformCoordinates
     ierr = DMDASetUniformCoordinates(da,0.0,1.0,0.0,1.0,-1.0,-1.0); CHKERRQ(ierr);
+    ierr = DMDASetFieldName(da,0,"v"); CHKERRQ(ierr);
+    ierr = DMDASetFieldName(da,1,"u"); CHKERRQ(ierr);
 
     ierr = SNESCreate(PETSC_COMM_WORLD,&snes); CHKERRQ(ierr);
     ierr = SNESSetDM(snes,da); CHKERRQ(ierr);
     ierr = DMDASNESSetFunctionLocal(da,INSERT_VALUES,
                (DMDASNESFunction)FormFunctionLocal,&user); CHKERRQ(ierr);
+    ierr = SNESSetType(snes,SNESKSPONLY); CHKERRQ(ierr);
     ierr = SNESSetFromOptions(snes); CHKERRQ(ierr);
 
     ierr = DMGetGlobalVector(da,&w_initial); CHKERRQ(ierr);
@@ -96,18 +114,21 @@ int main(int argc,char **argv) {
     ierr = SNESGetDM(snes,&da); CHKERRQ(ierr);
     ierr = DMDAGetLocalInfo(da,&info); CHKERRQ(ierr);
 
-    ierr = DMGetGlobalVector(da,&w_exact); CHKERRQ(ierr);
+    ierr = VecDuplicate(w,&w_exact); CHKERRQ(ierr);
     ierr = DMDAVecGetArray(da,w_exact,&aW); CHKERRQ(ierr);
     ierr = FormExactWLocal(&info,aW,&user); CHKERRQ(ierr);
     ierr = DMDAVecRestoreArray(da,w_exact,&aW); CHKERRQ(ierr);
+    ierr = VecStrideNorm(w_exact,0,NORM_INFINITY,&normv); CHKERRQ(ierr);
+    ierr = VecStrideNorm(w_exact,1,NORM_INFINITY,&normu); CHKERRQ(ierr);
     ierr = VecAXPY(w,-1.0,w_exact); CHKERRQ(ierr);
-    ierr = DMRestoreGlobalVector(da,&w_exact); CHKERRQ(ierr);
     ierr = VecStrideNorm(w,0,NORM_INFINITY,&errv); CHKERRQ(ierr);
     ierr = VecStrideNorm(w,1,NORM_INFINITY,&erru); CHKERRQ(ierr);
     ierr = PetscPrintf(PETSC_COMM_WORLD,
-        "done on %d x %d grid;  errors |u-uexact|_inf = %.5e, |v-vexact|_inf = %.5e\n",
-        info.mx,info.my,erru,errv); CHKERRQ(ierr);
+        "done on %d x %d grid ...\n"
+        "  errors |v-vex|_inf/|vex|_inf = %.5e, |u-uex|_inf/|uex|_inf = %.5e\n",
+        info.mx,info.my,errv/normv,erru/normu); CHKERRQ(ierr);
 
+    ierr = VecDestroy(&w_exact); CHKERRQ(ierr);
     ierr = SNESDestroy(&snes); CHKERRQ(ierr);
     return PetscFinalize();
 }
@@ -130,79 +151,43 @@ PetscErrorCode FormExactWLocal(DMDALocalInfo *info, Field **aW, BiharmCtx *user)
     return 0;
 }
 
-FIXME from here
-PetscErrorCode FormFunctionLocal(DMDALocalInfo *info, double **au,
-                                 double **FF, PoissonCtx *user) {
+PetscErrorCode FormFunctionLocal(DMDALocalInfo *info, Field **aW,
+                                 Field **FF, BiharmCtx *user) {
     PetscErrorCode ierr;
-    MinimalCtx *mctx = (MinimalCtx*)(user->addctx);
     int        i, j;
-    double     xymin[2], xymax[2], hx, hy, hxhy, hyhx, x, y,
-               ue, uw, un, us, une, use, unw, usw,
-               dux, duy, De, Dw, Dn, Ds;
+    double     xymin[2], xymax[2], hx, hy, darea, scx, scy, scdiag, x, y,
+               ve, vw, vn, vs, ue, uw, un, us;
     ierr = DMDAGetBoundingBox(info->da,xymin,xymax); CHKERRQ(ierr);
     hx = (xymax[0] - xymin[0]) / (info->mx - 1);
     hy = (xymax[1] - xymin[1]) / (info->my - 1);
-    hxhy = hx / hy;
-    hyhx = hy / hx;
+    darea = hx * hy;               // multiply FD equations by this
+    scx = hy / hx;
+    scy = hx / hy;
+    scdiag = 2.0 * (scx + scy);    // diagonal scaling
     for (j = info->ys; j < info->ys + info->ym; j++) {
-        y = j * hy;
+        y = xymin[1] + j * hy;
         for (i = info->xs; i < info->xs + info->xm; i++) {
-            x = i * hx;
-            if (j==0 || i==0 || i==info->mx-1 || j==info->my-1) {
-                FF[j][i] = au[j][i] - user->g_bdry(x,y,0.0,user);
+            x = xymin[0] + i * hx;
+            if (i==0 || i==info->mx-1 || j==0 || j==info->my-1) {
+                FF[j][i].v = scdiag * aW[j][i].v;
+                FF[j][i].u = scdiag * aW[j][i].u;
             } else {
-                // assign neighbor values with either boundary condition or
-                //     current u at that point (==> symmetric matrix)
-                ue  = (i+1 == info->mx-1) ? user->g_bdry(x+hx,y,0.0,user)
-                                          : au[j][i+1];
-                uw  = (i-1 == 0)          ? user->g_bdry(x-hx,y,0.0,user)
-                                          : au[j][i-1];
-                un  = (j+1 == info->my-1) ? user->g_bdry(x,y+hy,0.0,user)
-                                          : au[j+1][i];
-                us  = (j-1 == 0)          ? user->g_bdry(x,y-hy,0.0,user)
-                                          : au[j-1][i];
-                if (i+1 == info->mx-1 || j+1 == info->my-1) {
-                    une = user->g_bdry(x+hx,y+hy,0.0,user);
-                } else {
-                    une = au[j+1][i+1];
-                }
-                if (i-1 == 0 || j+1 == info->my-1) {
-                    unw = user->g_bdry(x-hx,y+hy,0.0,user);
-                } else {
-                    unw = au[j+1][i-1];
-                }
-                if (i+1 == info->mx-1 || j-1 == 0) {
-                    use = user->g_bdry(x+hx,y-hy,0.0,user);
-                } else {
-                    use = au[j-1][i+1];
-                }
-                if (i-1 == 0 || j-1 == 0) {
-                    usw = user->g_bdry(x-hx,y-hy,0.0,user);
-                } else {
-                    usw = au[j-1][i-1];
-                }
-                // gradient  (dux,duy)   at east point  (i+1/2,j):
-                dux = (ue - au[j][i]) / hx;
-                duy = (un + une - us - use) / (4.0 * hy);
-                De = DD(dux * dux + duy * duy, mctx->q);
-                // ...                   at west point  (i-1/2,j):
-                dux = (au[j][i] - uw) / hx;
-                duy = (unw + un - usw - us) / (4.0 * hy);
-                Dw = DD(dux * dux + duy * duy, mctx->q);
-                // ...                  at north point  (i,j+1/2):
-                dux = (ue + une - uw - unw) / (4.0 * hx);
-                duy = (un - au[j][i]) / hy;
-                Dn = DD(dux * dux + duy * duy, mctx->q);
-                // ...                  at south point  (i,j-1/2):
-                dux = (ue + use - uw - usw) / (4.0 * hx);
-                duy = (au[j][i] - us) / hy;
-                Ds = DD(dux * dux + duy * duy, mctx->q);
-                // evaluate residual
-                FF[j][i] = - hyhx * (De * (ue - au[j][i]) - Dw * (au[j][i] - uw))
-                           - hxhy * (Dn * (un - au[j][i]) - Ds * (au[j][i] - us));
+                ve = (i+1 == info->mx-1) ? 0.0 : aW[j][i+1].v;
+                vw = (i-1 == 0)          ? 0.0 : aW[j][i-1].v;
+                vn = (j+1 == info->my-1) ? 0.0 : aW[j+1][i].v;
+                vs = (j-1 == 0)          ? 0.0 : aW[j-1][i].v;
+                FF[j][i].v = scdiag * aW[j][i].v - scx * (vw + ve) - scy * (vs + vn)
+                             - darea * (*(user->f))(x,y);
+                ue = (i+1 == info->mx-1) ? 0.0 : aW[j][i+1].u;
+                uw = (i-1 == 0)          ? 0.0 : aW[j][i-1].u;
+                un = (j+1 == info->my-1) ? 0.0 : aW[j+1][i].u;
+                us = (j-1 == 0)          ? 0.0 : aW[j-1][i].u;
+                FF[j][i].u = - darea * aW[j][i].v
+                             + scdiag * aW[j][i].u - scx * (uw + ue) - scy * (us + un);
             }
         }
     }
+    ierr = PetscLogFlops(18.0*info->xm*info->ym);CHKERRQ(ierr);
     return 0;
 }
 
