f754f9baf3d419ac2a145a101e9b14605da94323
Ed Bueler <elbueler@alaska.edu>
2019-08-01 10:19:38 -0800

    put this script in the right place

---
+++ c/ch3/study/poissonmats.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+set +x
+
+# generates data read by p4pdes-book/figs/poissoneigs.py to generate figure
+# in chapter 3
+
+POIS="../poisson"
+
+$POIS -da_grid_x 5 -da_grid_y 5 -mat_view binary:poissonmat5.dat
+$POIS -da_refine 0 -mat_view binary:poissonmat9.dat
+$POIS -da_refine 1 -mat_view binary:poissonmat17.dat
+$POIS -da_refine 2 -mat_view binary:poissonmat33.dat
+$POIS -da_refine 3 -mat_view binary:poissonmat65.dat
+$POIS -da_refine 4 -mat_view binary:poissonmat129.dat
+
