19b67780903a303698ff9dbaa912adef68a293f7
Ed Bueler <elbueler@alaska.edu>
2019-08-04 13:10:54 -0800

    generation script for figure in chapter 13

--- python/ch13/study/stokesmats.sh
+++ python/ch13/study/stokesmats.sh
@@ -2,13 +2,15 @@
 set -e
 set +x
 
-# generate .dat files, i.e. matrices, for script p4pdes-book/figs/stokeseigs.py
+# generate matrix .dat files for script p4pdes-book/figs/stokeseigs.py
+# note the problem is -nobase so that there is no kernel
 # (it generates a figure in chapter 13 of book)
 
-SOLVE="-s_ksp_type gmres  -s_ksp_converged_reason -s_ksp_rtol 1.0e-8 -pcpackage schur_lower_gmg"
+SOLVE="-s_ksp_type gmres -s_ksp_converged_reason -s_ksp_rtol 1.0e-8 -schurgmg lower"
 
+# -refine 2 is 9x9 grid
 for MU in 0.01 0.1 1.0 10.0 100.0; do
-    ../stokes.py -mu $MU -refine 1 $SOLVE \
-        -s_mat_type aij -s_ksp_view_mat binary:stokesmat${MU}.dat
+    echo "mu = ${MU}:"
+    ../stokes.py -nobase -mu $MU -refine 2 $SOLVE \
+        -s_mat_type aij -s_ksp_view_mat binary:stokesmat_mu${MU}.dat
 done
-
