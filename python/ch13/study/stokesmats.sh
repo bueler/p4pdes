e3e94f2d59c55e3f520073c17d8bc60bf6390e55
Ed Bueler <elbueler@alaska.edu>
2019-08-04 16:57:09 -0800

    understanding the spectrum of K needs computing cases where mu is smaller, in which case you see the nice behavior of our stable mixed FE method

--- python/ch13/study/stokesmats.sh
+++ python/ch13/study/stokesmats.sh
@@ -9,7 +9,7 @@ set +x
 SOLVE="-s_ksp_type gmres -s_ksp_converged_reason -s_ksp_rtol 1.0e-8 -schurgmg lower"
 
 # -refine 2 is 9x9 grid
-for MU in 0.01 0.1 1.0 10.0 100.0; do
+for MU in 0.0001 0.001 0.01 0.1 1.0 10.0 100.0; do
     echo "mu = ${MU}:"
     ../stokes.py -nobase -mu $MU -refine 2 $SOLVE \
         -s_mat_type aij -s_ksp_view_mat binary:stokesmat_mu${MU}.dat
