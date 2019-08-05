13ab5bdb932a87788337114308a031fa1601f839
Ed Bueler <elbueler@alaska.edu>
2019-08-05 09:37:17 -0800

    script for stokes.py convergence figure in chapter 13

--- python/ch13/study/stokesconv.sh
+++ python/ch13/study/stokesconv.sh
@@ -5,17 +5,23 @@ set +x
 # run as
 #    ./stokesconv.sh &> stokesconv.txt
 
-# FE methods are: P2xP1, Q2xQ1, P3xP2, CD(P2xP0)
+# minimal testing suggests this is faster than:
+#    GMRES+lower, MINRES+diag, MINRES+diag_nomass
+SOLVE="-s_ksp_type gmres -schurgmg lower_nomass"
+COMMON="-analytical -s_ksp_rtol 1.0e-8 -s_ksp_converged_reason $SOLVE"
 
-for FE in "" "-quad" "-udegree 3 -pdegree 2" "-pdegree 0 -dpressure"; do
-    for SOLVE in "-s_ksp_type gmres -pcpackage schur_lower_gmg" \
-                 "-s_ksp_type fgmres -pcpackage schur_lower_gmg_nomass" \
-                 "-s_ksp_type gmres -pcpackage schur_diag_gmg"; do
-        #for LEV in 2 3 4 5 6 7 8; do  # WARNING: level 8 (=513x513) is expensive
-        for LEV in 3 4 5 6; do  # quicker
-            /usr/bin/time -f "real %e" ../stokes.py -analytical $FE -refine $LEV $SOLVE \
-                -s_ksp_converged_reason -s_ksp_rtol 1.0e-8
-        done
+# FE methods are: P2xP1, Q2xQ1, P3xP2, CD(P2xP0)
+# level 8 is 513x513
+# level 9 is 1025x1025
+for FE in "" "-quad" "-pdegree 0 -dpressure"; do
+    for LEV in 2 3 4 5 6 7 8 9; do
+        /usr/bin/time -f "real %e" ../stokes.py $COMMON $FE -refine $LEV
+    done
+done
+# higher-degree is more memory per element
+for FE in "-udegree 3 -pdegree 2"; do
+    for LEV in 2 3 4 5 6 7 8; do
+        /usr/bin/time -f "real %e" ../stokes.py $COMMON $FE -refine $LEV
     done
 done
 
