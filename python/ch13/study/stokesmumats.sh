9cbfc1a55ee8a6bf6fd011b8079ac2cb00a9f36e
Ed Bueler <elbueler@alaska.edu>
2019-08-11 12:26:40 -0800

    rename to clarify goal of mats

--- python/ch13/study/stokesmumats.sh
+++ python/ch13/study/stokesmumats.sh
@@ -2,9 +2,10 @@
 set -e
 set +x
 
-# generate matrix .dat files for script p4pdes-book/figs/stokeseigs.py
+# generate matrix .dat files for script p4pdes-book/figs/stokesmueigs.py
+#   which generates a figure in chapter 13 of book
+# shows dependence of eigenvalues on the viscosity mu
 # note the problem is -nobase so that there is no kernel
-# (it generates a figure in chapter 13 of book)
 
 SOLVE="-s_ksp_type gmres -s_ksp_converged_reason -s_ksp_rtol 1.0e-8 -schurgmg lower_mass"
 
@@ -12,5 +13,5 @@ SOLVE="-s_ksp_type gmres -s_ksp_converged_reason -s_ksp_rtol 1.0e-8 -schurgmg lo
 for MU in 0.00001 0.0001 0.001 0.01 0.1 1.0 10.0 100.0; do
     echo "mu = ${MU}:"
     ../stokes.py -nobase -mu $MU -refine 2 $SOLVE \
-        -s_mat_type aij -s_ksp_view_mat binary:stokesmat_mu${MU}.dat
+        -s_mat_type aij -s_ksp_view_mat binary:stokes_mu${MU}.dat
 done
