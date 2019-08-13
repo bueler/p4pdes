08125977c0ba13cc0b6442e08695f889cdcf65bc
Ed Bueler <elbueler@alaska.edu>
2019-08-13 13:24:20 -0800

    add more mixed FE types and clean up comments

--- python/ch13/study/schurmats.sh
+++ python/ch13/study/schurmats.sh
@@ -2,28 +2,41 @@
 set -e
 set +x
 
-# generate matrix .dat files for script p4pdes-book/figs/schureigs.py
-#   which generates a figure in chapter 13 of book; the goal is to show
-#   dependence of norms of S_h^{-1} on the mesh size h, for two different
-#   mixed FE methods
-# here we just write the whole system matrix K_h
-# note the problem is -nobase so that K_h has no kernel
+# generate matrix .dat files to show dependence of norms of S_h^{-1} on the
+# mesh size h, for different mixed FE methods; here we just write the whole
+# system matrix K_h; the problem is -nobase so K_h is invertible IF the
+# elements are stable
+
+# see p4pdes-book/figs/schurinverse.py to process these
 
 # set rtol to 1.0 so succeeds on first iter
 SOLVE="-s_ksp_type minres -s_pc_type none -s_ksp_rtol 1.0"
 
-#FIXME what elements do I want?
-
-for LEV in 1 2 3 4; do  # LEV=4 is already generating N=O(10^4) matrices
+for LEV in 1 2 3 4; do  # LEV=4 is generates N=O(10^4) matrices
     OUT=schur_P2P1_lev${LEV}.dat
     echo "generating ${OUT} ..."
     ../stokes.py -nobase $SOLVE -refine $LEV \
         -udegree 2 -pdegree 1 \
         -s_mat_type aij -s_ksp_view_mat binary:${OUT}
+    OUT=schur_Q2Q1_lev${LEV}.dat
+    echo "generating ${OUT} ..."
+    ../stokes.py -nobase $SOLVE -refine $LEV \
+        -quad -udegree 2 -pdegree 1 \
+        -s_mat_type aij -s_ksp_view_mat binary:${OUT}
     OUT=schur_CD_lev${LEV}.dat
     echo "generating ${OUT} ..."
     ../stokes.py -nobase $SOLVE -refine $LEV \
         -udegree 2 -pdegree 0 -dpressure \
+        -s_mat_type aij -s_ksp_view_mat binary:${OUT}
+    OUT=schur_P1P1_lev${LEV}.dat
+    echo "generating ${OUT} ..."
+    ../stokes.py -nobase $SOLVE -refine $LEV \
+        -udegree 1 -pdegree 1 \
+        -s_mat_type aij -s_ksp_view_mat binary:${OUT}
+    OUT=schur_P1P0_lev${LEV}.dat
+    echo "generating ${OUT} ..."
+    ../stokes.py -nobase $SOLVE -refine $LEV \
+        -udegree 1 -pdegree 0 -dpressure \
         -s_mat_type aij -s_ksp_view_mat binary:${OUT}
 done
 
