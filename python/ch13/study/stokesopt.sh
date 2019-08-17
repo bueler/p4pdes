440c93d96369bd96467ceff3461719801583a641
Ed Bueler <elbueler@alaska.edu>
2019-08-17 14:01:03 -0800

    update script to latest options

--- python/ch13/study/stokesopt.sh
+++ python/ch13/study/stokesopt.sh
@@ -9,21 +9,23 @@ set +x
 # FE method is Q^2 x Q^1 Taylor-Hood
 
 MAXLEV=9   # LEV=9 is 1025x1025 uniform grid with N about 10^7
-for SOLVE in "-s_ksp_type minres -schurgmg diag_mass" \
-             "-s_ksp_type minres -schurgmg diag_eye" \
-             "-s_ksp_type minres -schurgmg diag" \
-             "-s_ksp_type gmres -schurgmg lower_mass" \
-             "-s_ksp_type gmres -schurgmg lower_eye" \
-             "-s_ksp_type gmres -schurgmg lower"; do
-    for (( LEV=2; LEV<=$MAXLEV; LEV++ )); do
-        cmd="../stokes.py -quad -showinfo -s_ksp_converged_reason ${SOLVE} -refine ${LEV} -log_view"
-        echo $cmd
-        rm -f foo.txt
-        $cmd &> foo.txt
-        'grep' "sizes:" foo.txt
-        'grep' "solve converged due to" foo.txt
-        'grep' "Flop:  " foo.txt | awk '{print $2}'
-        'grep' "Time (sec):" foo.txt | awk '{print $3}'
+for SGMG in "-s_ksp_type minres -schurgmg diag" \
+            "-s_ksp_type gmres -schurgmg lower" \
+            "-s_ksp_type gmres -schurgmg full"; do
+    for SPRE in "-schurpre selfp" \
+                "-schurpre mass" \
+                "-schurpre eye"; do
+        for (( LEV=2; LEV<=$MAXLEV; LEV++ )); do
+            cmd="../stokes.py -quad -showinfo -s_ksp_converged_reason ${SGMG} ${SPRE} -refine ${LEV} -log_view"
+            echo $cmd
+            rm -f foo.txt
+            $cmd &> foo.txt
+            'grep' "sizes:" foo.txt
+            'grep' "solve converged due to" foo.txt
+            'grep' "Flop:  " foo.txt | awk '{print $2}'
+            'grep' "Time (sec):" foo.txt | awk '{print $3}'
+        done
+        echo
     done
 done
 
