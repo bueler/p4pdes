b241a50c91961b6eb1b173b868004816afcec9f0
Ed Bueler <elbueler@alaska.edu>
2019-08-22 15:39:31 -0800

    update the cluster script example

--- c/cluster/README.md
+++ c/cluster/README.md
@@ -1,13 +1,13 @@
 Running jobs on CLUSTER
 =======================
 
-The only thing in this subdirectory is a sample batch script for running codes
-from previous Chapters.
+This subdirectory contains a sample batch script which runs codes from
+the book _PETSc for Partial Differential Equations_.
 
 CLUSTER is a modest-sized Linux cluster at the University of Alaska Fairbanks:
 http://www.gi.alaska.edu/research-computing-systems/hpc/chinook
 
 Access is generally limited to UAF students and faculty.  Thus this batch
 script is _only_ an example which might be useful as a suggestion for how
-PETSc codes are run on many-node machines.
+PETSc codes are run in a batch system on many-node machines.
 
--- c/cluster/cluster.sh
+++ c/cluster/cluster.sh
@@ -1,39 +1,42 @@
 #!/bin/bash
 
-# This is a sample batch file for running jobs on a linux cluster.  A goal is
-# that the total run time is less than an hour for P > 8 processes.  With a
-# bigger machine (e.g. >100 processes) consider refining the grids more in
-# the runs below.
+# WARNING:  You will need to edit this file to match your batch system!!
+
+# This is a sample batch file for running jobs on a linux cluster.
+
+# The goals for the runs below, and their various sizes, is that
+# that the total run time is less than 15 minutes for P >= 4 processes.
+# With a bigger machine (e.g. >100 processes) consider further refinement of
+# grids.  The total memory usage is less than 16 Gb.
 
 # In parent directory ../c/, set PETSC_ARCH to a --with-debugging=0 PETSc
 # configuration.  Then run "make distclean" to clean out old executables.
 # Then run "make test" to make sure executables in ../ch*/ get built.
 
-# For each run a -log_view is generated.  These can be analyzed for performance
-# data.  For example do
-#   grep "(sec):" cluster.xxxx
-# to see total times of runs.
-
-# You will need to edit this file to match your batch system!!
+# For each run a -log_view is generated in a file cluster.xxx.  This can be
+# analyzed for performance data.  For example do
+#   grep "Flop:  " p4pdes.xxx
+#   grep "Time (sec):" p4pdes.xxx
+# to see total flops and max times of runs.
 
 # Many details below are particular to the Slurm batch system;
 #     https://slurm.schedmd.com/
 
-#SBATCH --partition=debug
-#SBATCH --ntasks=12
+#SBATCH --partition=t1standard
+#SBATCH --ntasks=32
 #SBATCH --tasks-per-node=4
-#SBATCH --mail-user=elbueler@alaska.edu   # FIXME don't release this way; I'll get spammed by slurm!
+#SBATCH --mail-user=<USERNAME>@alaska.edu
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
-#SBATCH --output=cluster.o.%j
-
-# Submit from c/cluster/ for this to work.
-cd $SLURM_SUBMIT_DIR
+#SBATCH --output=p4pdes.o.%j
 
 # This cluster needs these for some reason.
 ulimit -s unlimited
 ulimit -l unlimited
+
+# Submit from c/cluster/ for this to work.
+cd $SLURM_SUBMIT_DIR
 
 # Generate a list of allocated nodes; will serve as a machinefile for mpirun.
 srun -l /bin/hostname | sort -n | awk '{print $2}' > ./nodes.$SLURM_JOB_ID
@@ -41,41 +44,40 @@ srun -l /bin/hostname | sort -n | awk '{print $2}' > ./nodes.$SLURM_JOB_ID
 # Launch the MPI application.  Tested with mpich2.
 GO="mpiexec -n $SLURM_NTASKS -machinefile ./nodes.$SLURM_JOB_ID"
 
-#-------------------------- EXAMPLES ------------------------------------
+#-------------------  PARALLEL RUNS -------------------
 
 # PATTERN:  solve 2D, time-dependent, coupled diffusion-reaction equations
-# -da_refine 6 is 256x256; this does ~230 time steps
-$GO ../ch5/pattern -da_refine 6 -ptn_phi 0.05 -ptn_kappa 0.063 -ts_final_time 5000 -ptn_noisy_init 0.15 -ts_monitor -log_view
-
-# FISH with ASM+LU (single-level DD) solver:  solve 2D Poisson equation
-# -da_refine 9 is 1025 x 1025; this does hundreds of KSP iterations
-# note: this needs 256 tasks to be a data point in a figure in Chapter 6
-$GO ../ch6/fish -fsh_dim 2 -snes_type ksponly -ksp_type gmres -ksp_rtol 1.0e-10 -ksp_converged_reason -log_view -pc_type asm -sub_pc_type lu -da_refine 9
-
-# FISH with Dryja&Widlund (two-level DD) solver:  solve 2D Poisson equation
-# -da_refine 9 is 1025 x 1025; this should do fewer than than 20 KSP iterations
-# note: this needs 256 tasks to be a data point in a figure in Chapter 6
-$GO ../ch6/fish -fsh_dim 2 -snes_type ksponly -ksp_type gmres -ksp_rtol 1.0e-10 -ksp_converged_reason -log_view -pc_type mg -pc_mg_levels 2 -pc_mg_type additive -mg_levels_ksp_type preonly -mg_levels_pc_type asm -mg_levels_sub_pc_type lu -mg_coarse_ksp_type preonly -mg_coarse_pc_type redundant -mg_coarse_redundant_pc_type lu -da_refine 9
+# using ARKIMEX time-stepping and GMRES+BJACOBI+ILU to solve implicit steps
+# -da_refine 6 is 256x256; does ~100 time steps
+$GO ../ch5/pattern -da_refine 6 -ptn_phi 0.05 -ptn_kappa 0.063 -ts_max_time 2000 -ptn_noisy_init 0.15 -ts_monitor -log_view
 
 # FISH:  solve 3D Poisson equation
-# coarse grid is 17x17x17 so should work on O(10^3) processors (or fewer)
-# -da_refine 8 is 513x513x513; uses ~6Gb memory
-# FIXME consider adding telescope
-$GO ../ch6/fish -fsh_dim 3 -da_refine 8 -pc_mg_levels 6 -pc_type mg -snes_type ksponly -ksp_converged_reason -log_view
+# using optimal CG+GMG solver
+# -da_refine 7 is 257x257x257
+# ~ 12 Gb memory
+# coarse grid is 9x9x9 so should work up to several hundred processors
+$GO ../ch6/fish -fsh_dim 3 -da_refine 7 -pc_mg_levels 6 -pc_type mg -snes_type ksponly -ksp_converged_reason -log_view
 
-# FIXME consider a minimal example using NEWTON+CG+GMG
+# MINIMAL:  solve 2D minimal surface equation
+# using optimal grid-sequenced Newton GMRES+GMG solver
+# 33x33 base grid with -snes_grid_sequence 6 is 2049x2049 finest grid
+# ~ 8 Gb memory
+# coarse grid is 33x33 so should work up to several hundred processors
+$GO ../ch7/minimal -da_grid_x 33 -da_grid_y 33 -snes_grid_sequence 6 -snes_fd_color -snes_converged_reason -snes_monitor -ksp_converged_reason -pc_type mg -log_view
 
-# ADVECT:  solve 2D advection equation
-# -da_refine 8 is 1280x1280; this does ~2400 time steps
-$GO ../ch11/advect -da_refine 8 -ts_final_time 1.0 -ts_rk_type 3bs -ts_monitor -log_view
+# ADVECT:  solve 2D, time-dependent advection equation
+# using RK3bs (explicit) solver
+# -da_refine 8 is 1280x1280; does ~1200 time steps
+$GO ../ch11/advect -da_refine 8 -ts_max_time 0.5 -ts_rk_type 3bs -ts_monitor -log_view
 
 # OBSTACLE:  solve 2D inequality-constrained free-boundary problem
+# using optimal grid-sequenced bound-constrained Newton CG+GMG solver
 # 33x33 base grid with -snes_grid_sequence 6 is 2049x2049 finest grid
+# ~ 8 Gb memory
+# coarse grid is 33x33 so should work up to several hundred processors
 $GO ../ch12/obstacle -da_grid_x 33 -da_grid_y 33 -snes_grid_sequence 6 -snes_converged_reason -snes_monitor -ksp_converged_reason -pc_type mg -log_view
 
-#FIXME add an unstructured Stokes problem from Chapter 13 using Firedrake
-
-#-----------------------------------------------------------------------
+#------------------------------------------------------
 
 # Clean up the machinefile.
 rm ./nodes.$SLURM_JOB_ID
