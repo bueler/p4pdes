866d457a0cdaf6e38eb151a1a2925297d728b7d3
Ed Bueler <elbueler@alaska.edu>
2019-09-22 14:05:56 -0800

    weak scaling scripts simpler: use minimal only

--- c/ch8/genweak.py
+++ c/ch8/genweak.py
@@ -6,19 +6,29 @@ from argparse import ArgumentParser, RawTextHelpFormatter
 import numpy as np
 
 intro = '''
-Write SLURM batch files for weak scaling study using both ch7/minimal.c in 2D
-and ch6/fish.c in 3D.  Examples:
-    ./genweak.py -email xx@yy.edu -queue debug -maxP 16 -pernode 6 -minutes 30 -streams
-    ./genweak.py -email xx@yy.edu -queue t2standard -maxP 256 -pernode 8 -minutes 60
+Write SLURM batch files for weak scaling study using ch7/minimal.c.  Examples:
+    ./genweak.py -email elbueler@alaska.edu -queue debug -maxP 1 -minutes 60
+    ./genweak.py -email elbueler@alaska.edu -queue debug -minP 4 -maxP 4 -pernode 2 -minutes 60
+    ./genweak.py -email elbueler@alaska.edu -queue debug -minP 4 -maxP 4 -pernode 4 -minutes 60
+    ./genweak.py -email elbueler@alaska.edu -queue t2standard -minP 4 -maxP 4 -pernode 1 -minutes 60
+    ./genweak.py -email elbueler@alaska.edu -queue t2standard -minP 16 -maxP 64 -pernode 4 -minutes 60
+    ./genweak.py -email elbueler@alaska.edu -queue t2standard -minP 16 -maxP 256 -pernode 8 -minutes 60
+Solves 2D minimal surface equation using grid-sequenced Newton GMRES+GMG solver
+and 33x33 coarse grid.  Each process gets a 512x512 grid with N/P = 2.6e5.
 '''
 
 parser = ArgumentParser(description=intro, formatter_class=RawTextHelpFormatter)
 parser.add_argument('-email', metavar='EMAIL', type=str,
                     default='USERNAME@alaska.edu', help='email address')
+parser.add_argument('-lev', type=int, default=4, metavar='X',
+                    help='''refinement level for -snes_grid_sequence; in {4,5,6,7,8}''')
 parser.add_argument('-maxP', type=int, default=4, metavar='P',
                     help='''maximum number of MPI processes;
-power of 4 like 16,64,256,1024,4096,... recommended''')
-parser.add_argument('-minutes', type=int, default=120, metavar='T',
+power of 4 like 4,16,64,256,1024,... recommended''')
+parser.add_argument('-minP', type=int, default=1, metavar='P',
+                    help='''minimum number of MPI processes;
+power of 4 like 1,4,16,64,256,... recommended''')
+parser.add_argument('-minutes', type=int, default=60, metavar='T',
                     help='''max time in minutes for SLURM job''')
 parser.add_argument('-queue', metavar='Q', type=str,
                     default='debug', help='SLURM queue (partition) name')
@@ -33,15 +43,11 @@ args = parser.parse_args()
 print('settings: %s queue, %d max tasks per node, %s as email, request time %d minutes'
       % (args.queue,args.pernode,args.email,args.minutes))
 
-m2D = int(np.floor(np.log(float(args.maxP)) / np.log(4.0)))
-Plist2D = np.round(4.0**np.arange(m2D+1)).astype(int).tolist()
-print('2D runs (ch7/minimal.c) will use P in'),
-print(Plist2D)
-
-m3D = int(np.floor(np.log(float(args.maxP)) / np.log(8.0)))
-Plist3D = np.round(8.0**np.arange(m3D+1)).astype(int).tolist()
-print('3D runs (ch6/fish.c) will use P in'),
-print(Plist3D)
+m_min = int(np.floor(np.log(float(args.minP)) / np.log(4.0)))
+m_max = int(np.floor(np.log(float(args.maxP)) / np.log(4.0)))
+Plist = np.round(4.0**np.arange(m_min,m_max+1)).astype(int).tolist()
+print('runs (ch7/minimal.c) will use P in'),
+print(Plist)
 
 rawpre = r'''#!/bin/bash
 
@@ -75,19 +81,6 @@ make streams NPMAX=$SLURM_NTASKS
 cd $SLURM_SUBMIT_DIR
 '''
 
-rawfish = r'''
-# FISH:  solve 3D Poisson equation
-# using optimal CG+GMG solver and 9x9x9 coarse grid
-# with -da_refine %d is %dx%dx%d fine grid
-# each process has N/P = %d degrees of freedom
-
-$GO ../ch6/fish -fsh_dim 3 -da_grid_x 9 -da_grid_y 9 -da_grid_z 9 -da_refine %d -pc_type mg -snes_type ksponly -ksp_converged_reason -log_view
-'''
-fishdict = {  1: (3,65),
-              8: (4,129),
-             64: (5,257),
-            512: (6,513)}
-
 rawminimal = r'''
 # MINIMAL:  solve 2D minimal surface equation
 # using grid-sequenced Newton GMRES+GMG solver and 33x33 coarse grid
@@ -102,38 +95,26 @@ minimaldict = {  1: (4,513),
                 64: (7,4097),
                256: (8,8193)}
 
-for dim in [2, 3]:
-    if dim == 2:
-        Plist = Plist2D
-        code = 'minimal'
-        xdict = minimaldict
-    else:
-        Plist = Plist3D
-        code = 'fish'
-        xdict = fishdict
-    for P in Plist:
-        rlev = xdict[P][0]  # refinement level
-        grid = xdict[P][1]
-        if dim == 2:
-            wrun = rawminimal % (rlev,grid,grid,grid*grid/P,rlev)
-        else:
-            wrun = rawfish % (rlev,grid,grid,grid,grid*grid*grid/P,rlev)
+for P in Plist:
+    rlev = minimaldict[P][0]  # refinement level
+    grid = minimaldict[P][1]
+    wrun = rawminimal % (rlev,grid,grid,grid*grid/P,rlev)
 
-        pernode = min(P,args.pernode)
-        nodes = P / pernode
-        print('  case: run %s with %d nodes, %d tasks per node, and P=%d processes on %d^%d grid'
-              % (code,nodes,pernode,P,grid,dim))
+    pernode = min(P,args.pernode)
+    nodes = P / pernode
+    print('  case: %d nodes, %d tasks per node, and P=%d processes on %dx%d grid'
+          % (nodes,pernode,P,grid,grid))
 
-        root = 'weak_%s_%d' % (code,P)
-        preamble = rawpre % (args.queue,P,pernode,args.minutes,args.email,
-                             root + r'.o.%j')
+    root = 'weak_minimal_%s_%d_%d' % (args.queue[:2],P,pernode)
+    preamble = rawpre % (args.queue,P,pernode,args.minutes,args.email,
+                         root + r'.o.%j')
 
-        batchname = root + '.sh'
-        print('    writing %s ...' % batchname)
-        batch = open(batchname,'w')
-        batch.write(preamble)
-        if args.streams:
-            batch.write(rawstreams)
-        batch.write(wrun)
-        batch.close()
+    batchname = root + '.sh'
+    print('    writing %s ...' % batchname)
+    batch = open(batchname,'w')
+    batch.write(preamble)
+    if args.streams:
+        batch.write(rawstreams)
+    batch.write(wrun)
+    batch.close()
 
