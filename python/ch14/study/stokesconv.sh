76a5eb48a1aaef46fb18b331299476191675188e
Ed Bueler <elbueler@alaska.edu>
2020-04-18 18:17:48 -0800

    this now runs to completion .. need -schurpre option because default missing (should I add it?) .. have not looked at results but once they are good I will strip-out -vectorlap from "make test" runs

--- python/ch14/study/stokesconv.sh
+++ python/ch14/study/stokesconv.sh
@@ -5,23 +5,21 @@ set +x
 # run as
 #    ./stokesconv.sh &> stokesconv.txt
 
-# minimal testing suggests this is faster than:
-#    GMRES+lower_mass, MINRES+diag_mass, MINRES+diag
-SOLVE="-s_ksp_type gmres -schurgmg lower"
-COMMON="-analytical -s_ksp_rtol 1.0e-8 -s_ksp_converged_reason $SOLVE"
+COMMON="-analytical -s_ksp_rtol 1.0e-8 -s_ksp_converged_reason"
+
+# some GMG+Schur solver alternatives use one each of the following:
+#    -s_ksp_type gmres|fgmres|minres
+#    -schurgmg diag|lower|full
+#    -schurpre selfp|mass
+SOLVE="-s_ksp_type gmres -schurgmg lower -schurpre selfp"
 
 # FE methods are: P2xP1, Q2xQ1, P2xP0 (CD) and then P3xP2
-# level 8 is 513x513
-# level 9 is 1025x1025
-for FE in "" "-quad" "-pdegree 0 -dp"; do
-    for LEV in 2 3 4 5 6 7 8 9; do
-        /usr/bin/time -f "real %e" ../stokes.py $COMMON $FE -refine $LEV
-    done
-done
-# higher-degree is more memory per element
-for FE in "-udegree 3 -pdegree 2"; do
-    for LEV in 2 3 4 5 6 7 8; do
-        /usr/bin/time -f "real %e" ../stokes.py $COMMON $FE -refine $LEV
+# level 6 is 129x129, 7 is 257x257, 8 is 513x513, 9 is 1025x1025
+for FE in "" "-quad" "-pdegree 0 -dp" "-udegree 3 -pdegree 2"; do
+    for LEV in 2 3 4 5 6; do   # add levels as desired
+        cmd="../stokes.py ${COMMON} ${SOLVE} ${FE} -refine ${LEV}"
+        echo $cmd
+        /usr/bin/time -f "real %e" $cmd
     done
 done
 
