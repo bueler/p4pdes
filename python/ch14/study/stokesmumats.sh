16bdcd700352ac06fc480133b5a7c7c3f64d6c3f
Ed Bueler <elbueler@alaska.edu>
2020-05-01 21:17:46 -0800

    update so I can regenerate figure in chapter 14

--- python/ch14/study/stokesmumats.sh
+++ python/ch14/study/stokesmumats.sh
@@ -3,11 +3,12 @@ set -e
 set +x
 
 # generate matrix .dat files for script p4pdes-book/figs/stokesmueigs.py
-#   which generates a figure in chapter 13 of book
-# shows dependence of eigenvalues on the viscosity mu
+# which generates a figure in chapter 14 of book showing dependence of
+# eigenvalues on the viscosity mu
+
 # note the problem is -nobase so that there is no kernel
 
-SOLVE="-s_ksp_type gmres -s_ksp_converged_reason -s_ksp_rtol 1.0e-8 -schurgmg lower_mass"
+SOLVE="-s_ksp_type gmres -s_ksp_converged_reason -s_ksp_rtol 1.0e-8 -schurgmg lower -schurpre mass"
 
 # -refine 2 is 9x9 grid
 for MU in 0.00001 0.0001 0.001 0.01 0.1 1.0 10.0 100.0; do
