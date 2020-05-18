9fc7a3a0d0762bc5ca05779b253dfb12a8995c48
Ed Bueler <elbueler@alaska.edu>
2020-05-18 14:54:39 -0800

    do exercise 1.1

--- c/ch1/e.c
+++ c/ch1/e.c
@@ -19,7 +19,7 @@ int main(int argc, char **argv) {
   ierr = MPI_Allreduce(&localval,&globalsum,1,MPIU_REAL,MPIU_SUM,
       PETSC_COMM_WORLD); CHKERRQ(ierr);
 
-  // output one estimate of e and report on work from each process
+  // output estimate of e and report on work from each process
   ierr = PetscPrintf(PETSC_COMM_WORLD,
       "e is about %17.15f\n",globalsum); CHKERRQ(ierr);
   ierr = PetscPrintf(PETSC_COMM_SELF,
