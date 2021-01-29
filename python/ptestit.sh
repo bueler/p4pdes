8a7524a841c7802e2f2284d527ecbfc6e5eb7beb
Ed Bueler <elbueler@alaska.edu>
2021-01-28 21:57:19 -0500

    Start test harness; re-uses .c output.

---
+++ python/ptestit.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# A script to run regression tests from the c/chN/ directories.  Use "make test"
+# which runs this script as follows:
+#    ./testit.sh PROGRAM OPTS PROCESSES TESTNUM
+
+rm -f maketmp tmp difftmp
+
+make $1 > maketmp 2>&1;
+
+grep warning maketmp
+
+CURRDIR=${PWD##*/}
+
+if [ $3 -eq 1 ]; then
+    CMD="./$1.py $2"
+elif [ $3 -eq -1 ]; then
+    CMD="$1.py $2"
+else
+    CMD="mpiexec -n $3 ./$1.py $2"
+fi
+
+if [[ ! -f output/$1.test$4 ]]; then
+    echo "FAIL: Test #$4 of $CURRDIR/$1.py"
+    echo "       command = '$CMD'"
+    echo "       OUTPUT MISSING"
+
+else
+
+    $CMD &> tmp
+
+    diff output/$1.test$4 tmp > difftmp
+
+    if [[ -s difftmp ]] ; then
+       echo "FAIL: Test #$4 of $CURRDIR/$1.py"
+       echo "       command = '$CMD'"
+       echo "       diffs follow:"
+       cat difftmp
+    else
+       echo "PASS: Test #$4 of $CURRDIR/$1.py"
+       rm -f maketmp tmp difftmp
+    fi
+
+fi
+
