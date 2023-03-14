acbcfbdfc0ddc5ccc7f657b2fca9f25a2104ca3a
Ed Bueler <elbueler@alaska.edu>
2023-03-14 13:07:20 -0800

    a few bells and whistles

--- python/ch13/solns/bratu.py
+++ python/ch13/solns/bratu.py
@@ -1,19 +1,17 @@
 from firedrake import *
+from firedrake.petsc import PETSc
+
 mesh = UnitSquareMesh(10, 10)
 V = FunctionSpace(mesh, "CG", 1)
-
-u = Function(V)
-v = TestFunction(V)
 
 x, y = SpatialCoordinate(mesh)
 frhs = Function(V).interpolate(2.0*(x*(1.0-x) + y*(1.0-y)) + exp(x*(1.0-x)*y*(1.0-y)))
 
 u = Function(V)
+v = TestFunction(V)
 F = (inner(grad(u), grad(v)) + exp(u) * v - frhs * v) * dx
 
-bdry_ids = (1, 2, 3, 4)
-bc = DirichletBC(V, Constant(0.0), bdry_ids)
-
+bc = DirichletBC(V, Constant(0.0), (1, 2, 3, 4))
 sp = {'snes_type': 'newtonls',
       'snes_linesearch_type': 'basic',
       'snes_monitor': None,
@@ -22,4 +20,11 @@ sp = {'snes_type': 'newtonls',
 solve(F == 0, u, solver_parameters=sp, bcs=bc)
 
 uexact = Function(V).interpolate(x*(1.0-x)*y*(1.0-y)) # for comparison
+u.rename('u (numerical soln)')
+uexact.rename('uexact (exact soln)')
 File("bratu.pvd").write(u,uexact)
+
+diffu = Function(V).interpolate(u - uexact)
+error_L2 = sqrt(assemble(dot(diffu, diffu) * dx))
+PETSc.Sys.Print('done on mesh with %d nodes:  |u-uexact|_2 = %.3e' \
+      % (len(u.dat.data), error_L2))
